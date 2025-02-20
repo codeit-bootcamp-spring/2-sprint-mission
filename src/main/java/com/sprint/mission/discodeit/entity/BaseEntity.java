4750875369e5e2c912f4191a0d236d2fc4202115
manleKim <kk410910@naver.com>
2025-02-20 11:00:13 +0900

    feat: BaseEntity 도메인 모델 구현
    - 도메인의 공통 속성과 메서드를 모아 추상화
    - id, createdAt, updateAt 필드
    - getter, updateUpdatedAt 메서드

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.UUID;
+
+public abstract class BaseEntity {
+    protected final UUID id;
+    protected final long createdAt;
+    protected long updatedAt;
+
+    public BaseEntity() {
+        this.id = UUID.randomUUID();
+        this.createdAt = System.currentTimeMillis();
+        this.updatedAt = System.currentTimeMillis();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void updateUpdatedAt() {
+        this.updatedAt = System.currentTimeMillis();
+    }
+}
