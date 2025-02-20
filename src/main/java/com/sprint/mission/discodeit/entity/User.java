be024ff06d8ee090db3f0d89461911428f9bd84a
manleKim <kk410910@naver.com>
2025-02-20 09:46:29 +0900

    feat: User 도메인 모델 구현

---
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private Long createdAt;
+    private Long updatedAt;
+
+    public User() {
+        this.id = UUID.randomUUID();
+        this.createdAt = System.currentTimeMillis();
+        this.updatedAt = System.currentTimeMillis();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void updateUpdatedAt() {
+        this.updatedAt = System.currentTimeMillis();
+    }
+}
