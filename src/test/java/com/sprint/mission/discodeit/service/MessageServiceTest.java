68939ff99ae31a2125dfcc3fb81ac3089dc77626
황지환 <hth99131@gmail.com>
2025-02-20 22:12:18 +0900

    refactor(MessageServiceTest): Message 테스트 코드정리

--- src/test/java/com/sprint/mission/discodeit/service/MessageServiceTest.java
+++ src/test/java/com/sprint/mission/discodeit/service/MessageServiceTest.java
@@ -5,26 +5,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.sprint.mission.application.MessageDto;
 import com.sprint.mission.discodeit.jcf.JCFMessageService;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MessageServiceTest {
-    @Test
-    void 메세지_생성(){
-        MessageService messageService = new JCFMessageService();
-        String context = "안녕하세요";
+    private MessageService messageService;
+    private MessageDto messageDto;
+    private static final String CONTEXT = "안녕하세요";
 
-        MessageDto messageDto = messageService.create(context);
-        assertThat(messageDto.context()).isEqualTo(context);
+    @BeforeEach
+    void setUp() {
+        messageService = new JCFMessageService();
+        messageDto = messageService.create(CONTEXT);
     }
 
     @Test
-    void 메세지_단건_조회(){
-        MessageService messageService = new JCFMessageService();
-        String context = "안녕하세요";
+    void 메세지_생성() {
+        assertThat(messageDto.context()).isEqualTo(CONTEXT);
+    }
 
-        MessageDto messageDto = messageService.create(context);
+    @Test
+    void 메세지_단건_조회() {
         List<MessageDto> messages = messageService.findById(messageDto.messageId());
 
-        assertThat(messages.get(0).context()).isEqualTo(context);
+        assertThat(messages.get(0).context()).isEqualTo(CONTEXT);
     }
 }
