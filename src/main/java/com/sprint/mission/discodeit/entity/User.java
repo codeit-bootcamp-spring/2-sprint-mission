5b9b3f9ab6bd023be6cb85de49dd56a9639bd3c0
Leekb0804 <lkb8713@gmail.com>
2025-02-20 13:44:26 +0900

    feat(entity): User Entity 필드 및 메서드 추가
    - BaseEntity 상속
    - userName, userEmail, password 필드 추가
    - 생성자에서 BaseEntity 생성자 호출 및 필드 초기화
    - userName, userEmail, password 값을 불러오는 getter 메소드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,4 +1,28 @@
 package com.sprint.mission.discodeit.entity;
 
-public class User {
+public class User extends BaseEntity {
+    private String userName;
+    private String userEmail;
+    private String password;
+
+    public User(String userName, String userEmail, String password) {
+        super();
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.password = password;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getPassWord() {
+        return password;
+    }
+
 }
