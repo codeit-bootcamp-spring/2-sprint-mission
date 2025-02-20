5431824391d661bea764c57b6dbebdff53760b79
Leekb0804 <lkb8713@gmail.com>
2025-02-20 16:20:35 +0900

    feat(entity): User Entity update 메소드, 유효성 검증 메소드 추가
    - userName을 수정하는 updateUserName() 메소드 추가
    - userName, userEmail, password 값을 검사하는 validate 메소드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -7,10 +7,12 @@ public class User extends BaseEntity {
 
     public User(String userName, String userEmail, String password) {
         super();
+        validateUserName(userName);
+        validateUserEmail(userEmail);
+        validatePassword(password);
         this.userName = userName;
         this.userEmail = userEmail;
         this.password = password;
-
     }
 
     public String getUserName() {
@@ -21,8 +23,32 @@ public class User extends BaseEntity {
         return userEmail;
     }
 
-    public String getPassWord() {
+    public String getPassword() {
         return password;
     }
 
+    public void updateUserName(String newUserName) {
+        validateUserName(newUserName);
+        this.userName = newUserName;
+    }
+
+    private void validateUserName(String userName) {
+        if (userName == null || userName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Username 은 null 이거나 공백일 수 없다!!!");
+        }
+    }
+    private void validateUserEmail(String userEmail) {
+        if (userEmail == null || userEmail.trim().isEmpty()) {
+            throw new IllegalArgumentException("UserEmail 은 null 이거나 공백일 수 없다!!!");
+        }
+
+        // 이메일 패턴 검증 예외처리 추가로 필요 + 중복확인
+    }
+    private void validatePassword(String password) {
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("password 은 null 이거나 공백일 수 없다!!!");
+        }
+
+        // 비밀번호 패턴 검증 예외처리 추가로 필요
+    }
 }
