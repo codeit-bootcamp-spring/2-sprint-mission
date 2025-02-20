bbc0079007a8c8f72a5885f4b73301a0d1fc1c00
sangyeobhan <sangyeobhandev@gmail.com>
2025-02-20 10:09:33 +0900

    feat: Message 도메인 모델 정의 및 클래스 생성

---
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.entity;
+
+public class Message extends BaseEntity {
+    private User sender;
+    private String content;
+
+    public Message(User sender, String content) {
+        super();
+        setSender(sender);
+        setContent(content);
+    }
+
+    public void setSender(User sender) {
+        if (sender == null) {
+            throw new IllegalArgumentException("유효하지 않은 송신자입니다");
+        }
+        this.sender = sender;
+    }
+
+    public void setContent(String content) {
+        if (content == null || content.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 메세지입니다.");
+        }
+        this.content = content;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
