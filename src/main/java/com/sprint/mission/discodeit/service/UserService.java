f3757a20be019653469a9950ab33e707aead2a6f
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-02-20 16:10:22 +0900

    Feat: Add UserService interface with CRUD operations
    - 유저 서비스 인터페이스를 생성하고 기본적인 CRUD 기능을 추가

---
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+    // 유저 생성
+    void createUser(User user);
+
+    // 유저 읽기
+    // UUID id를 파라미터로 사용해 불러오기엔, UUID는 난수값이라 미리 알기가 힘들고 User DB를 따로 사용하지 않으므로 알길이 없음
+    // => username으로 User를 찾는게 훨씬 편할 것 같다.
+    User getUser(String username);
+
+    // 유저 모두 읽기
+    List<User> getAllUsers();
+
+    // 유저 수정
+    void updateUser(User user);
+
+    // 유저 삭제
+    void deleteUser(String username);
+
+}
