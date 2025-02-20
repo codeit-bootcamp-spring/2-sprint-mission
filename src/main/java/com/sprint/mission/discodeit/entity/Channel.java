9fa18e266aa9521dfdf1722f486230e47461dbb8
Leekb0804 <lkb8713@gmail.com>
2025-02-20 17:05:31 +0900

    feat(entity): Channel Entity update 메소드 추가
    - updateChannelName() 메소드 추가
    - addParticipant() 메소드 추가
    - addMessage() 메소드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -28,6 +28,31 @@ public class Channel extends BaseEntity {
         return messages;
     }
 
+    public void updateChannelName(String newChannelName) {
+        validChannelName(newChannelName);
+        this.channelName = newChannelName;
+        super.updateUpdatedAt();
+    }
+
+    public void addParticipant(User newParticipant) {
+        if (newParticipant == null) {
+            throw new IllegalArgumentException("newParticipant 는 null 이 될 수 없습니다!!!");
+        }
+        if (this.participants.contains(newParticipant)) {
+            throw new IllegalArgumentException("newParticipant 는 이미 채널에 참여 중 입니다!!! ");
+        }
+        this.participants.add(newParticipant);
+        super.updateUpdatedAt();
+    }
+
+    public void addMessage(Message newMessage) {
+        if (newMessage == null) {
+            throw new IllegalArgumentException("newMessage 는 null 이 될 수 없습니다!!!");
+        }
+        this.messages.add(newMessage);
+        super.updateUpdatedAt();
+    }
+
     private void validChannelName(String channelName) {
         if (channelName == null || channelName.trim().isEmpty()) {
             throw new IllegalArgumentException("channelName 은 null 이거나 공백일 수 없다!!!");
