4e3b930e947534f0475aa5ed20c5466266c0aa96
smthswt <hin19823@gmail.com>
2025-02-20 10:46:09 +0900

    feat: git clone, package 생성

---
+++ src/main/java/com/sprint/mission/discodeit/entity/ChattingRepository.java
@@ -0,0 +1,6 @@
+package com.sprint.mission.discodeit.entity;
+
+public interface ChattingRepository {
+    //[ ] id: 객체를 식별하기 위한 id로 UUID 타입으로 선언합니다.
+    //[ ] createdAt, updatedAt: 각각 객체의 생성, 수정 시간을 유닉스 타임스탬프로 나타내기 위한 필드로 Long 타입으로 선언합니다.
+}
