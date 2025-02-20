de9b3ea0e61cc0a2ba2d611c932f0b195c02318d
Jaeseok Heo <jasonheo1998@gmail.com>
2025-02-20 17:11:10 +0900

    JCF 유저 인터페이스 추가

--- src/main/java/com/sprint/mission/discodeit/service/CrudService.java
+++ src/main/java/com/sprint/mission/discodeit/service/CrudService.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 // 각 도메인(User,Channel,Message)의 CRUD(Create,Read,Update,Delete)는 공통적으로 필요
@@ -8,7 +9,7 @@ import java.util.UUID;
 
 public interface CrudService<T> {
     void create(T entity);
-    T read(UUID id);
+    Optional<T> read(UUID id); // 안전하게 처리하기 위해 null 값 출력 방지로 Nullpointer 오류 방지
     List<T> readAll();
     void update(UUID id, T entity);
     void delete(UUID id);
