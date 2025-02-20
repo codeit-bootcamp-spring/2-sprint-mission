89c4dbe1dc0dc4ef899b9c78a33c0ba03587fc8a
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-02-20 11:18:43 +0900

    Feat: User 도메인에 권한(roles) 필드 및 권한 추가/삭제 기능 구현
    - User 클래스에 권한을 나타내는 권한 필드 추가
    - 권한 추가(addRole) 메서드와 삭제(removeRole) 메서드 구현

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,21 +1,26 @@
 package com.sprint.mission.discodeit.entity;
 
+import javax.management.relation.Role;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class User extends Common{
     // 아이디 - 디스코드에서는 이메일 사용
-    private String username;
+    private final String username;
     // 비밀번호
     private String password;
     // 닉네임
     private String nickname;
+    // 권한 - 채널 여러개의 권한이 필요하므로 List로 생성
+    // 채널마다 관리자 권한명을 다르게 해줘야함 -> enum보단 String 사용?
+    private List<String> roles = new ArrayList<>();
 
     public User(String username, String password, String nickname) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
     }
-
 
     public String getUsername() {
         return username;
@@ -37,5 +42,15 @@ public class User extends Common{
 
     public void updateNickname(String nickname) {
         this.nickname = nickname;
+    }
+
+    // 권한 추가
+    public void addRole(String role) {
+        roles.add(role);
+    }
+
+    //권한 삭제
+    public void removeRole(String role) {
+        roles.remove(role);
     }
 }
