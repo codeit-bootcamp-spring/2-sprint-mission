405afc69e82c41a43ff2d8680f22fc09024d725e
Leekb0804 <lkb8713@gmail.com>
2025-02-20 16:25:14 +0900

    feat(entity): Channel Entity 필드 및 메소드 추가 및 BaseEntity 상속
    - BaseEntity 상속
    - channelName, participants, messages 필드 추가
    - 생성자 추가
    - getter 메소드 추가
    - channelName 유효성 검증 메소드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,4 +1,36 @@
 package com.sprint.mission.discodeit.entity;
 
-public class Channel {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Channel extends BaseEntity {
+    private String channelName;
+    private Set<User> participants;
+    private List<Message> messages;
+
+    public Channel(String channelName) {
+        this.channelName = channelName;
+        participants = new TreeSet<User>();
+        messages = new ArrayList<>();
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public Set<User> getParticipants() {
+        return participants;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    private void validChannelName(String channelName) {
+        if (channelName == null || channelName.trim().isEmpty()) {
+            throw new IllegalArgumentException("channelName 은 null 이거나 공백일 수 없다!!!");
+        }
+    }
 }
