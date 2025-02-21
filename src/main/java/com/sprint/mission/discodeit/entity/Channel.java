dee006d873562d3fa4df09c03589e4479b1eb902
정종현 <bellhyunforgithub@gmail.com>
2025-02-21 10:24:41 +0900

    Refactor: Refactoring entities to BaseEntity
    -(User, Channel, Message) extends BaseEntity
    
    -공통 속성 BaseEntity로 리팩토링
    
    -Override toString()

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,41 +1,29 @@
 package com.sprint.mission.discodeit.entity;
 
-import java.util.UUID;
-
-public class Channel {
-    private final UUID id;
+public class Channel extends BaseEntity {
     private String name;
-    private final Long createdAt;
-    private Long updatedAt;
 
     public Channel(String name) {
+        super();
         this.name = name;
-        this.id = UUID.randomUUID();
-        this.createdAt = System.currentTimeMillis();
-        this.updatedAt = createdAt;
     }
 
     public String getName() {
         return name;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public Long getCreatedAt() {
-        return createdAt;
-    }
-
-    public Long getUpdatedAt() {
-        return updatedAt;
-    }
-
     public void setName(String name) {
         this.name = name;
+        setUpdatedAt();
     }
 
-    public void setUpdatedAt(Long updatedAt) {
-        this.updatedAt = updatedAt;
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
     }
 }
--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,41 +1,29 @@
 package com.sprint.mission.discodeit.entity;
 
-import java.util.UUID;
-
-public class Message {
-    private final UUID id;
+public class Message extends BaseEntity{
     private String text;
-    private final Long createdAt;
-    private Long updatedAt;
+
+    public Message(String text) {
+        super();
+        this.text = text;
+    }
 
     public String getText() {
         return text;
     }
 
-    public Message(String text) {
-        this.text = text;
-        this.id = UUID.randomUUID();
-        this.createdAt = System.currentTimeMillis();
-        this.updatedAt = createdAt;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public Long getCreatedAt() {
-        return createdAt;
-    }
-
-    public Long getUpdatedAt() {
-        return updatedAt;
-    }
-
     public void setText(String text) {
         this.text = text;
+        setUpdatedAt();
     }
 
-    public void setUpdatedAt(Long updatedAt) {
-        this.updatedAt = updatedAt;
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
     }
 }
