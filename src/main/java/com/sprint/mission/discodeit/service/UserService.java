3734f6e77baa925b79572949cb2b19582b965fdd
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-02-21 11:51:17 +0900

    UserService 인터페이스 추가 (CRUD 작업 포함)

---
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -0,0 +1,20 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    // CRUD(생성, 읽기, 모두 읽기, 수정, 삭제)
+    void createUser(String username); //유저 생성
+
+    User getUser(String username);  //유저 조회
+    List<User> getAllUsers();   //모든 유저 조회
+
+    void updateUsername(User user, String newUsername);   //유저 이름 변경
+    void addChannel(User user, Channel channel);  //유저 채널 추가
+
+    void deleteUser(User user);   //유저 삭제
+    void deleteChannel(User user, Channel channel);   //유저 채널 삭제
+}
