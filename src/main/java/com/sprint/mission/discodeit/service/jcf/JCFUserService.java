263e81f71521ef92a9c358f49050938e27b3c803
Leekb0804 <lkb8713@gmail.com>
2025-02-21 09:41:30 +0900

    feat(service): JCFUserService 추가
    - UserService 인터페이스 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -0,0 +1,44 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class JCFUserService implements UserService {
+    private final UserRepository userRepository;
+
+    public JCFUserService() {
+        this.userRepository = new UserRepository();
+    }
+
+    public void createUser(User newUser) {
+        this.userRepository.addUser(newUser);
+    }
+
+    public User readUser(UUID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("null 인 id 값이 들어왔습니다!!!");
+        }
+        return userRepository.findUserById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 id를 가진 사용자가 존재하지 않습니다!!!"));
+    }
+
+    public Set<User> readAllUsers() {
+        return userRepository.getUsers();
+    }
+
+    public void updateUserName(UUID id, String newUserName) {
+        readUser(id).updateUserName(newUserName);
+    }
+
+    public void updatePassword(UUID id, String newPassword) {
+        readUser(id).updateUserPassword(newPassword);
+    }
+
+    public void deleteUser(UUID id) {
+        userRepository.deleteUser(id);
+    }
+}
