806b564f97e2a0fc0aa07b132df67c732ea77faa
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-02-21 16:22:16 +0900

    Refactor: Change channelName field to final
    - Channel 클래스의 channelName 필드에 final 키워드 추가
    - channelName을 ID 값으로 사용하기로 결정하여 불변성을 보장하도록 변경  (UUID 문제)
    - changeChannelName 메서드 삭제 (채널명 변경 불가능하도록 수정)

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -9,7 +9,9 @@ public class Channel extends Common {
     // 해당 채널의 주인
     private final User user;
     // 채널명
-    private String channelName;
+    // 채널명을 바꿀 수 있게 하고싶었으나, id값은 UUID 타입이라 DB가 없는 지금은 마음대로 조회하기 힘들 것 같아서
+    // channelName을 바꿀 수 없게 만들고 중복 불가능하게 만들어서 id값 대신 사용할 예정 (괜찮을까요?)
+    private final String channelName;
     // 해당 채널의 유저리스트
     private List<User> users = new ArrayList<>();
 
@@ -27,11 +29,6 @@ public class Channel extends Common {
 
     public List<User> getUsers() {
         return users;
-    }
-
-    // 채널명 수정
-    public void updateChannelName(String channelName) {
-        this.channelName = channelName;
     }
 
     // 유저 추가 - 유저가 채널에 들어올 경우
