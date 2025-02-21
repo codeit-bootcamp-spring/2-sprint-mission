9773ee98293385593407c1886371daadfcad7f97
정종현 <bellhyunforgithub@gmail.com>
2025-02-21 16:41:29 +0900

    Refactor: Refactoring 구현체
    -인터페이스 수정에 따른 save 메서드 수정
    
    -출력 포멧 toString으로 간소화
    
    -stream API 도입

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -2,66 +2,55 @@ package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.service.ChannelService;
-import com.sprint.mission.discodeit.service.TimeFormatter;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public class JCFChannelService implements ChannelService {
+    private final Map<UUID, Channel> channelRepository;
 
-    Map<UUID, Channel> channelRepository = new HashMap<>();
+    public JCFChannelService() {
+        this.channelRepository = new HashMap<>();
+    }
 
     @Override
-    public void saveChannel(Channel channel) {
+    public Channel saveChannel(String name) {
+        Channel channel = new Channel(name);
         channelRepository.put(channel.getId(), channel);
+        return channel;
     }
 
     @Override
     public void findAll() {
-        List<Channel> channelList = channelRepository.values().stream().toList();
-
-        if(channelList.isEmpty()){
-            System.out.println("등록된 채널이 없습니다.");
-        }else {
-            for (Channel channel : channelList) {
-                System.out.println("채널 ID: " + channel.getId());
-                System.out.println("채널 이름: " + channel.getName());
-                System.out.println("채널 생성 일자: " + TimeFormatter.format(channel.getCreatedAt()));
-                System.out.println("채널 수정 일자: " + TimeFormatter.format(channel.getUpdatedAt()));
-                System.out.println("----------------------------------");
-            }
-        }
+        channelRepository.values().stream()
+                .findFirst()
+                .ifPresentOrElse(
+                        channel -> channelRepository.values().forEach(System.out::println),
+                        () -> System.out.println("등록된 채널이 없습니다.")
+                );
     }
 
     @Override
     public void findByName(String name) {
-        List<Channel> channelList = channelRepository.values().stream()
-                                .filter(channel -> channel.getName()
-                                        .equalsIgnoreCase(name)).toList();
-
-        if(channelList.isEmpty()){
-            System.out.println("이름이 " + name + " 인 채널이 없습니다.");
-        }else{
-            System.out.println("[" + name + "]" + " 채널 검색 결과");
-            for (Channel channel : channelList) {
-                System.out.println("채널 ID: " + channel.getId());
-                System.out.println("채널 이름: " + channel.getName());
-                System.out.println("채널 생성 일자: " + TimeFormatter.format(channel.getCreatedAt()));
-                System.out.println("채널 수정 일자: " + TimeFormatter.format(channel.getUpdatedAt()));
-                System.out.println("----------------------------------");
-            }
-        }
+        channelRepository.values().stream()
+                .filter(channel -> channel.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .ifPresentOrElse(
+                        channel -> System.out.println(channel),
+                        () -> System.out.println(name + "으로 등록된 채널이 없습니다.")
+                );
     }
 
     @Override
     public void update(UUID id, String name) {
-        if(!channelRepository.containsKey(id)){
-            System.out.println("없는 채널 입니다.");
-        }
-        channelRepository.get(id).setName(name);
-        channelRepository.get(id).setUpdatedAt(System.currentTimeMillis());
+        Optional<Channel> channel = Optional.ofNullable(channelRepository.get(id));
+
+        channel.ifPresentOrElse(
+                ch -> ch.setName(name),
+                () -> System.out.println("해당 id로 등록된 채널이 없습니다.")
+        );
     }
 
     @Override
