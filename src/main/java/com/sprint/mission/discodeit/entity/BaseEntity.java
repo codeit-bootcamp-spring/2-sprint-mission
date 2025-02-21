f5986803e7120bda1b9ff8f39cb2eea8952c7257
ddday366 <ddday366@gmail.com>
2025-02-21 14:02:46 +0900

    feat: 공통 엔티티(BaseEntity) 추가

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -1,4 +1,27 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.util.UUID;
+
 public class BaseEntity {
+    protected final UUID id;
+    protected Long createAt;
+    protected Long updateAt;
+
+    public BaseEntity(UUID id, Long createAt, Long updateAt) {
+        this.id = id;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Long getCreateAt() {
+        return createAt;
+    }
+
+    public Long getUpdateAt() {
+        return updateAt;
+    }
 }
