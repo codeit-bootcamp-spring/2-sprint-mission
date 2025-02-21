0af0c1b793b0e759d83d7e105106ea4170a0eac4
Leekb0804 <lkb8713@gmail.com>
2025-02-21 15:24:54 +0900

    feat(repository): ChannelRepository 추가
    - 채널 생성 및 저장 기능 구현 (addChannel)
    - 채널 ID로 조회 기능 추가 (findChannelById)
    - 채널 삭제 기능 추가 (deleteChannel)
    - 특정 채널에 참가자 추가 기능 구현 (addParticipant)
    - 데이터 관리를 위해 ArrayDeque 활용

---
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -0,0 +1,41 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.ArrayDeque;
+import java.util.Objects;
+import java.util.UUID;
+
+public class ChannelRepository {
+    ArrayDeque<Channel> channels;
+
+    public ArrayDeque<Channel> getChannels() {
+        return channels;
+    }
+
+    public ChannelRepository() {
+        this.channels = new ArrayDeque<>();
+    }
+
+    public void addChannel(Channel channel) {
+        this.channels.addFirst(channel);
+    }
+
+    public Channel findChannelById(UUID channelId) {
+        return channels.stream()
+                .filter(channel -> Objects.equals(channel.getId(), channelId))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("해당 channelId를 가진 channel 을 찾을 수 없다!!!"));
+    }
+
+    public void addParticipant(UUID channelId, User newParticipant) {
+        findChannelById(channelId).addParticipant(newParticipant);
+    }
+
+    public void deleteChannel(UUID channelId) {
+        boolean removed = channels.removeIf(channel -> Objects.equals(channel.getId(), channelId));
+        if (!removed) {
+            throw new IllegalArgumentException("삭제하려는 channelId가 존재하지 않습니다!!!");
+        }
+    }
+}
