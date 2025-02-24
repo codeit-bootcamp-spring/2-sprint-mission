d2931556af2fc797c3d77386500abf2d9c7ab8ed
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-02-24 11:38:26 +0900

    Refactor: Update MessageService Methods Parameters
    - MessageService의 deleteMessage, getMessage 파라미터를 UUID id로 변경
    - MessageService의 updateMessage 파라미터에 UUID id로 추가

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -11,15 +11,16 @@ public interface MessageService {
 
     // 메시지 읽기
     // UUID id를 파라미터로 사용해 불러오기엔, UUID는 난수값이라 미리 알기가 힘들고 Message DB를 따로 사용하지 않으므로 알길이 없음
-    // => content 으로 Message를 찾는게 훨씬 편할 것 같다.
-    Message getMessage(String content);
+    // 그렇다고 content로 가져오기엔 content는 수정도 가능해야하고 중복도 가능해야함
+    // 일단, 여기서는 해당 객체에서 UUID를 추출해서 그걸로 가져와보자.
+    Message getMessage(UUID id);
 
     // 메시지 모두 읽기
     List<Message> getAllMessages();
 
     // 메시지 수정
-    void updateMessage(Message message);
+    void updateMessage(UUID id, Message message);
 
     // 메시지 삭제
-    void deleteMessage(String content);
+    void deleteMessage(UUID id);
 }
