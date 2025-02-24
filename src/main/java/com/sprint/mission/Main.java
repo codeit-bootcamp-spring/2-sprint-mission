632d32f0b87da3d3cee9d13052c3a7815875bb2f
leejunnyeop <daffodil8620@naver.com>
2025-02-24 22:26:39 +0900

    [refactor] 입력 구분하기
    case 를 사용하여, 입력을 choice 하게 한다. 그리고 0을 입력할때 까지 프로그램을 계속 실행 시킨다.

--- src/main/java/com/sprint/mission/Main.java
+++ src/main/java/com/sprint/mission/Main.java
@@ -4,17 +4,49 @@ import com.sprint.mission.service.UserService;
 import com.sprint.mission.service.UserServiceImpl;
 import com.sprint.mission.view.input.UserInput;
 import com.sprint.mission.view.output.UserOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-
         UserOutput userOutput = new UserOutput();
-
         UserService userService = new UserServiceImpl(userOutput);
-
         UserInput userInput = new UserInput(userService);
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n원하는 기능을 선택하세요:");
+            System.out.println("1. 회원가입");
+            System.out.println("2. 이메일로 회원 조회");
+            System.out.println("3. 회원 전체 조회");
+            System.out.println("4. 회원 정보 수정");
+            System.out.println("0. 프로그램 종료");
+            System.out.print("> ");
+
+            String choice = scanner.nextLine().trim();
+
+            switch (choice) {
+                case "1":
+                    userInput.creatInput(); // 회원가입 실행
+                    break;
+                case "2":
+                    userInput.getEmailInput(); // 이메일로 조회
+                    break;
+                case "3":
+                    userInput.getAllInput(); // 전체 회원 조회 실행
+                    break;
+                case "4":
+                    userInput.updateInput(); // 회원 정보 수정 실행
+                    break;
+                case "0":
+                    System.out.println("프로그램을 종료합니다.");
+                    System.exit(0);
+                default:
+                    System.out.println("올바른 선택이 아닙니다. 다시 입력해주세요.");
+            }
+        }
     }
+
 }
