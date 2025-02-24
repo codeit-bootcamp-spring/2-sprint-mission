8f68fbeaaf247c245580f987d31c66cf605f7f69
leejunnyeop <daffodil8620@naver.com>
2025-02-24 15:26:14 +0900

    [test] 회원 가입: 입력 받기로 테스트

--- src/main/java/com/sprint/mission/Main.java
+++ src/main/java/com/sprint/mission/Main.java
@@ -1,7 +1,21 @@
 package com.sprint.mission;
 
+import com.sprint.mission.controller.UserService;
+import com.sprint.mission.controller.UserServiceImpl;
+import com.sprint.mission.view.input.UserInput;
+import com.sprint.mission.view.output.UserOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        UserOutput userOutput = new UserOutput();
+
+        UserService userService = new UserServiceImpl(userOutput);
+
+        UserInput userInput = new UserInput(userService);
+
+        userInput.creatInput();
     }
 }
