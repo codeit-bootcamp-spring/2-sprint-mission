6c17496f912818f79451851a847d51f295cd8e11
KimSeeun <9035anny@naver.com>
2025-02-25 16:28:46 +0900

    도메인 모델링 구현

---
+++ src/main/java/com/sprint/mission/discodeit/entity/ChannelEntity.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChannelEntity extends BaseEntity {
+    private String name;
+    private String type;
+
+    public ChannelEntity(String name, String type) {
+        super();
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() { return name;}
+    public String getType() { return type;}
+
+    public void updateChannelName(String newName) {
+        this.name = newName;
+    }
+
+    public void updateChannelType(String newType) {
+        if (!newType.equals("text")&& !newType.equals("voice")) {
+            throw new IllegalArgumentException("유효한 채널 유형이 아닙니다.");
+        }
+        this.type = newType;
+    }
+
+    public static void addChannel(List<ChannelEntity> channelList, ChannelEntity newChannel) {
+        if (!channelList.contains(newChannel)) {
+            channelList.add(newChannel);
+        }
+    }
+
+    public static void deleteChannel(List<ChannelEntity> channelList, ChannelEntity channel) {
+        channelList.remove(channel);
+    }
+}
