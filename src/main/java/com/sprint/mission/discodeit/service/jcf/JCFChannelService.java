92818483c8d7d30ae159c252b880315a6169a47d
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-02-25 10:22:53 +0900

    Feat: ChannelService 구현체 JCFChannelService 구현 완료

---
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.*;
+
+public class JCFChannelService implements ChannelService {
+
+    private final Map<UUID, Channel> data;
+
+    public JCFChannelService() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public void create(Channel channel) {
+        data.put(channel.getId(), channel);
+    }
+
+    @Override
+    public Channel findById(UUID channelId) {
+        return data.get(channelId);
+    }
+
+    @Override
+    public List<Channel> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void delete(UUID id) {
+        data.remove(id);
+    }
+
+    @Override
+    public void update(UUID id, String name) {
+        if(data.containsKey(id)) {
+            Channel channel = data.get(id);
+            channel.setName(name);
+        }
+    }
+}
