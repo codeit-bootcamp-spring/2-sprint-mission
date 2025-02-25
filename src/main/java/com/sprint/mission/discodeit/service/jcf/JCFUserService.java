4d3ba3c99c689f2dd8d6ab3554ad6f8998d2fbc5
JaeKwan AN <jaekwan99@gmail.com>
2025-02-25 15:26:59 +0900

    Feat: JCFUserService 구현(CRUD 기능 추가)

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -1,4 +1,49 @@
 package com.sprint.mission.discodeit.service.jcf;
 
-public class JCFUserService {
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class JCFUserService implements UserService {
+    private final Map<UUID, User> data = new HashMap<>();
+
+    @Override
+    public User createUser(String name) {
+        User user = new User(name);
+        data.put(user.getId(), user);  // id를 키로, User 객체를 맵에 저장
+        return user;
+    }
+
+    @Override
+    public Optional<User> getUserById(UUID userid) {
+        return Optional.ofNullable(data.get(userid));
+    }
+
+    @Override
+    public List<User> getUsersByName(String name) {
+        return data.values().stream()
+                .filter(user -> user.getName().equals(name))  // 이름이 같은 사용자 필터링
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void updateUser(UUID userId, String newName) {
+        User user = data.get(userId);  // 해당 Id의 User 객체 가져오기
+        if (user != null) {
+            user.update(newName);
+        }
+    }
+
+    @Override
+    public void deleteUser(UUID userId) {
+        data.remove(userId);
+    }
+
 }
