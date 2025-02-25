520388aa712d13b3d4a357a653e80a11504a4caa
JongwonLee <gudtksfl@gmail.com>
2025-02-25 15:39:12 +0900

    refactor: User객체, service, JCFUserService 이동 및 구조 변경

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -3,5 +3,14 @@ package com.sprint.mission.discodeit.service;
 import com.sprint.mission.discodeit.entity.User;
 
 public interface UserService {
-
+    public abstract void currentHead();
+    public abstract void createServer(String name);
+    public abstract void createChannel(String name);
+    public abstract void printServer();
+    public abstract void printChannel();
+    public abstract void replaceHead();
+    public abstract void removeServer(String targetName);
+    public abstract void removeChannel(String targetName);
+    public abstract void updateServer(String targetName, String replaceName);
+    public abstract void updateChannel(String targetName, String replaceName);
 }
