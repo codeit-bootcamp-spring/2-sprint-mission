61ad0b704a19719cac7e4c32fa571d7290ecc898
Leekb0804 <lkb8713@gmail.com>
2025-02-25 15:13:48 +0900

    feat(service): JCFChannelService에 messageRepository 추가 및 readMessageListByChannelId 구현
    - messageRepository 필드 추가하여 메시지 데이터 조회 기능 확장
    - 특정 채널에 속한 메시지 목록을 조회하는 readMessageListByChannelId(UUID channelId) 메서드 구현

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -1,18 +1,23 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 
 import java.util.ArrayDeque;
+import java.util.List;
 import java.util.UUID;
 
 public class JCFChannelService implements ChannelService {
     private final ChannelRepository channelRepository;
+    private final MessageRepository messageRepository;
 
-    public JCFChannelService(ChannelRepository channelRepository) {
+    public JCFChannelService(ChannelRepository channelRepository, MessageRepository messageRepository) {
         this.channelRepository = channelRepository;
+        this.messageRepository = messageRepository;
     }
 
     @Override
@@ -34,6 +39,17 @@ public class JCFChannelService implements ChannelService {
     @Override
     public ArrayDeque<Channel> readAllChannels() {
         return channelRepository.getChannels();
+    }
+
+    @Override
+    public List<Message> readMessageListByChannelId(UUID channelId) {
+        if (channelId == null) {
+            throw new IllegalArgumentException("channelId is null!!!");
+        }
+        if (!channelRepository.existsById(channelId)) {
+            throw new IllegalArgumentException("channelId is not exists!!!");
+        }
+        return messageRepository.findMessageListByChannelId(channelId);
     }
 
     @Override
