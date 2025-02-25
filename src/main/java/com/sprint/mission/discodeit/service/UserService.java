5664e3f7f5475f133786f52d016862c94df129c4
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-02-25 10:13:16 +0900

    Feat: 서비스 인터페이스 설계

---
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -0,0 +1,14 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+    void create(User user);
+    User findById(UUID id);
+    List<User> findAll();
+    void delete(UUID id);
+    void update(UUID id, String nickname);
+}
