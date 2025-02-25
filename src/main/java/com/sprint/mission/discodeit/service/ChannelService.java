17de3cf53d33cc880170131c4fa38efd44e44d3f
Leekb0804 <lkb8713@gmail.com>
2025-02-25 15:10:56 +0900

    feat(service): ChannelService 인터페이스에 readMessageListByChannelId 메서드 추가
    - 특정 채널에 속한 메시지 목록을 조회하는 기능 정의

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,15 +1,18 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 
 import java.util.ArrayDeque;
+import java.util.List;
 import java.util.UUID;
 
 public interface ChannelService {
     void createChannel(Channel newChannel);
     Channel readChannel(UUID channelId);
     ArrayDeque<Channel> readAllChannels();
+    List<Message> readMessageListByChannelId(UUID channelId);
     void updateChannelName(UUID channelId, String newChannelName);
     void addChannelParticipant(UUID channelId, User newParticipant);
     void deleteChannel(UUID channelId);
