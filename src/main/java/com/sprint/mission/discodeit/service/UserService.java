a90dfa3192730a0c59c008b70abd7c236cdb59e1
Leekb0804 <lkb8713@gmail.com>
2025-02-26 17:13:31 +0900

    refactor(service): UserService 인터페이스의 createUser 메서드 시그니처 변경 및 validateUserId 추가
    - createUser(User newUser) → createUser(String userName, String userEmail, String password)로 변경
      - 사용자 생성 시 DTO가 아닌 개별 필드(userName, userEmail, password)만 전달하도록 변경
    
    - validateUserId(UUID userId, UserRepository userRepository) 정적 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -2,16 +2,23 @@ package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.UserCreateRequest;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
 
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 public interface UserService {
-    void createUser(User newUser);
+    void createUser(String userName, String userEmail, String password);
     User readUser(UUID id);
     Set<User> readAllUsers();
     void updateUserName(UUID id, String newUserName);
     void updatePassword(UUID id, String newPassword);
     void deleteUser(UUID id);
+    static void validateUserId(UUID userId, UserRepository userRepository) {
+        if (userId == null) {
+            throw new IllegalArgumentException("null 인 id 값이 들어왔습니다!!!");
+        }
+        userRepository.findUserById(userId);
+    }
 }
