cc7ec32c2a9d7323320e5731a73f3a986d73ba1c
Leekb0804 <lkb8713@gmail.com>
2025-02-26 17:50:28 +0900

    feat(service): createMessage() 메서드 시그니처 변경 및 ChannelService에 validateChannelId() 메서드 추가
    - Message 객체를 직접 생성하는 방식에서 개별 요소를 전달하는 방식으로 수정
    - static 메서드 validateMessageId(UUID channelId, MessageRepository messageRepository) 추가
    - 서비스 레이어에서 채널 ID 검증을 일관되게 수행하도록 개선

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -1,14 +1,23 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
-    void createMessage(Message newMessage);
+    void createMessage(User sender, String content, Channel channel);
     Message readMessage(UUID messageId);
     List<Message> readAllMessages();
     void updateMessageContent(UUID messageId, String content);
     void deleteMessage(UUID messageId);
+    static void validateMessageId(UUID messageId, MessageRepository messageRepository) {
+        if (messageId == null) {
+            throw new IllegalArgumentException("입력받은 messageId 가 null 입니다!!!");
+        }
+        messageRepository.findMessageByMessageId(messageId);
+    }
 }
