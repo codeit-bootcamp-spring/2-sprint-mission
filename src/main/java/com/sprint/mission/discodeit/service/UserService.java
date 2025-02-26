e466fa922143604f94731a2d92d58d88dfb761c7
JongwonLee <gudtksfl@gmail.com>
2025-02-26 15:18:14 +0900

    refactor: UserService 중 Server 생성, 추가, 삭제, 업데이트 기능 구현 완료

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,15 +1,36 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.Repository.UserRepository;
 import com.sprint.mission.discodeit.entity.Container.Channel;
 import com.sprint.mission.discodeit.entity.Server;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface UserService {
     public abstract Server createServer(String name);
+
+    public abstract void addServer(UUID userId, String name);
+
+    public abstract void addServer(UUID userId, Server server);
+
+    public abstract Server getServer(UUID userId, String name);
+
+    public abstract void printServer(UUID userId);
+
+    public abstract void printServer(List<Server> list);
+
+    public abstract boolean removeServer(UUID userId, String targetName);
+
+    public abstract boolean updateServer(UUID userId, String targetName, String replaceName);
+
     public abstract Channel createChannel(String name);
-    public abstract void printServer();
-    public abstract void printChannel();
-    public abstract void removeServer(String targetName);
-    public abstract void removeChannel(String targetName);
-    public abstract void updateServer(String targetName, String replaceName);
-    public abstract void updateChannel(String targetName, String replaceName);
+
+    public abstract void addChannel(UUID userId, Channel Channel);
+
+    public abstract void printChannel(UUID id);
+
+    public abstract boolean removeChannel(String targetName);
+
+    public abstract boolean updateChannel(String targetName, String replaceName);
 }
--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -3,10 +3,25 @@ package com.sprint.mission.discodeit.service.jcf;
 import com.sprint.mission.discodeit.Factory.CreateChannalFactory;
 import com.sprint.mission.discodeit.Factory.CreateServerFactory;
 import com.sprint.mission.discodeit.Factory.Factory;
+import com.sprint.mission.discodeit.Repository.UserRepository;
+import com.sprint.mission.discodeit.Repository.impl.LinkedListUserRepository;
 import com.sprint.mission.discodeit.entity.Container.Channel;
 import com.sprint.mission.discodeit.entity.Server;
 import com.sprint.mission.discodeit.service.UserService;
 
+import java.util.*;
+
+/**
+ * <h3>유저 서비스 구현체 </h3><p>
+ * 서버와 채널을 생성, 삭제, 추가, 업데이트 하는 기능을 수행한다. <br>
+ * 유저마다 동일한 기능을 수행해야 한다. <br>
+ * 유저마다 유저 레포지토리는 달라야한다.<br>
+ * 이를 위해서 Map<유저 ID,유저 레포지토리>를 생성한다.<br>
+ * </p>
+ *
+ * @version 1
+ * @JongwonLee
+ */
 public class JCFUserService implements UserService {
     private static JCFUserService instance;
 
@@ -20,36 +35,137 @@ public class JCFUserService implements UserService {
         return instance;
     }
 
+    private final Map<UUID, UserRepository> userTable = new HashMap<>();
+
+    //레포지토리 생성
+    private UserRepository getUserRepository(UUID id) {
+        UserRepository userRepository = userTable.get(id);
+        if (userRepository == null) {
+            userTable.put(id, new LinkedListUserRepository());
+        }
+        return userRepository;
+    }
+
     public Server createServer(String name) {
-        Factory<Server> factory = CreateServerFactory.getInstance();
-        Server server = factory.create(name);
-        System.out.println("서버 생성 성공");
-        return server;
+        return CreateServerFactory.getInstance().create(name);
     }
 
     public Channel createChannel(String name) {
-        Factory<Channel> factory = CreateChannalFactory.getInstance();
-        Channel channel = factory.create(name);
-        System.out.println("채널 생성 성공");
-        return channel;
+        return CreateChannalFactory.getInstance().create(name);
     }
 
-    public void printServer() {
+    @Override
+    public void addServer(UUID userId, String name) {
+        UserRepository userRepository = getUserRepository(userId);
+        Server server = createServer(name);
+        userRepository.add(server);
+
+        //로그
+        System.out.println(server.getName() + " 서버 추가 성공");
     }
 
-    public void printChannel() {
+    @Override
+    public void addServer(UUID userId, Server server) {
+        UserRepository userRepository = getUserRepository(userId);
+        userRepository.add(server);
+
+        //로그
+        System.out.println(server.getName() + " 서버 추가 성공");
     }
 
-    public void removeServer(String targetName) {
+    @Override
+    public Server getServer(UUID userId, String name) {
+        UserRepository userRepository = getUserRepository(userId);
+        List<Server> list = userRepository.getList();
+        for (Server server : list) {
+            if (server.getName().equals(name)) {
+                //로그
+                System.out.println(server.getName() + " 이(가) 반환됩니다.");
+                return server;
+            }
+        }
+        //로그
+        System.out.println("존재하지 않습니다.");
+        return null;
     }
 
-    public void removeChannel(String targetName) {
+    @Override
+    public void printServer(UUID userId) {
+        UserRepository userRepository = getUserRepository(userId);
+        List<Server> list = userRepository.getList();
+        System.out.println("=========서버 목록==========");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i + 1 + " : " + list.get(i).getName());
+        }
+        System.out.println("=========================");
     }
 
-    public void updateServer(String targetName, String replaceName) {
+    @Override
+    public void printServer(List<Server> list) {
+        System.out.println("=========서버 목록==========");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i + 1 + " : " + list.get(i).getName());
+        }
+        System.out.println("=========================");
     }
 
-    public void updateChannel(String targetName, String replaceName) {
+    @Override
+    public boolean removeServer(UUID userId, String targetName) {
+        UserRepository userRepository = getUserRepository(userId);
+        List<Server> list = userRepository.getList();
+        for (Server server : list) {
+            if (server.getName().equals(targetName)) {
+                //로그
+                System.out.println(server.getName() + " 이(가) 삭제됩니다.");
+                list.remove(server);
+                return true;
+            }
+        }
+        System.out.println("존재하지 않습니다.");
+        return false;
     }
 
+    @Override
+    public boolean updateServer(UUID userId, String targetName, String replaceName) {
+        UserRepository userRepository = getUserRepository(userId);
+        List<Server> list = userRepository.getList();
+        for (Server server : list) {
+            if (server.getName().equals(targetName)) {
+                server.setName(replaceName);
+                //로그
+                System.out.println(server.getName() + " 이(가) 됩니다.");
+                return true;
+            }
+        }
+        System.out.println("존재하지 않습니다.");
+        return false;
+    }
+
+    @Override
+    public void addChannel(UUID userId, Channel Channel) {
+        UserRepository userRepository = getUserRepository(userId);
+        List<Server> list = userRepository.getList();
+        printServer(list);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("추가할 서버의 번호를 입력하시오. : ");
+        int i = sc.nextInt();
+        sc.nextLine();
+        Server server = list.get(i);
+
+    }
+
+    @Override
+    public void printChannel(UUID userId) {
+
+    }
+
+    @Override
+    public boolean removeChannel(String targetName) {
+        return false;
+    }
+
+    @Override
+    public boolean updateChannel(String targetName, String replaceName) {
+        return false;
+    }
 }
