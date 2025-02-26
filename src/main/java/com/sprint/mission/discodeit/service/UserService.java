a4e7d38702b3374cc415fb42c6a56b69e5c5cace
ChoiMiJin <choimi2230@gmail.com>
2025-02-26 21:40:25 +0900

    누락파일

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -6,8 +6,6 @@ import com.sprint.mission.discodeit.entity.User;
 import java.util.UUID;
 
 public interface UserService {
-    //어떤 기능이 있는지 추상적으로
-    //생성
     void createUser(String username, Channel channel);
     User findByUserId(UUID userId);
     UUID findByUsername(String username);
