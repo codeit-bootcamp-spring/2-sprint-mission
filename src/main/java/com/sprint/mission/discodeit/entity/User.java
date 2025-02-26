5a80a5e42b3204a76246d5608739bc5ec3fbb298
Leekb0804 <lkb8713@gmail.com>
2025-02-26 17:10:59 +0900

    refactor(entity): User 엔티티의 검증 메서드를 static 메서드로 변경
    - validateUserName, validateUserEmail, validatePassword 메서드를 static으로 변경
    - 기존 private 접근 제어자를 public static으로 수정하여 외부에서도 재사용 가능하도록 개선

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -39,19 +39,19 @@ public class User extends BaseEntity implements Comparable<User> {
         super.updateUpdatedAt();
     }
 
-    private void validateUserName(String userName) {
+    public static void validateUserName(String userName) {
         if (userName == null || userName.trim().isEmpty()) {
             throw new IllegalArgumentException("Username 은 null 이거나 공백일 수 없다!!!");
         }
     }
-    private void validateUserEmail(String userEmail) {
+    public static void validateUserEmail(String userEmail) {
         if (userEmail == null || userEmail.trim().isEmpty()) {
             throw new IllegalArgumentException("UserEmail 은 null 이거나 공백일 수 없다!!!");
         }
 
         // 이메일 패턴 검증 예외처리 추가로 필요 + 중복확인
     }
-    private void validatePassword(String password) {
+    public static void validatePassword(String password) {
         if (password == null || password.trim().isEmpty()) {
             throw new IllegalArgumentException("password 은 null 이거나 공백일 수 없다!!!");
         }
@@ -84,7 +84,7 @@ public class User extends BaseEntity implements Comparable<User> {
     }
 
     @Override
-    public int compareTo(User other) {
+    public int compareTo(User other) {      // 아직 사용하지 않는듯? 내가 왜 구현해놨는지 모르겠다
         return this.userName.compareTo(other.userName);
     }
 }
