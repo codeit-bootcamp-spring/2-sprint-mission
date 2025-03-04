4f7a6adeca4418081d46c69f5f90bc6d40df2300
Jaeseok Heo <jasonheo1998@gmail.com>
2025-03-04 17:05:54 +0900

    MessageRepository 인터페이스 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -0,0 +1,16 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MessageRepository {
+    void save(Message message);
+    Optional<Message> findById(UUID id);
+    List<Message> findAll();
+    void update(UUID id, User user);
+    void delete(UUID id);
+}
