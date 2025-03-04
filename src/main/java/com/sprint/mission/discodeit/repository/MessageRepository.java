c7f3a29608f02701f4d80aac4c0b0a53784e4bcb
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-03-04 18:40:16 +0900

    feat: 레포지토리 인터페이스 설계

---
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -0,0 +1,15 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Message;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MessageRepository {
+    void save(Message message);
+    Optional<Message> findById(UUID messageId);
+    List<Message> findAll();
+    void delete(UUID id);
+    void update(UUID id, String content);
+}
