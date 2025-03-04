f1f01e59d8c041f1c06b9db2e82c0d1797e84e5e
Jaeseok Heo <jasonheo1998@gmail.com>
2025-03-04 17:31:53 +0900

    FileMessageRepository 구현 완료

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -0,0 +1,75 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.io.*;
+import java.util.*;
+
+public class FileMessageRepository implements MessageRepository {
+    private final String FILE_PATH = "data/files/messages.dat";
+    private Map<UUID, Message> data = new HashMap<>();
+
+
+    @Override
+    public void save(Message message) {
+        data.put(message.getId(), message);
+        saveToFile();
+    }
+
+    @Override
+    public Optional<Message> findById(UUID id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void update(UUID id, Message message) {
+        if (data.containsKey(id)) {
+            message.update();
+            data.put(id, message);
+            saveToFile();
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        data.remove(id);
+        saveToFile();
+    }
+
+    private void saveToFile() {
+        File file = new File(FILE_PATH);
+        File parentDir = file.getParentFile(); // 부모 디렉토리 가져오기
+
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs(); // 저장할 디렉토리 자동 생성
+        }
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
+            oos.writeObject(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadFromFile() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            data = new HashMap<>();
+            return;
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
+            data = (Map<UUID, Message>) ois.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
