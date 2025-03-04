759ded836918f4524a3bc4cde4ca098b5dafd128
Jaeseok Heo <jasonheo1998@gmail.com>
2025-03-04 16:44:15 +0900

    FileUserService 레포지토리 사용하도록 변경

--- src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -1,75 +1,38 @@
 package com.sprint.mission.discodeit.service.file;
 
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.UserService;
 
 import java.io.*;
 import java.util.*;
 
 public class FileUserService implements UserService {
-    private final String FILE_PATH = "data/files/users.dat";
-    private Map<UUID, User> data = new HashMap<>();
-
-    public FileUserService() {
-        loadFromFile();
-    }
+    private final UserRepository userRepository = new FileUserRepository();
 
     @Override
     public void create(User entity) {
-        data.put(entity.getId(), entity);
-        saveToFile();
+        userRepository.save(entity);
     }
 
     @Override
     public Optional<User> read(UUID id) {
-        return Optional.ofNullable(data.get(id));
+        return userRepository.findById(id);
     }
 
     @Override
     public List<User> readAll() {
-        return new ArrayList<>(data.values());
+        return userRepository.findAll();
     }
 
     @Override
     public void update(UUID id, User entity) {
-        if (data.containsKey(id)) {
-            entity.update();
-            data.put(id, entity);
-            saveToFile();
-        }
+        userRepository.update(id, entity);
     }
 
     @Override
     public void delete(UUID id) {
-        data.remove(id);
-        saveToFile();
-    }
-
-    private void saveToFile() {
-        File file = new File(FILE_PATH);
-        File parentDir = file.getParentFile(); // 부모 디렉토리 가져오기
-
-        if (parentDir != null && !parentDir.exists()) {
-            parentDir.mkdirs(); // 저장할 디렉토리 자동 생성
-        }
-
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
-            oos.writeObject(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadFromFile() {
-        File file = new File(FILE_PATH);
-        if (!file.exists()) {
-            data = new HashMap<>();
-            return;
-        }
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
-            data = (Map<UUID, User>) ois.readObject();
-        } catch (ClassNotFoundException | IOException e) {
-            e.printStackTrace();
-        }
+        userRepository.delete(id);
     }
 }
