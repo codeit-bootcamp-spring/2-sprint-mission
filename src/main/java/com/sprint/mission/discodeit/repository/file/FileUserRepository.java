6d77e9f2ba5e7e51cf7aa9eb54bf886f39d93deb
Jaeseok Heo <jasonheo1998@gmail.com>
2025-03-04 16:37:01 +0900

    FileUserRepository 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
@@ -0,0 +1,75 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+
+import java.io.*;
+import java.util.*;
+
+public class FileUserRepository implements UserRepository {
+    private final String FILE_PATH = "data/files/users.dat";
+    private Map<UUID, User> data = new HashMap<>();
+
+    public FileUserRepository() {
+        loadFromFile();
+    }
+
+    @Override
+    public void save(User user) {
+        data.put(user.getId(), user);
+        saveToFile();
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void update(UUID id, User user) {
+        if (data.containsKey(id)) {
+            user.update();
+            data.put(id, user);
+            saveToFile();
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        data.remove(id);
+        saveToFile();
+    }
+
+    private void saveToFile() {
+        File file = new File(FILE_PATH);
+        File parentDir = file.getParentFile(); // 부모 디렉토리 가져오기
+
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs(); // 저장할 디렉토리 자동 생성
+        }
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
+            oos.writeObject(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadFromFile() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            data = new HashMap<>();
+            return;
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
+            data = (Map<UUID, User>) ois.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
