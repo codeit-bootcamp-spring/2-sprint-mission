1362a5a197d032166ee0d9882d948b204053b323
Jaeseok Heo <jasonheo1998@gmail.com>
2025-03-04 16:39:56 +0900

    JCFUserService 에서 레포지토리 사용하도록 변경

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -1,41 +1,38 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.UserService;
 
 import java.util.*;
 
 public class JCFUserService implements UserService {
-    private final Map<UUID, User> data = new HashMap<>();
+    private final UserRepository userRepository = new JCFUserRepository();
     // UUID를 키로 사용하여 User 객체를 저장 / 일반적으로 키는 String UUID Integer 타입(변하지 않는 값)
 
     @Override
     public void create(User entity) {
-        data.put(entity.getId(), entity);
+        userRepository.save(entity);
     }
 
     @Override
     public Optional<User> read(UUID id) {
-        return Optional.ofNullable(data.get(id));
-        // data.get(id) == null -> Optional.empty()생성
+        return userRepository.findById(id);
     }
 
     @Override
     public List<User> readAll() {
-        return new ArrayList<>(data.values());
-        // data.values() 값(User 객체들)을 Collection 타입으로 반환
+        return userRepository.findAll();
     }
 
     @Override
     public void update(UUID id, User entity) {
-        if (data.containsKey(id)) {
-            entity.update();
-            data.put(id, entity);
-        }
+        userRepository.update(id, entity);
     }
 
     @Override
     public void delete(UUID id) {
-        data.remove(id);
+        userRepository.delete(id);
     }
 }
