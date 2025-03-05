46222056bdcc687e016f8442fab3c3a68539e639
leejunnyeop <daffodil8620@naver.com>
2025-03-05 13:37:31 +0900

    [refactor] JCFMessage... : repository 와 service 역활 분리(코드 분리)

--- src/main/java/com/sprint/sprint2/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/sprint2/discodeit/service/jcf/JCFMessageService.java
@@ -1,6 +1,8 @@
 package com.sprint.sprint2.discodeit.service.jcf;
 
 import com.sprint.sprint2.discodeit.entity.Message;
+import com.sprint.sprint2.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.sprint2.discodeit.repository.jcf.JCFMessageRepository;
 import com.sprint.sprint2.discodeit.service.ChannelService;
 import com.sprint.sprint2.discodeit.service.MessageService;
 import com.sprint.sprint2.discodeit.service.UserService;
@@ -8,60 +10,45 @@ import com.sprint.sprint2.discodeit.service.UserService;
 import java.util.*;
 
 public class JCFMessageService implements MessageService {
-    private final Map<UUID, Message> data;
-    //
-    private final ChannelService channelService;
+
+    private final JCFChannelRepository jcfChannelRepository;
+    private final JCFMessageRepository jcfMessageRepository;
     private final UserService userService;
 
-    public JCFMessageService(ChannelService channelService, UserService userService) {
-        this.data = new HashMap<>();
-        this.channelService = channelService;
+    public JCFMessageService(JCFMessageRepository jcfMessageRepository, UserService userService, JCFChannelRepository channelRepository) {
+        this.jcfChannelRepository = channelRepository;
+        this.jcfMessageRepository = jcfMessageRepository;
         this.userService = userService;
     }
 
     @Override
     public Message create(String content, UUID channelId, UUID authorId) {
-        try {
-            channelService.find(channelId);
-            userService.find(authorId);
-        } catch (NoSuchElementException e) {
-            throw e;
-        }
-
+        jcfChannelRepository.findById(channelId.toString());
+        userService.find(authorId);
         Message message = new Message(content, channelId, authorId);
-        this.data.put(message.getId(), message);
-
+        jcfMessageRepository.save(message);
         return message;
     }
 
     @Override
     public Message find(UUID messageId) {
-        Message messageNullable = this.data.get(messageId);
-
-        return Optional.ofNullable(messageNullable)
-                .orElseThrow(() -> new NoSuchElementException("Message with id " + messageId + " not found"));
+        return jcfMessageRepository.findById(messageId.toString());
     }
 
     @Override
     public List<Message> findAll() {
-        return this.data.values().stream().toList();
+        return jcfMessageRepository.findByAll();
     }
 
     @Override
     public Message update(UUID messageId, String newContent) {
-        Message messageNullable = this.data.get(messageId);
-        Message message = Optional.ofNullable(messageNullable)
-                .orElseThrow(() -> new NoSuchElementException("Message with id " + messageId + " not found"));
+        Message message = jcfMessageRepository.findById(messageId.toString());
         message.update(newContent);
-
         return message;
     }
 
     @Override
     public void delete(UUID messageId) {
-        if (!this.data.containsKey(messageId)) {
-            throw new NoSuchElementException("Message with id " + messageId + " not found");
-        }
-        this.data.remove(messageId);
+      jcfMessageRepository.delete(messageId);
     }
 }
