51ada4db21f0558173e78ce28c1a32a842abb65d
ddday366 <ddday366@gmail.com>
2025-03-05 14:19:36 +0900

    fix: JCFChannelService 수정

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -1,40 +1,47 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
 public class JCFChannelService implements ChannelService {
-    private final Map<UUID, Channel> data = new HashMap<>();
+    private final ChannelRepository channelRepository;
+
+    public JCFChannelService(ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
+    }
 
     @Override
     public Channel createChannel(String channelName) {
         Channel channel = new Channel(channelName);
-        data.put(channel.getId(), channel);
+        channelRepository.save(channel);
         return channel;
     }
 
     @Override
     public Channel getChannel(UUID id) {
-        return data.get(id);
+        return channelRepository.findById(id);
     }
 
     @Override
     public List<Channel> getAllChannels() {
-        return new ArrayList<>(data.values());
+        return channelRepository.findAll();
     }
 
     @Override
     public void updateChannel(UUID id, String newChannelName) {
-        Channel channel = data.get(id);
+        Channel channel = channelRepository.findById(id);
         if (channel != null) {
             channel.updateChannelName(newChannelName);
+            channelRepository.save(channel);
         }
     }
 
     @Override
     public void deleteChannel(UUID id) {
-        data.remove(id);
+        channelRepository.delete(id);
     }
 }
