f0383e6680a20596a383de3e28edce89dc2b22bb
ddday366 <ddday366@gmail.com>
2025-03-05 13:40:01 +0900

    fix: User 수정

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,9 +1,41 @@
 package com.sprint.mission.discodeit.entity;
 
-public class User extends BaseEntity {
+import java.io.Serializable;
+import java.util.UUID;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String userName;
+    protected UUID id;
+    protected Long createAt;
+    protected Long updateAt;
+
+    public User() {
+        this.id = UUID.randomUUID();
+        this.createAt = System.currentTimeMillis();
+        this.updateAt = createAt;
+        this.userName = "Unknown";
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Long getCreateAt() {
+        return createAt;
+    }
+
+    public Long getUpdateAt() {
+        return updateAt;
+    }
+
 
     public User (String userName) {
+        this();
         this.userName = userName;
     }
 
