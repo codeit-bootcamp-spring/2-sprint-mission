42e16b8e75dc559d76aaa4805dc427539bb041e5
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-03-05 14:32:47 +0900

    Refactor: 스프린트 모범답안1로 코드 변경
    - 기존 코드는 연관관계가 꼬이면서 에러가 너무 많이 발생
    - 스프린트 모범답안1로 코드를 변경하고, 미션 진행

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,186 +1,67 @@
 package com.sprint.mission.discodeit.service.jcf;
 
-import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class JCFMessageService implements MessageService {
+    private final Map<UUID, Message> data;
+    //
+    private final ChannelService channelService;
+    private final UserService userService;
 
-    private final List<Message> data;
-    private final ChannelService channelService; // 채널 찾아올 때 사용해야함
-
-    private JCFMessageService(ChannelService channelService) {
-        data = new ArrayList<>();
+    public JCFMessageService(ChannelService channelService, UserService userService) {
+        this.data = new HashMap<>();
         this.channelService = channelService;
-    }
-
-    // 싱글톤 패턴 적용 - 지금은 javaApplication에서만 사용되는데 굳이 적용할 필요가 있을까? -> 나중을 위해 적용해놓자.
-    // 아직까진 멀티 쓰레드 환경이 아니므로 게으른 초기화 사용
-    private static MessageService messageService;
-
-    public static MessageService getInstance(ChannelService channelService) {
-        if(messageService == null) {
-            messageService = new JCFMessageService(channelService);
-        }
-        return messageService;
-    }
-
-
-    // =================================== 메시지 생성 ===================================
-    // 메시지 생성
-    @Override
-    public void createMessage(Message message) {
-        validateCreateMessage(message);
-        data.add(message);
-        addMessageToUserAndChannel(message);
-    }
-
-    private void validateCreateMessage(Message message) {
-        if (message == null) {
-            throw new IllegalArgumentException("생성하려는 메시지 정보가 존재하지 않습니다.");
-        }
-        // 해당 채널의 유저인지 확인
-        if(!message.getChannel().getUsers().contains(message.getSender())) {
-            throw new IllegalStateException("해당 채널의 유저가 아닙니다.");
-        }
-        if (message.getChannel() == null || message.getContent() == null || message.getSender() == null) {
-            throw new IllegalArgumentException("필수 입력 항목을 빠뜨리셨습니다.");
-        }
-    }
-
-    private void addMessageToUserAndChannel(Message message) {
-        // 메시지를 생성하면 User의 Message 목록에도 추가 (양방향 유지)
-        message.getSender().addMessage(message);
-        // 메시지를 생성하면 Channel의 Message 목록에도 추가 (양방향 유지)
-        message.getChannel().addMessage(message);
-    }
-
-
-    // =================================== 메시지 조회 ===================================
-    // 메시지 단건 조회
-    @Override
-    public Message getMessage(UUID id) {
-        if(id == null) {
-            throw new IllegalArgumentException("검색하려는 메시지의 Id값을 입력하지 않았습니다.");
-        }
-        Message findMessage = data.stream()
-                .filter(m -> m.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("해당 Id값을 가진 메시지는 존재하지 않습니다."));
-
-        return findMessage;
-    }
-
-    // 메시지 전체 조회
-    @Override
-    public List<Message> getAllMessages() {
-        return data;
-    }
-
-
-    // =================================== 메시지 수정 ===================================
-    // 메시지 수정
-    @Override
-    public void updateMessage(UUID id, User owner, String content) {
-        Message findMessage = getMessage(id);
-        validateUpdateMessage(findMessage, owner, content);
-        updateMessageInfo(findMessage, content);
-    }
-
-    private void validateUpdateMessage(Message message, User user, String content) {
-        if (content == null) {
-            throw new IllegalArgumentException("메시지 수정 정보가 존재하지 않습니다.");
-        }
-
-        if (!user.equals(message.getSender())) {
-            throw new IllegalStateException("메시지 작성자만 수정할 수 있습니다.");
-        }
-    }
-
-    private void updateMessageInfo(Message message, String content) {
-        message.updateUpdatedAt(System.currentTimeMillis());
-        message.updateContent(content);
-    }
-
-
-    // =================================== 메시지 삭제 ===================================
-    // 메시지 삭제
-    @Override
-    public void deleteMessage(UUID id, User owner) {
-        Message findMessage = getMessage(id);
-
-        // 채널 관리자나 메시지의 주인이 아니면 Exception 발생
-        if (!owner.equals(findMessage.getChannel().getOwner()) && !owner.equals(findMessage.getSender())) {
-            throw new IllegalStateException("메시지 작성자나 관리자만 삭제할 수 있습니다.");
-        }
-
-        // 메시지 삭제 시 사용자의 messages 필드에도 삭제됨
-        findMessage.getSender().getMessages().remove(findMessage);
-
-        data.remove(findMessage);
-    }
-
-
-    // 해당 채널의 해당 유저의 메시지만 검색
-    @Override
-    public List<Message> searchMessageByChannelAndUser(String channelName, String username) {
-        if(channelName == null) {
-            throw new IllegalArgumentException("검색에 필요한 정보가 입력되지 않았습니다. (채널명 필요)");
-        }
-        Channel findChannel = channelService.getChannel(channelName);
-        List<Message> messages = findChannel.getMessages();
-
-        // username이 null이나 빈칸일 경우 그냥 채널 메시지의 전체 리스트 반환
-        if(username == null || username.isBlank()) {
-            return messages;
-        }
-
-        return messages.stream()
-                .filter(m -> m.getSender().getUsername().equals(username))
-                .collect(Collectors.toList());
-    }
-
-    // 해당 채널의 메시지를 포함 내용으로 검색
-    @Override
-    public List<Message> searchMessagesContaining(String channelName, String content) {
-        if(channelName == null) {
-            throw new IllegalArgumentException("검색에 필요한 정보가 입력되지 않았습니다. (채널명 필요)");
-        }
-
-        Channel findChannel = channelService.getChannel(channelName);
-        List<Message> messages = findChannel.getMessages();
-
-        // content가 null이나 빈칸일 경우 그냥 채널 메시지의 전체 리스트 반환 (검색어가 없으므로)
-        if(content == null || content.isBlank()) {
-            return messages;
-        }
-
-        return messages.stream()
-                .filter(m -> m.getContent().contains(content))
-                .collect(Collectors.toList());
+        this.userService = userService;
     }
 
     @Override
-    public void saveToFile(Message message) {
+    public Message create(String content, UUID channelId, UUID authorId) {
+        try {
+            channelService.find(channelId);
+            userService.find(authorId);
+        } catch (NoSuchElementException e) {
+            throw e;
+        }
 
+        Message message = new Message(content, channelId, authorId);
+        this.data.put(message.getId(), message);
+
+        return message;
     }
 
     @Override
-    public List<Message> loadFromFile() {
-        return List.of();
+    public Message find(UUID messageId) {
+        Message messageNullable = this.data.get(messageId);
+
+        return Optional.ofNullable(messageNullable)
+                .orElseThrow(() -> new NoSuchElementException("Message with id " + messageId + " not found"));
     }
 
     @Override
-    public void deleteFile(Message message) {
+    public List<Message> findAll() {
+        return this.data.values().stream().toList();
+    }
 
+    @Override
+    public Message update(UUID messageId, String newContent) {
+        Message messageNullable = this.data.get(messageId);
+        Message message = Optional.ofNullable(messageNullable)
+                .orElseThrow(() -> new NoSuchElementException("Message with id " + messageId + " not found"));
+        message.update(newContent);
+
+        return message;
+    }
+
+    @Override
+    public void delete(UUID messageId) {
+        if (!this.data.containsKey(messageId)) {
+            throw new NoSuchElementException("Message with id " + messageId + " not found");
+        }
+        this.data.remove(messageId);
     }
 }
--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -6,161 +6,48 @@ import com.sprint.mission.discodeit.service.UserService;
 import java.util.*;
 
 public class JCFUserService implements UserService {
+    private final Map<UUID, User> data;
 
-    // 맵 객체가 더 나은가? - List 검색 시 처음부터 끝까지 순회 -> 데이터가 많아지면 검색 효율이 떨어짐
-    // 반면에 Map은 key값으로 바로바로 정보를 꺼내올 수 있음
-    private final List<User> data;
-
-    // 생성자에서 data 필드 초기화
-    // 요구사항엔 이렇게 적혀있는데, 항상 빈 배열로 초기화 할거면 왜 필드가 아니라 생성자에서 초기화하라고 했을까?
-    private JCFUserService() {
-        data = new ArrayList<>();
-    } // private으로 객체 생성 방지 -> 싱글톤 패턴
-
-    // 싱글톤 패턴 적용 - 지금은 javaApplication에서만 사용되는데 굳이 적용할 필요가 있을까? -> 나중을 위해 적용해놓자.
-    // 아직까진 멀티 쓰레드 환경이 아니므로 게으른 초기화 사용
-    private static UserService userService;
-
-    public static UserService getInstance() {
-        if(userService == null) {
-            userService = new JCFUserService();
-        }
-        return userService;
-    }
-
-
-    // =================================== 유저 생성 ===================================
-    // 로직이 복잡하므로 메소드 쪼개서 만들어보자 - 쪼개는데 정답은 없음! 가독성이 안나온다면 쪼개고, 흐름을 한눈에 파악할 수 있다면 안쪼개도됨
-    @Override
-    public void createUser(User user) {
-        validateCreateUser(user);
-        checkDuplicateUsername(user.getUsername());
-        data.add(user);
-    }
-
-    private void validateCreateUser(User user) {
-        if (user == null) {
-            throw new IllegalArgumentException("생성하려는 유저 정보가 입력되지 않았습니다.");
-        }
-        // 필수 입력 항목이 하나라도 빠지거나 공백이면 Exception 발생
-        // 너무 길어지지만, isBlank는 null 체크를 못하고, == null은 공백문자를 체크하지 못함
-        if (user.getUsername() == null || user.getUsername().isBlank()|| user.getPassword() == null || user.getPassword().isBlank() || user.getNickname() == null || user.getNickname().isBlank()) {
-            throw new IllegalArgumentException("필수 입력 항목을 빠뜨리셨습니다. (username, password, nickname)");
-        }
-    }
-
-    private void checkDuplicateUsername(String username) {
-        // 중복 유저 방지, username을 기준으로 회원을 찾거나 수정하므로 중복이 생기면 안된다.
-        if (data.stream().
-                anyMatch(u -> u.getUsername().equals(username))) {
-            // 처음엔 data.stream().filter(u -> u.getUsername().equals(user.getUsername()).findFirst().isPresent()로 작성하였으나,
-            // anyMatch를 사용하는게 더 간편해 보여 사용
-            throw new IllegalStateException("중복된 username 입니다."); // 입력값 자체는 괜찮으나, 시스템에서 중복 유저를 허용하지 않으므로 IllegalStateException 사용
-        }
-    }
-
-
-    // =================================== 유저 조회 ===================================
-    // 유저 하나 읽기
-    // 로직이 복잡하지 않고, 흐름이 한눈에 들어옴 -> 굳이 쪼갤 필요 없음
-    @Override
-    public User getUser(String username) {
-        if(username == null) {
-            throw new IllegalArgumentException("username이 입력되지 않았습니다.");
-        }
-
-        User findUser = data.stream()
-                .filter(u -> u.getUsername().equals(username))
-                .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("해당 username을 가진 유저는 존재하지 않습니다."));
-
-        return findUser;
-    }
-
-
-    // 유저 전체 읽기
-    @Override
-    public List<User> getAllUsers() {
-        // 유저가 없는 경우엔 빈 List를 반환하는게 좋을지, 예외를 던져주는게 좋을지 고민
-        // 일단 검색 결과가 없다는걸 띄워야하므로 빈 List 반환
-        return data;
-    }
-
-
-    // =================================== 유저 수정 ===================================
-    // 유저 정보 수정
-    @Override
-    public void updateUser(User user) { // 매개변수로 도대체 뭘 받아야 효율적일지 모르겟다. 필드별로 받기 vs User 객체로 받기
-        validateUpdateUser(user);
-        User findUser = getUser(user.getUsername()); // getUser 메소드로 반환되는 User 객체는 data 리스트에 이미 존재하는 객체의 참조 -> findUser를 수정하면 data의 해당 객체도 수정됨
-        updateUserInfo(findUser, user);
-    }
-
-    // updateUser와 createUser에서 유효성 검증하는 부분이 다르므로, 따로 빼주는게 맞을 것 같다.
-    private void validateUpdateUser(User user) {
-        if (user == null) {
-            throw new IllegalArgumentException("수정 정보가 입력되지 않았습니다.");
-        }
-        // 필수 입력 항목이 하나라도 빠지면 Exception 발생 (username은 아이디이므로 수정X)
-        if (user.getPassword() == null || user.getPassword().isBlank() || user.getNickname() == null || user.getNickname().isBlank()) {
-            throw new IllegalArgumentException("필수 입력 항목을 빠뜨리셨습니다.");
-        }
-    }
-
-    private void updateUserInfo(User findUser, User user) {
-        findUser.updateUpdatedAt(System.currentTimeMillis());
-        findUser.updateNickname(user.getNickname());
-        findUser.updatePassword(user.getPassword());
-    }
-
-
-    // =================================== 유저 권한 관련 ===================================
-    // 권한 추가
-    @Override
-    public void addRole(String role, String username) {
-        if (role == null) {
-            throw new IllegalArgumentException("추가할 권한이 입력되지 않았습니다.");
-        }
-
-        User findUser = getUser(username);
-        findUser.addRole(role);
-    }
-
-    // 권한 삭제
-    @Override
-    public void removeRole(String role, String username) {
-        if (role == null) { // 입력값 자체에 대한 예외
-            throw new IllegalArgumentException("삭제할 권한이 입력되지 않았습니다.");
-        }
-        User findUser = getUser(username);
-        // 해당 role이 있는지 먼저 확인
-        if (!findUser.getRoles().stream().anyMatch(r -> r.equals(role))) {
-            throw new NoSuchElementException("해당 유저에게 해당 권한은 존재하지 않습니다."); // role 자체는 유효한 값이지만, 검색 결과 role이 존재하지 않으므로 NoSuchElementException
-        }
-        findUser.removeRole(role);
-    }
-
-
-    // =================================== 유저 삭제 ===================================
-    // 유저 삭제
-    @Override
-    public void deleteUser(String username) {
-        User findUser = getUser(username);
-        data.remove(findUser);
+    public JCFUserService() {
+        this.data = new HashMap<>();
     }
 
     @Override
-    public void saveToFile(User user) {
+    public User create(String username, String email, String password) {
+        User user = new User(username, email, password);
+        this.data.put(user.getId(), user);
 
+        return user;
     }
 
     @Override
-    public List<User> loadFromFile() {
-        return List.of();
+    public User find(UUID userId) {
+        User userNullable = this.data.get(userId);
+
+        return Optional.ofNullable(userNullable)
+                .orElseThrow(() -> new NoSuchElementException("User with id " + userId + " not found"));
     }
 
     @Override
-    public void deleteFile(User user) {
+    public List<User> findAll() {
+        return this.data.values().stream().toList();
+    }
 
+    @Override
+    public User update(UUID userId, String newUsername, String newEmail, String newPassword) {
+        User userNullable = this.data.get(userId);
+        User user = Optional.ofNullable(userNullable)
+                .orElseThrow(() -> new NoSuchElementException("User with id " + userId + " not found"));
+        user.update(newUsername, newEmail, newPassword);
+
+        return user;
+    }
+
+    @Override
+    public void delete(UUID userId) {
+        if (!this.data.containsKey(userId)) {
+            throw new NoSuchElementException("User with id " + userId + " not found");
+        }
+        this.data.remove(userId);
     }
 }
