7ad5b0bb9823c1df38743aac79f2a4e11e23f4c0
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-03-05 18:47:18 +0900

    Feat: FileMessageRepository 클래스 구현
    - MessageRepository, FileRepository를 implements해 저장/삭제/조회 및 파일 직렬화/역직렬화 메소드 오버라이딩
    - Repository 계층이라 단순한 저장/삭제/조회 메소드만 담당하도록 함

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -0,0 +1,66 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.FileRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.util.SerializationUtil;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+public class FileMessageRepository implements MessageRepository, FileRepository<Message> {
+    private final Path directory = Paths.get(System.getProperty("user.dir"), "data", "messages");
+
+    @Override
+    public Message save(Message message) {
+        saveToFile(message);
+        return message;
+    }
+
+    @Override
+    public Message findById(UUID messageId) {
+        return loadOneFromFileById(messageId);
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return loadAllFromFile();
+    }
+
+    @Override
+    public void deleteById(UUID messageId) {
+        deleteFileById(messageId);
+    }
+
+    @Override
+    public void saveToFile(Message message) {
+        Path filePath = directory.resolve(message.getId() + ".ser");
+        SerializationUtil.init(directory);
+        SerializationUtil.serialization(filePath, message);
+    }
+
+    @Override
+    public Message loadOneFromFileById(UUID messageId) {
+        return SerializationUtil.reverseOneSerialization(directory,messageId);
+    }
+
+    @Override
+    public List<Message> loadAllFromFile() {
+        return SerializationUtil.reverseSerialization(directory);
+    }
+
+    @Override
+    public void deleteFileById(UUID messageId) {
+        Path filePath = directory.resolve(messageId + ".ser");
+        try {
+            Files.deleteIfExists(filePath);
+        } catch (IOException e) {
+            System.out.println("메시지 파일 삭제 예외 발생 : " + e.getMessage());
+        }
+    }
+}
