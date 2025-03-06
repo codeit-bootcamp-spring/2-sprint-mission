b624e597a4d03b8d65ccc662e60919a9a3c946c5
Leekb0804 <lkb8713@gmail.com>
2025-03-06 14:20:14 +0900

    refactor(repository): Channel entity 의 participants 자료형 변경에 따른 코드 수정 및 AbstractRepository 인터페이스 반영

--- src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -1,10 +1,9 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.User;
 
 import java.util.UUID;
 
 public interface ChannelRepository extends Repository<Channel> {
-    void addParticipant(UUID channelId, User newParticipant);
+    void addParticipant(UUID channelId, UUID newParticipantId);
 }
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -1,16 +1,15 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
 import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.AbstractRepository;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import java.util.*;
 
-public class JCFChannelRepository implements ChannelRepository {
+public class JCFChannelRepository extends AbstractRepository<Channel> implements ChannelRepository {
     private static volatile JCFChannelRepository instance;         // volatile을 사용하여 변수의 값을 JVM이 캐시하지 않도록 보장
-    private final Map<UUID, Channel> channels;
 
     private JCFChannelRepository() {
-        channels = new HashMap<>();
+        super(Channel.class, new HashMap<>());
     }
 
     public static JCFChannelRepository getInstance() {
@@ -26,41 +25,7 @@ public class JCFChannelRepository implements ChannelRepository {
         return instance;
     }
 
-    @Override
-    public void add(Channel channel) {
-        this.channels.put(channel.getId(), channel);
-    }
-
-    @Override
-    public boolean existsById(UUID channelId) {
-        if (channelId == null) {
-            throw new IllegalArgumentException("null값을 가지는 channelId가 들어왔습니다!!!");
-        }
-        return channels.containsKey(channelId);
-    }
-
-    @Override
-    public Channel findById(UUID channelId) {
-        if (!existsById(channelId)) {
-            throw new NoSuchElementException("해당 channelId를 가진 채널이 존재하지 않습니다 : " + channelId);
-        }
-        return channels.get(channelId);
-    }
-
-    @Override
-    public Map<UUID, Channel> getAll() {
-        return channels;
-    }
-
-    @Override
-    public void deleteById(UUID channelId) {
-        if (!existsById(channelId)) {
-            throw new NoSuchElementException("해당 channelId를 가진 채널이 존재하지 않습니다 : " + channelId);
-        }
-        channels.remove(channelId);
-    }
-
-    public void addParticipant(UUID channelId, User newParticipant) {
-        findById(channelId).addParticipant(newParticipant);
+    public void addParticipant(UUID channelId, UUID newParticipantId) {
+        super.findById(channelId).addParticipant(newParticipantId);
     }
 }
