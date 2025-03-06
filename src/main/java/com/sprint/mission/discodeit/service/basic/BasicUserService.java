93ed081cf524b8f548a1940a05d9f5555ee15d54
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-03-06 13:27:54 +0900

    Faet: BasicUserService 구현
    - UserService를 implements한 BasicUserService 구현
    - 싱글톤 패턴을 적용하여 인스턴스가 한번만 생성되도록 함
    - userRepository를 생성자를 통해 주입받음

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,62 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+public class BasicUserService implements UserService {
+    private static volatile BasicUserService instance;
+    private final UserRepository userRepository;
+
+    public static BasicUserService getInstance(UserRepository userRepository) {
+        if (instance == null) {
+            synchronized (BasicUserService.class) {
+                if (instance == null) {
+                    instance = new BasicUserService(userRepository);
+                }
+            }
+        }
+        return instance;
+    }
+
+    private BasicUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(String username, String email, String password) {
+        User user = new User(username, email, password);
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public User find(UUID userId) {
+        return userRepository
+                .findById(userId)
+                .orElseThrow(() -> new NoSuchElementException("유저가 존재하지 않습니다."));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User update(UUID userId, String newUsername, String newEmail, String newPassword) {
+        User user = find(userId);
+        user.update(newUsername, newEmail, newPassword);
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public void delete(UUID userId) {
+        find(userId); // 유저가 존재하는지 확인
+        userRepository.deleteById(userId);
+    }
+}
