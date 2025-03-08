8ede3f10a7cb81f219b327c5c28545eb39764431
hanna kong <khn1346@gmail.com>
2025-03-08 20:08:06 +0900

    feat: 서비스 인터페이스의 구현체(Basic_) 구현

---
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -0,0 +1,143 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.*;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+
+public class BasicMessageService implements MessageService {
+
+    private static volatile BasicMessageService instance;
+    private final ChannelService channelService;
+    private final UserService userService;
+    private final MessageRepository messageRepository;
+
+    private BasicMessageService(UserService userService, ChannelService channelService, MessageRepository messageRepository) {
+        this.userService = userService;
+        this.channelService = channelService;
+        this.messageRepository = messageRepository;
+    }
+
+    public static BasicMessageService getInstance(UserService userService, ChannelService channelService, MessageRepository messageRepository) {
+        if (instance == null) {
+            synchronized (BasicMessageService.class) {
+                if (instance == null) {
+                    instance = new BasicMessageService(userService, channelService, messageRepository);
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void createMessage(Message message) {
+        //데이터 유효성 확인
+        messageValidationCheck(message);
+        //요청자 확인
+        User user = findUserOrThrow(message.getUserId());
+        //채널 확인
+        Channel channel = findChannelOrThrow(message.getChannelId());
+        //작성 권한 확인
+        validateUserPermission(user, null, channel.getWritePermission(), ActionType.WRITE);
+        //create
+        messageRepository.createMessage(message);
+    }
+
+    @Override
+    public Optional<Message> selectMessageById(UUID id) {
+        return messageRepository.selectMessageById(id);
+    }
+
+    @Override
+    public List<Message> selectMessagesByChannel(UUID channelId) {
+        return messageRepository.selectMessagesByChannel(channelId);
+    }
+
+    @Override
+    public void updateMessage(UUID id, String content, UUID userId, UUID channelId) {
+        //요청자 확인
+        User user = findUserOrThrow(userId);
+        //메시지 유효성 확인
+        Message message = findMessageOrThrow(id);
+        //채널 확인
+        ensureMessageBelongsToChannel(message, channelId);
+        //수정 권한 확인
+        validateUserPermission(user, message.getUserId(), null, ActionType.EDIT);
+        //update
+        if (content != null) {
+            messageRepository.updateMessage(id, content, userId, channelId);
+        }
+    }
+
+    @Override
+    public void deleteMessage(UUID id, UUID userId, UUID channelId) {
+        //요청자 확인
+        User user = findUserOrThrow(userId);
+        //메시지 유효성 확인
+        Message message = findMessageOrThrow(id);
+        //채널 확인
+        ensureMessageBelongsToChannel(message, channelId);
+        //삭제 권한 확인
+        validateUserPermission(user, message.getUserId(), null, ActionType.DELETE);
+        //delete
+        messageRepository.deleteMessage(id, userId, channelId);
+    }
+
+    /*******************************
+     * Validation check
+     *******************************/
+    private void messageValidationCheck(Message message){
+        // 1. null check
+        if (message == null) {
+            throw new IllegalArgumentException("message 객체가 null입니다.");
+        }
+        if (message.getId() == null) {
+            throw new IllegalArgumentException("ID 값이 없습니다.");
+        }
+        if (message.getContent() == null) {
+            throw new IllegalArgumentException("메시지 내용이 null입니다.");
+        }
+        if (message.getUserId() == null) {
+            throw new IllegalArgumentException("채널 개설자의 ID가 없습니다.");
+        }
+        if (message.getChannelId() == null) {
+            throw new IllegalArgumentException("채널의 ID가 없습니다.");
+        }
+    }
+
+    private void ensureMessageBelongsToChannel(Message message, UUID channelId){
+        if (!message.getChannelId().equals(channelId)) {
+            throw new RuntimeException("해당 메시지는 요청한 채널에 속하지 않습니다.");
+        }
+    }
+
+    private void validateUserPermission(User user, UUID messageUserId, UserRole channelRequiredRole, ActionType action) {
+        if (action == ActionType.WRITE) {
+            if (user.getRole() != channelRequiredRole && user.getRole() != UserRole.ADMIN) {
+                throw new RuntimeException("메시지를 작성할 권한이 없습니다.");
+            }
+        } else {
+            if (!user.getId().equals(messageUserId) && user.getRole() != UserRole.ADMIN) {
+                throw new RuntimeException(String.format("메시지를 %s할 권한이 없습니다.", action == ActionType.EDIT ? "수정" : "삭제"));
+            }
+        }
+    }
+
+    private Message findMessageOrThrow(UUID messageId){
+        return selectMessageById(messageId)
+                .orElseThrow(() -> new RuntimeException("해당 메시지가 존재하지 않습니다. : " + messageId));
+    }
+
+    private Channel findChannelOrThrow(UUID channelId){
+        return channelService.selectChannelById(channelId)
+                .orElseThrow(() -> new RuntimeException("해당 채널이 존재하지 않습니다. : " + channelId));
+    }
+
+    private User findUserOrThrow(UUID userId) {
+        return userService.selectUserById(userId)
+                .orElseThrow(() -> new RuntimeException("해당 사용자가 존재하지 않습니다. : " + userId));
+    }
+}
---
+++ 2-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,191 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserRole;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class BasicUserService implements UserService {
+
+    private static volatile BasicUserService instance;
+    private final UserRepository userRepository;
+
+    private BasicUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public static BasicUserService getInstance(UserRepository userRepository) {
+        if (instance == null) {
+            synchronized (BasicUserService.class) {
+                if (instance == null) {
+                    instance = new BasicUserService(userRepository);
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void createUser(User user) {
+        //데이터 유효성 확인
+        userValidationCheck(user);
+        //사용자 중복 확인
+        checkUserExists(user);
+        //create
+        userRepository.createUser(user);
+    }
+
+    @Override
+    public Optional<User> selectUserById(UUID id) {
+        return userRepository.selectUserById(id);
+    }
+
+    @Override
+    public Optional<User> selectUserByNickname(String nickname) {
+        if (nickname == null) {
+            return Optional.empty();
+        }
+        return userRepository.selectUserByNickname(nickname);
+    }
+
+    @Override
+    public Optional<User> selectUserByEmail(String email) {
+        if (email == null) {
+            return Optional.empty();
+        }
+        return userRepository.selectUserByEmail(email);
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return userRepository.selectAllUsers();
+    }
+
+    @Override
+    public void updateUser(UUID id, String password, String nickname, UserStatus status, UserRole role) {
+        //데이터 유효성 확인
+        validateId(id);
+        //사용자 확인
+        User user = findUserOrThrow(id);
+        //유효성 검증
+        if (password != null && !password.equals(user.getPassword())) {
+            validatePassword(password);
+        }
+        if (nickname != null && !nickname.equals(user.getNickname())) {
+            validateNickname(nickname);
+            checkUserNicknameExists(nickname);
+        }
+        //update
+        if (password != null || nickname != null || status != null || role != null) {
+            userRepository.updateUser(id, password, nickname, status, role);
+        }
+    }
+
+    @Override
+    public void deleteUser(UUID id) {
+        //데이터 유효성 확인
+        validateId(id);
+        //사용자 확인
+        User user = findUserOrThrow(id);
+        //delete
+        userRepository.deleteUser(user.getId());
+    }
+
+    @Override
+    public void clearUsers() {
+        userRepository.clearUsers();
+    }
+
+    /****************************
+     * Validation check
+     ****************************/
+    private void userValidationCheck(User user){
+        // 1. null check
+        if (user == null) {
+            throw new IllegalArgumentException("User 객체가 null입니다.");
+        }
+        if (user.getId() == null) {
+            throw new IllegalArgumentException("ID 값이 없습니다.");
+        }
+        if (user.getEmail() == null || user.getEmail().trim().isEmpty()) {
+            throw new IllegalArgumentException("이메일이 없습니다.");
+        }
+        if (user.getPassword() == null || user.getPassword().trim().isEmpty()) {
+            throw new IllegalArgumentException("비밀번호가 없습니다.");
+        }
+        if (user.getNickname() == null || user.getNickname().trim().isEmpty()) {
+            throw new IllegalArgumentException("닉네임이 없습니다.");
+        }
+        if (user.getStatus() == null) {
+            throw new IllegalArgumentException("Status 값이 없습니다.");
+        }
+        if (user.getRole() == null) {
+            throw new IllegalArgumentException("Role 값이 없습니다.");
+        }
+        //2. 이메일 형식 check
+        validateEmail(user.getEmail());
+        //3. 비밀번호 길이 check
+        validatePassword(user.getPassword());
+        //4. 닉네임 길이 check
+        validateNickname(user.getNickname());
+    }
+
+    private void validateEmail(String email){
+        if (!email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")) {
+            throw new IllegalArgumentException("이메일 형식이 올바르지 않습니다.");
+        }
+    }
+
+    private void validatePassword(String password){
+        if (password.length() < 6) {
+            throw new IllegalArgumentException("비밀번호는 최소 6자 이상이어야 합니다.");
+        }
+    }
+
+    private void validateNickname(String nickname){
+        if (nickname.length() < 3 || nickname.length() > 20) {
+            throw new IllegalArgumentException("닉네임은 3~20자 사이여야 합니다.");
+        }
+    }
+
+    private void validateId(UUID id){
+        if (id == null) {
+            throw new IllegalArgumentException("사용자 ID 값이 없습니다.");
+        }
+    }
+
+    private User findUserOrThrow(UUID id) {
+        return selectUserById(id)
+                .orElseThrow(() -> new RuntimeException("해당 사용자가 존재하지 않습니다. : " + id));
+    }
+
+    private void checkUserExists(User user) {
+        checkUserIdExists(user.getId());
+        checkUserEmailExists(user.getEmail());
+        checkUserNicknameExists(user.getNickname());
+    }
+
+    private void checkUserIdExists(UUID id) {
+        if (selectUserById(id).isPresent()) {
+            throw new RuntimeException("이미 존재하는 사용자입니다.");
+        }
+    }
+
+    private void checkUserEmailExists(String email) {
+        if (selectUserByEmail(email).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+    }
+
+    private void checkUserNicknameExists(String nickname) {
+        if (selectUserByNickname(nickname).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+    }
+
+}
