51cc134aa89e00c5249d55845c742d25656eef8e
최성현 <round1126@naver.com>
2025-03-09 18:42:37 +0900

    feat: 레포지토리 인터페이스 구현체(JCF)

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -0,0 +1,39 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JCFUserRepository implements UserRepository {
+    private final Map<UUID, User> data;
+
+    public JCFUserRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public User save(User user) {
+        data.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public List<User> findAll(){
+        return this.data.values().stream().toList();
+    }
+
+    @Override
+    public User findById(UUID userId){
+        return Optional.ofNullable(data.get(userId)).orElseThrow(()->new NoSuchElementException("User with id " + userId + " not found"));
+    }
+
+    @Override
+    public void delete(UUID userId){
+        data.remove(userId);
+    }
+}
