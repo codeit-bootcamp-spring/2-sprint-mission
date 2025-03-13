6f570e674c0edcb938fe5ca022bf07b69c27cf0a
manleKim <kk410910@naver.com>
2025-03-13 15:04:41 +0900

    fix: 채널 이름 중복 방지 로직 추가
    - 채널 생성 시 이름 중복 방지 로직 추가
    - 채널 수정 시 이름 중복 방지 로직 추가

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -5,6 +5,7 @@ import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,13 @@ public class BasicChannelService implements ChannelService {
 
     @Override
     public Channel create(ChannelType type, String name, String description) {
+        boolean isExistChannel = findAll().stream()
+                .anyMatch(channel -> channel.getName().equals(name)); // 같은 이름 == 같은 채널
+
+        if (isExistChannel) {
+            throw new RuntimeException(name + " 채널은 이미 존재합니다.");
+        }
+
         Channel newChannel = new Channel(type, name, description);
 
         return channelRepository.save(newChannel);
@@ -29,7 +37,7 @@ public class BasicChannelService implements ChannelService {
         Channel channel = channelRepository.findById(channelId);
 
         if (channel == null) {
-            throw new RuntimeException(channelId + " 채널을 찾을 수 없습니다.");
+            throw new NoSuchElementException(channelId + " 채널을 찾을 수 없습니다.");
         }
 
         return channel;
@@ -42,6 +50,12 @@ public class BasicChannelService implements ChannelService {
 
     @Override
     public Channel update(UUID channelId, String newName, String newDescription) {
+        boolean isExistChannel = findAll().stream().anyMatch(channel -> channel.getName().equals(newName));
+
+        if (isExistChannel) {
+            throw new RuntimeException(channelId + " 채널이 이미 존재해 수정할 수 없습니다.");
+        }
+
         Channel channel = findById(channelId);
         channel.update(newName, newDescription);
 
