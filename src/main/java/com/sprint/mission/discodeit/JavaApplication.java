d39a5d461bba658a93501c034f99aaf49541237e
sangyeobhan <sangyeobhandev@gmail.com>
2025-03-13 11:49:08 +0900

    refactor: File*Service, JCF*Service 삭제

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -19,9 +19,7 @@ import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.basic.BasicChannelService;
 import com.sprint.mission.discodeit.service.basic.BasicMessageService;
 import com.sprint.mission.discodeit.service.basic.BasicUserService;
-import com.sprint.mission.discodeit.service.file.FileChannelService;
-import com.sprint.mission.discodeit.service.file.FileMessageService;
-import com.sprint.mission.discodeit.service.jcf.JCFUserService;
+
 
 import java.io.IOException;
 import java.nio.file.Path;
