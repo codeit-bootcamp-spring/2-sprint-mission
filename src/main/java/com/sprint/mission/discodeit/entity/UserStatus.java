ae2696ccbcccacf17033e7393752a3c41b9e0a85
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-03-13 19:20:18 +0900

    Feat: UserStatus 엔티티 생성
    - 사용자의 마지막 로그인 시간을 표현하는 엔티티
    - userId로 User와 연관관계 매핑
    - 로그인 상태를 반환하는 isLoginUser() 메소드 생성

---
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class UserStatus {
+    private final UUID id;
+    private final UUID userId;
+    private final Instant createdAt;
+    private Instant updatedAt; // 유저의 마지막 접속시간
+
+    public UserStatus(UUID userId) {
+        this.id = UUID.randomUUID();
+        this.userId = userId;
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    public void updateUserStatus() {
+        this.updatedAt = Instant.now();
+    }
+
+    public boolean isLoginUser() {
+        if(Duration.between(updatedAt, Instant.now()).toMinutes() < 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
