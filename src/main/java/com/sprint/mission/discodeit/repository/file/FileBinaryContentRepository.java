f3d2bc2e57946d44d9774ef62d100b352b1bd63c
Leekb0804 <lkb8713@gmail.com>
2025-03-14 14:16:19 +0900

    feat(Repository): FileBinaryContentRepository를 @Repository Bean으로 추가

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileBinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileBinaryContentRepository.java
@@ -1,11 +1,12 @@
 package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.BinaryContent;
-import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import org.springframework.stereotype.Repository;
 
 import java.nio.file.Paths;
 
+@Repository
 public class FileBinaryContentRepository extends AbstractFileRepository<BinaryContent> implements BinaryContentRepository {
     public FileBinaryContentRepository() {
         super(BinaryContent.class, Paths.get(System.getProperty("BinaryContent.dir")).resolve("src\\main\\java\\com\\sprint\\mission\\discodeit\\repository\\file\\BinaryContentData"));      // 현재 프로그램이 실행되고 있는 디렉토리로 설정);
