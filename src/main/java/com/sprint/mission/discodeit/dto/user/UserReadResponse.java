6ac7d42d9e9a54d62a4f39f64cb959d85bc48176
Leekb0804 <lkb8713@gmail.com>
2025-03-14 15:39:17 +0900

    refactor(DTO): UserReadResponse DTO에 userId 필드 추가

--- src/main/java/com/sprint/mission/discodeit/dto/user/UserReadResponse.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/UserReadResponse.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.dto.user;
 import java.util.UUID;
 
 public record UserReadResponse (
+        UUID userId,
         String userName,
         String userEmail,
         UUID profileId,
