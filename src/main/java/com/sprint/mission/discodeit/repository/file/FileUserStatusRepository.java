2c9490c8aa397688e275a7710b2646c394006fa3
Leekb0804 <lkb8713@gmail.com>
2025-03-14 14:15:29 +0900

    feat(Repository): FileUserStatusRepository 클래스에 add 메서드 오버라이딩 및 findUserStatusIdByUserId 메서드 추가
    - 빠른 탐색을 위한 userIdMap<userId, UserStatus> 필드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileUserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserStatusRepository.java
@@ -6,12 +6,27 @@ import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import org.springframework.stereotype.Repository;
 
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Repository
 public class FileUserStatusRepository extends AbstractFileRepository<UserStatus> implements UserStatusRepository {
+    private Map<UUID, UserStatus> userIdMap = new HashMap<>();
+
     public FileUserStatusRepository() {
         super(UserStatus.class, Paths.get(System.getProperty("userStatus.dir")).resolve("src\\main\\java\\com\\sprint\\mission\\discodeit\\repository\\file\\userStatusdata"));      // 현재 프로그램이 실행되고 있는 디렉토리로 설정);
     }
 
-    //
+    @Override
+    public void add(UserStatus newUserStatus) {
+        super.add(newUserStatus);
+        super.saveToFile(directory.resolve(newUserStatus.getId().toString() + ".ser"), newUserStatus);    // file에 반영
+        userIdMap.put(newUserStatus.getUserId(), newUserStatus);
+    }
+
+    @Override
+    public UUID findUserStatusIDByUserId(UUID userId) {
+        return userIdMap.get(userId).getId();
+    }
 }
