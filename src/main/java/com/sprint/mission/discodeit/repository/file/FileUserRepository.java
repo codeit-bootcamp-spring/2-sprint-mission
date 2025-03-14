302963f888d9911d319040d5a217936bfa4b255b
Leekb0804 <lkb8713@gmail.com>
2025-03-14 14:46:11 +0900

    feat & refactor(Repository): findByUserName, findByEmail 메서드 추가 및 기타 메서드 로직 수정
    - usernames, emails 빠른 반환을 위해 자료구조 Set->Map
    - existsByUserName, existsByEmail 메서드에 예외처리 추가

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Repository;
 
 import java.nio.file.Paths;
 import java.util.Map;
-import java.util.Set;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Repository
 public class FileUserRepository extends AbstractFileRepository<User> implements UserRepository {
     // userName과 email의 빠른 중복 확인을 위한 필드
-    private Set<String> usernames;
-    private Set<String> emails;
+    private Map<String, User> usernames;
+    private Map<String, User> emails;
 
     public FileUserRepository() {
         super(User.class, Paths.get(System.getProperty("user.dir")).resolve("src\\main\\java\\com\\sprint\\mission\\discodeit\\repository\\file\\userdata"));      // 현재 프로그램이 실행되고 있는 디렉토리로 설정);
@@ -23,8 +23,8 @@ public class FileUserRepository extends AbstractFileRepository<User> implements 
     public void add(User newUser) {
         super.add(newUser);                                                                 // users에 반영
         super.saveToFile(directory.resolve(newUser.getId().toString() + ".ser"), newUser);    // file에 반영
-        usernames.add(newUser.getUserName());
-        emails.add(newUser.getUserName());
+        usernames.put(newUser.getUserName(), newUser);
+        emails.put(newUser.getUserEmail(), newUser);
     }
 
     // existsById(),findById(), getAll()  굳이 file을 탐색할 필요 없다고 생각해 storage를 통해 정보 확인, -> super.add, super.findById, super.getAll 사용
@@ -39,37 +39,56 @@ public class FileUserRepository extends AbstractFileRepository<User> implements 
 
     @Override
     public void updateUserName(UUID userId, String userName) {
-        if (existsById(userId)) {
-            super.storage.get(userId).updateUserName(userName);
-        }
-        saveToFile(directory.resolve(userId.toString() + ".ser"), super.storage.get(userId));
-        usernames.remove(super.findById(userId).getUserName());
-        usernames.add(userName);
+        User findUser = super.findById(userId);     // findById 에서 예외 처리
+        findUser.updateUserName(userName);
+        saveToFile(directory.resolve(userId.toString() + ".ser"), findUser);
+        usernames.remove(findUser.getUserName());
+        usernames.put(userName, findUser);
     }
 
     @Override
     public void updatePassword(UUID userId, String newPassword) {
-        if (existsById(userId)) {
-            super.storage.get(userId).updateUserPassword(newPassword);
-        }
-        saveToFile(directory.resolve(userId.toString() + ".ser"), super.storage.get(userId));
+        User findUser = super.findById(userId);     // findById 에서 예외 처리
+        findUser.updateUserPassword(newPassword);
+        saveToFile(directory.resolve(userId.toString() + ".ser"), findUser);
     }
 
     @Override
     public void updateProfileId(UUID userId, UUID newProfileId) {
-        if (existsById(userId)) {
-            super.storage.get(userId).updateProfileId(newProfileId);
-        }
-        saveToFile(directory.resolve(userId.toString() + ".ser"), super.storage.get(userId));
+        User findUser = super.findById(userId);
+        findUser.updateProfileId(newProfileId);
+        saveToFile(directory.resolve(userId.toString() + ".ser"), findUser);
     }
 
     @Override
     public boolean existsByUserName(String userName) {
-        return usernames.contains(userName);
+        if (userName == null) {
+            throw new NullPointerException("userName is null");
+        }
+        return usernames.containsKey(userName);
     }
 
     @Override
     public boolean existsByEmail(String email) {
-        return emails.contains(email);
+        if (email == null) {
+            throw new NullPointerException("email is null");
+        }
+        return emails.containsKey(email);
+    }
+
+    @Override
+    public User findByUserName(String userName) {
+        if (!existsByUserName(userName)) {
+            throw new NoSuchElementException("해당 userName을 가진 user 가 존재하지 않습니다.");
+        }
+        return usernames.get(userName);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        if (!existsByEmail(email)) {
+            throw new NoSuchElementException("해당 email을 가진 user 가 존재하지 않습니다.");
+        }
+        return emails.get(email);
     }
 }
