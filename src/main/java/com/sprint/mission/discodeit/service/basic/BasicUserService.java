7ce1626b551a800e44ef2be25a78a09b4f4bede5
Leekb0804 <lkb8713@gmail.com>
2025-03-14 09:53:30 +0900

    feat(Service): createUser 로직 변경 및 추가
    - userName, userEmail 중복 확인
    - 파라미터 UserCreateRequest DTO 로 받기
    - UserStatus도 같이 생성

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -1,7 +1,12 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.dto.user.UserCreateRequest;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.exception.DuplicateEmailException;
+import com.sprint.mission.discodeit.exception.DuplicateUserNameException;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +18,20 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BasicUserService implements UserService {
     private final UserRepository userRepository;
+    private final UserStatusRepository userStatusRepository;
 
     @Override
-    public User createUser(String userName, String userEmail, String password) {
-        User newUser = new User(userName, userEmail, password); //각 요소에 대한 유효성 검증은 User 생성자에게 맡긴다
+    public User createUser(UserCreateRequest userCreateRequest) {
+        if (userRepository.existsByUserName(userCreateRequest.userName())) {
+            throw new DuplicateUserNameException(userCreateRequest.userName());
+        }
+        if (userRepository.existsByEmail(userCreateRequest.userEmail())) {
+            throw new DuplicateEmailException(userCreateRequest.userEmail());
+        }
+        User newUser = new User(userCreateRequest.userName(), userCreateRequest.userEmail(), userCreateRequest.password(), userCreateRequest.profileId()); //각 요소에 대한 유효성 검증은 User 생성자에게 맡긴다
         this.userRepository.add(newUser);
+        UserStatus newUserStatus = new UserStatus(newUser.getId());
+        this.userStatusRepository.add(newUserStatus);
         return newUser;
     }
 
