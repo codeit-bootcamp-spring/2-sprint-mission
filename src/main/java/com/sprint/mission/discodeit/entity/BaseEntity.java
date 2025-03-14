b7678f57e7e21b951bb51e7a369779a391cbaa85
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-03-14 14:05:46 +0900

    refactor: 시간 타입 long -> Instant

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -8,16 +8,16 @@ import java.util.UUID;
 @Getter
 public abstract class BaseEntity {
     protected final UUID id;
-    protected final Long createdAt;
-    protected Long updatedAt;
+    protected final Instant createdAt;
+    protected Instant updatedAt;
 
     public BaseEntity() {
         this.id = UUID.randomUUID();  // UUID 자동 생성
-        this.createdAt = Instant.now().getEpochSecond();
+        this.createdAt = Instant.now();
         this.updatedAt = this.createdAt;
     }
 
     public void updateTimestamp() {
-        this.updatedAt = Instant.now().getEpochSecond();
+        this.updatedAt = Instant.now();
     }
 }
