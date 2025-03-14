99093cd1a42ae8e7dc343ab731c9fd51450e5adf
Leekb0804 <lkb8713@gmail.com>
2025-03-14 16:45:27 +0900

    feat & refactor(Service): ChannelService의 createPrivateChannel() 메서드의 파라미터를 rivateChannelCreateRequest로 변경

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.dto.channel.PrivateChannelCreateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
@@ -7,7 +8,7 @@ import com.sprint.mission.discodeit.repository.ChannelRepository;
 import java.util.*;
 
 public interface ChannelService {
-    Channel createPrivateChannel(String channelName);
+    Channel createPrivateChannel(PrivateChannelCreateRequest privateChannelCreateRequest);
     Channel createPublicChannel(String channelName);
     Channel readChannel(UUID channelId);
     Map<UUID, Channel> readAllChannels();
