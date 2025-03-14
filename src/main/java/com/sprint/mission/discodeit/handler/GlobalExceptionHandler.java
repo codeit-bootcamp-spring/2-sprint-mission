fd71521b3076934a2f17ee83b0d7ff8eaea9fbb0
Leekb0804 <lkb8713@gmail.com>
2025-03-14 15:17:53 +0900

    feat(Exception): InvalidCredentialsException 추가
    - username 또는 password 로그인 성공여부 확인

--- src/main/java/com/sprint/mission/discodeit/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.handler;
 
 import com.sprint.mission.discodeit.exception.DuplicateEmailException;
 import com.sprint.mission.discodeit.exception.DuplicateUserNameException;
+import com.sprint.mission.discodeit.exception.InvalidCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +16,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateEmailException.class)
     public String handleDuplicateEmail(DuplicateEmailException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public String handleInvalidCredentials(InvalidCredentialsException e) {
         return e.getMessage();
     }
 }
