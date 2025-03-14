742944f53d432fb1c6c499a64cd226b36c4de0a9
Leekb0804 <lkb8713@gmail.com>
2025-03-14 14:13:54 +0900

    feat(Repository): UserStatusRepository 인터페이스에 findUserStatusIdByUserId 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -2,5 +2,8 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.UserStatus;
 
+import java.util.UUID;
+
 public interface UserStatusRepository extends Repository<UserStatus> {
+    UUID findUserStatusIDByUserId(UUID userId);
 }
