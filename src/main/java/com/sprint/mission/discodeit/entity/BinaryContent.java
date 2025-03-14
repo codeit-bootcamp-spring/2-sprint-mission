7590c5f43d78a96c58e753eae34c459c1534c99c
manleKim <kk410910@naver.com>
2025-03-14 14:09:33 +0900

    modify: BinaryContent 도메인 record에서 class로 변경

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -2,10 +2,17 @@ package com.sprint.mission.discodeit.entity;
 
 import java.time.Instant;
 import java.util.UUID;
+import lombok.Getter;
 
-public record BinaryContent(
-        UUID id,
-        Instant createdAt,
-        byte[] binaryImage
-) {
+@Getter
+public class BinaryContent {
+    private final UUID id;
+    private final Instant createdAt;
+    private final byte[] binaryImage;
+
+    public BinaryContent(byte[] binaryImage) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.binaryImage = binaryImage;
+    }
 }
