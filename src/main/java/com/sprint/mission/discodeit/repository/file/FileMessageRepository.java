5a2034a89a99618f658ce13cdce1603bf872db60
Leekb0804 <lkb8713@gmail.com>
2025-03-16 12:46:47 +0900

    feat(Repository): channelId를 key로 가지고 NavigatebleSet을 value로 가지는 channelIdMessages 필드 생성
    - channelId를 이용한 메세지 탐색을 용이하게
    - channelId 별로 정렬된 message들이 시간 순으로 정렬되도록

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -10,14 +10,23 @@ import java.util.stream.Collectors;
 
 @Repository
 public class FileMessageRepository extends AbstractFileRepository<Message> implements MessageRepository {
+    private Map<UUID, NavigableSet<Message>> channelIdMessages;       // channelId를 Key로 가지는 TreeSet<Message> / List가 아니라 NavigatbaleSet을 사용하는 이유는 구간조회가 빠르기 때문
+
     public FileMessageRepository() {
         super(Message.class, Paths.get(System.getProperty("user.dir")).resolve("src\\main\\java\\com\\sprint\\mission\\discodeit\\repository\\file\\messagedata"));
+        channelIdMessages = new HashMap<>();
     }
 
     @Override
     public void add(Message newMessage) {
         super.add(newMessage);
         super.saveToFile(super.directory.resolve(newMessage.getId().toString() + ".ser"), newMessage);
+        channelIdMessages.computeIfAbsent(newMessage.getChannelId(),
+                        id -> new TreeSet<>(
+                                Comparator.comparing(Message::getCreatedAt)     // 생성된 시간 순서대로 정렬 (오름차순, 오래된 메세지가 첫번째)
+                                        .thenComparing(Message::getId)          // 생성된 시간이 동일한 경우 id 순으로 정렬 (예외 처리를 위해, 정렬 순서에 별 의미는 없음)
+                        )
+                ).add(newMessage);
     }
 
     // existsById(),findById(), getAll()  굳이 file을 탐색할 필요 없다고 생각해 storage를 통해 정보 확인, -> 상속 받은걸 사용
@@ -26,6 +35,7 @@ public class FileMessageRepository extends AbstractFileRepository<Message> imple
     public void deleteById(UUID messageId) {
         super.deleteById(messageId);
         super.deleteFile(messageId);
+        channelIdMessages.get(super.findById(messageId).getChannelId()).remove(super.findById(messageId));
     }
 
     public List<Message> findMessageListByChannelId(UUID channelId) {   //해당 channelID를 가진 message가 없을 때, 빈 리스트 반환
