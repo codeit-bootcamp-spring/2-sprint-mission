e5410d8595be66ac1740345373882236520d455f
leejunnyeop <daffodil8620@naver.com>
2025-03-17 19:50:56 +0900

    rename : (UserProfileImgResponse) 이름 변경

--- src/main/java/com/sprint/discodeit/service/UserServiceV1.java
+++ src/main/java/com/sprint/discodeit/service/UserServiceV1.java
@@ -1,6 +1,7 @@
 package com.sprint.discodeit.service;
 
 import com.sprint.discodeit.domain.dto.UserNameResponse;
+import com.sprint.discodeit.domain.dto.UserProfileImgResponseDto;
 import com.sprint.discodeit.domain.dto.UserRequestDto;
 import com.sprint.discodeit.domain.entity.User;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.UUID;
 
 public interface UserServiceV1 {
 
-    UserNameResponse create(UserRequestDto userRequestDto);
+    UserNameResponse create(UserRequestDto userRequestDto, UserProfileImgResponseDto userProfileImgResponseDto);
     User find(UUID userId);
     List<User> findAll();
     User update(UUID userId, String newUsername, String newEmail, String newPassword);
