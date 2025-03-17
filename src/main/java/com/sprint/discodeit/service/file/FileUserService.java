5141ffb238cf7d9352ccfe92dfd688c31d1e7e1d
leejunnyeop <daffodil8620@naver.com>
2025-03-17 19:53:46 +0900

    refactor : (userService) 중복, 닉네임과 이메일 확인 로직

--- src/main/java/com/sprint/discodeit/service/file/FileUserService.java
+++ src/main/java/com/sprint/discodeit/service/file/FileUserService.java
@@ -1,9 +1,11 @@
 package com.sprint.discodeit.service.file;
 
+import com.sprint.discodeit.domain.dto.UserProfileImgResponseDto;
 import com.sprint.discodeit.domain.dto.UserNameResponse;
 import com.sprint.discodeit.domain.mapper.UserMapper;
 import com.sprint.discodeit.domain.dto.UserRequestDto;
 import com.sprint.discodeit.domain.entity.User;
+import com.sprint.discodeit.repository.UserRepository;
 import com.sprint.discodeit.repository.file.FileUserRepository;
 import com.sprint.discodeit.service.UserServiceV1;
 import java.util.List;
@@ -17,12 +19,25 @@ public class FileUserService implements UserServiceV1 {
 
     private final FileUserRepository fileUserRepository;
 
-    @Override
-    public UserNameResponse create(UserRequestDto userRequestDto) {
-        User userMapper = UserMapper.toUserMapper(userRequestDto);
-        fileUserRepository.save(userMapper);
-        return UserMapper.toUserNameResponse(userMapper.getUsername());
+    public UserNameResponse create(UserRequestDto userRequestDto, UserProfileImgResponseDto userProfileImgResponseDto) {
+        // 중복된 유저 확인
+        if (fileUserRepository.findByUsername(userRequestDto.username()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다. 확인 해주세요.");
+        }
+
+        // 이메일과 닉네임이 동일한지 확인
+        if (userRequestDto.username().equals(userRequestDto.email())) {
+            throw new IllegalArgumentException("이메일과 닉네임이 동일합니다. 수정해주세요.");
+        }
+
+        //  User 객체 생성
+        User user = new User(userRequestDto.username(), userRequestDto.email(), userRequestDto.password());
+        fileUserRepository.save(user);
+
+        // User -> UserNameResponse 변환
+        return UserMapper.toUserNameResponse(user.getUsername());
     }
+
 
     @Override
     public User find(UUID userId) {
