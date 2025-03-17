85f3dad6fbadc687ceb222d8aa82fe5f6da126dd
Leekb0804 <lkb8713@gmail.com>
2025-03-17 13:50:10 +0900

    feat(Repository): channelIdMessages 필드 추가, addChannelIdToChannelIdMessage(), add(), deleteById() 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 public class JCFMessageRepository extends AbstractRepository<Message> implements MessageRepository {
     private static volatile JCFMessageRepository instance;         // volatile을 사용하여 변수의 값을 JVM이 캐시하지 않도록 보장
+    private Map<UUID, NavigableSet<Message>> channelIdMessages;
 
     private JCFMessageRepository() {
         super(Message.class, new ConcurrentHashMap<>());
@@ -26,6 +27,32 @@ public class JCFMessageRepository extends AbstractRepository<Message> implements
             }
         }
         return instance;
+    }
+
+    @Override
+    public void addChannelIdToChannelIdMessage(UUID channelId) {
+        if (channelIdMessages.containsKey(channelId)) {         // 자체적인 무결성 확보
+            throw new IllegalArgumentException("이미 존재하는 채널입니다: " + channelId);
+        }
+        channelIdMessages.put(channelId, new TreeSet<>(Comparator.comparing(Message::getCreatedAt)
+                .thenComparing(Message::getId)));
+    }
+
+    @Override
+    public void add(Message newMessage) {
+        super.add(newMessage);
+        channelIdMessages.computeIfAbsent(newMessage.getChannelId(),
+                id -> new TreeSet<>(
+                        Comparator.comparing(Message::getCreatedAt)     // 생성된 시간 순서대로 정렬 (오름차순, 오래된 메세지가 첫번째)
+                                .thenComparing(Message::getId)          // 생성된 시간이 동일한 경우 id 순으로 정렬 (예외 처리를 위해, 정렬 순서에 별 의미는 없음)
+                )
+        ).add(newMessage);
+    }
+
+    @Override
+    public void deleteById(UUID messageId) {
+        super.deleteById(messageId);
+        channelIdMessages.get(super.findById(messageId).getChannelId()).remove(super.findById(messageId));
     }
 
     @Override
