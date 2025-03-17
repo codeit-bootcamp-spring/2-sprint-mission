6efdbe6968dd886c49f66a210a533572dfa6de4d
Leekb0804 <lkb8713@gmail.com>
2025-03-17 13:45:19 +0900

    feat(Repository): addChannelIdToChannelIdMessage() 메서드 추가
    - channelId를 받아 Map<UUID, NavigableSet<Message>> channelIdMessages 에 추가하는 메서드

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface MessageRepository extends Repository<Message> {
+    void addChannelIdToChannelIdMessage(UUID channelId);
     List<Message> findMessageListByChannelId(UUID channelId);
     void updateMessageContent(UUID messageId, String newContent);
 }
