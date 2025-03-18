06042735417f5d9752c5983380c3a49251865ba7
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-03-18 15:04:09 +0900

    feat: ChannelService 고도화

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,14 +1,18 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.dto.channel.ChannelResDto;
+import com.sprint.mission.discodeit.dto.channel.CreateChannelReqDto;
+import com.sprint.mission.discodeit.dto.channel.UpdateChannelReqDto;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ChannelService {
-    void create(Channel channel);
-    Channel findById(UUID channelId);
-    List<Channel> findAll();
+    ChannelResDto createPublicChannel(CreateChannelReqDto.Public createChannelReqDto);
+    ChannelResDto createPrivateChannel(CreateChannelReqDto.Private createChannelReqDto);
+    ChannelResDto find(UUID channelId);
+    List<ChannelResDto> findAll();
+    List<ChannelResDto> findAllByUserId(UUID userId);
+    ChannelResDto update(UUID channelId, UpdateChannelReqDto updateChannelReqDto);
     void delete(UUID channelId);
-    void update(UUID channelId, String name, String description);
 }
