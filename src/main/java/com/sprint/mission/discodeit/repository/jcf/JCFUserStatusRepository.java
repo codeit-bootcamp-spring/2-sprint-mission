a43fb4ee1824f94e48ce3fc07ce3759403361da7
sangyeobhan <sangyeobhandev@gmail.com>
2025-03-18 17:29:33 +0900

    feat: UserStatus jcf 레포 구현

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserStatusRepository.java
@@ -1,36 +1,59 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
+import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public class JCFUserStatusRepository implements UserStatusRepository {
+    private final Map<UUID, UserStatus> data;
+
+    public JCFUserStatusRepository() {
+        this.data = new HashMap<>();
+    }
+
     @Override
     public UserStatus save(UserStatus userStatus) {
-        return null;
+        data.put(userStatus.getId(), userStatus);
+        return userStatus;
     }
 
     @Override
     public Optional<UserStatus> findById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(data.get(id));
     }
 
     @Override
     public Optional<UserStatus> findByUserId(UUID userId) {
-        return Optional.empty();
+        return data.values().stream()
+                .filter(userStatus -> userStatus.getUserId().equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public List<UserStatus> findAll() {
+        return data.values().stream().toList();
     }
 
     @Override
     public boolean existsById(UUID id) {
-        return false;
+        return data.containsKey(id);
     }
 
     @Override
     public void deleteById(UUID id) {
+        data.remove(id);
+    }
 
+    @Override
+    public void deleteByUserId(UUID userId) {
+        List<UUID> keysToRemove = data.values().stream()
+                .filter(userStatus -> userStatus.getUserId().equals(userId))
+                .map(UserStatus::getId)
+                .toList();
+        keysToRemove.forEach(data::remove);
     }
 }
