5a75c27eeaa9004d8a9cf4b9cb40076f17ede98a
Leekb0804 <lkb8713@gmail.com>
2025-03-18 18:17:52 +0900

    refactor(Service): ReadStatusUpdatetRequest 를 ReadStatusUpdateRequest 으로 변경

--- src/main/java/com/sprint/mission/discodeit/service/ReadStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ReadStatusService.java
@@ -2,7 +2,7 @@ package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.readstatus.ReadStatusCreateRequest;
 import com.sprint.mission.discodeit.dto.readstatus.ReadStatusFindResponse;
-import com.sprint.mission.discodeit.dto.readstatus.ReadStatusUpdatetRequest;
+import com.sprint.mission.discodeit.dto.readstatus.ReadStatusUpdateRequest;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,6 +11,6 @@ public interface ReadStatusService {
     UUID createReadStatus(ReadStatusCreateRequest readStatusCreateRequest);
     ReadStatusFindResponse findReadStatus(UUID readStatusId);
     List<ReadStatusFindResponse> findAllReadStatusByUserId(UUID userId);
-    void updateReadStatus(ReadStatusUpdatetRequest readStatusUpdateRequest);
+    void updateReadStatus(ReadStatusUpdateRequest readStatusUpdateRequest);
     void deleteReadStatus(UUID readStatusId);
 }
