346c5e8bd51be6c16f8415fd0caeaa6b90824d73
Leekb0804 <lkb8713@gmail.com>
2025-03-18 18:18:49 +0900

    feat(Repository): updateReadTime(), deleteByUserId(), deleteByChannelId() 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -2,10 +2,14 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 public interface ReadStatusRepository extends Repository<ReadStatus> {
     List<ReadStatus> findByUserId(UUID userId);
     List<ReadStatus> findByChannelId(UUID channelId);
+    void updateReadTime(UUID readStatusId, Instant readTime);
+    void deleteByUserId(UUID userId);
+    void deleteByChannelId(UUID channelId);
 }
