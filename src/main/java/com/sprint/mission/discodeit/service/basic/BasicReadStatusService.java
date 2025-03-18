534c4ff2ff199d2953b4a1a6c9eec54935c2e131
Leekb0804 <lkb8713@gmail.com>
2025-03-18 16:50:12 +0900

    feat(Service): BasicReadStatusService 클래스 추가 및 createReadStatus() 메서드 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
@@ -0,0 +1,57 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.readstatus.ReadStatusCreateRequest;
+import com.sprint.mission.discodeit.dto.readstatus.ReadStatusFindResponse;
+import com.sprint.mission.discodeit.dto.readstatus.ReadStatusUpdatetRequest;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.ReadStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BasicReadStatusService implements ReadStatusService {
+    private final UserRepository userRepository;
+    private final ChannelRepository channelRepository;
+    private final ReadStatusRepository readStatusRepository;
+
+    @Override
+    public UUID createReadStatus(ReadStatusCreateRequest readStatusCreateRequest) {
+        if (!userRepository.existsById(readStatusCreateRequest.userId())) {
+            throw new NoSuchElementException("해당 id를 가진 user가 존재하지 않습니다 : " + readStatusCreateRequest.userId());
+        }
+        if (!channelRepository.existsById(readStatusCreateRequest.channelId())) {
+            throw new NoSuchElementException("해당 id를 가진 channel이 존재하지 않습니다 : " + readStatusCreateRequest.channelId());
+        }
+        ReadStatus newReadStatus = new ReadStatus(readStatusCreateRequest.userId(), readStatusCreateRequest.channelId());
+        readStatusRepository.add(newReadStatus);
+        return newReadStatus.getId();
+    }
+
+    @Override
+    public ReadStatusFindResponse findReadStatus(UUID readStatusId) {
+        return null;
+    }
+
+    @Override
+    public List<ReadStatusFindResponse> findAllReadStatusByUserId(UUID userId) {
+        return List.of();
+    }
+
+    @Override
+    public void updateReadStatus(ReadStatusUpdatetRequest readStatusUpdateRequest) {
+
+    }
+
+    @Override
+    public void deleteReadStatus(UUID readStatusId) {
+
+    }
+}
