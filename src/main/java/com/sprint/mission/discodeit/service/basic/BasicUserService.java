e6a9707586f0f34c3b28bb1b3e3a615ba957f6aa
Leekb0804 <lkb8713@gmail.com>
2025-03-18 17:14:13 +0900

    refactor(Service): findUserStatusIDByUserId() 메서드의 반환타입 UserStatus로 변경으로 인한 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -84,7 +84,7 @@ public class BasicUserService implements UserService {
     @Override
     public void deleteUser(UUID userId) {
         User deleteUser = this.userRepository.findById(userId);
-        this.userStatusRepository.deleteById(userStatusRepository.findUserStatusIDByUserId(deleteUser.getId()));
+        this.userStatusRepository.deleteById(userStatusRepository.findUserStatusIDByUserId(deleteUser.getId()).getId());
         this.readStatusRepository.findByUserId(deleteUser.getId()).forEach(readStatus -> this.readStatusRepository.deleteById(readStatus.getId()));
         this.binaryContentService.deleteByID(deleteUser.getProfileId());
         this.userRepository.deleteById(userId);
