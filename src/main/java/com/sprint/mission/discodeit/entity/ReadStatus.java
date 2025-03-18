fcd14f95c8b0856fb810967967326274ac423d56
Leekb0804 <lkb8713@gmail.com>
2025-03-18 17:27:56 +0900

    feat(Entity): readTime 필드 추가 및 updateReadTime() 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -2,16 +2,23 @@ package com.sprint.mission.discodeit.entity;
 
 import lombok.Getter;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
 public class ReadStatus extends BaseEntity {
     private final UUID userId;
     private final UUID channelId;
+    private Instant readTime;
 
     public ReadStatus(UUID userId, UUID channelId) {
         super();
         this.userId = userId;
         this.channelId = channelId;
+        this.readTime = null;           // null을 넣는게 맞는가?
+    }
+
+    public void updateReadTime(Instant readTime) {
+        this.readTime = readTime;
     }
 }
