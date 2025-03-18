7c629f7458699936fb6e438a4b7e148138ec07ec
sangyeobhan <sangyeobhandev@gmail.com>
2025-03-18 16:50:24 +0900

    feat: ReadStatus jcf 레포 구현

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFReadStatusRepository.java
@@ -1,47 +1,69 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
+import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public class JCFReadStatusRepository implements ReadStatusRepository {
+    private final Map<UUID, ReadStatus> data;
+
+    public JCFReadStatusRepository() {
+        this.data = new HashMap<>();
+    }
+
     @Override
     public ReadStatus save(ReadStatus readStatus) {
-        return null;
+        data.put(readStatus.getId(), readStatus);
+        return readStatus;
     }
 
     @Override
     public Optional<ReadStatus> findById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(data.get(id));
     }
 
     @Override
     public Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId) {
-        return Optional.empty();
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getUserId().equals(userId)
+                        && readStatus.getChannelId().equals(channelId))
+                .findFirst();
     }
 
     @Override
     public List<ReadStatus> findAllByUserId(UUID userId) {
-        return List.of();
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getUserId().equals(userId))
+                .toList();
     }
 
     @Override
     public List<ReadStatus> findAllByChannelId(UUID channelId) {
-        return List.of();
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getChannelId().equals(channelId))
+                .toList();
     }
 
     @Override
     public boolean existsById(UUID id) {
-        return false;
+        return data.containsKey(id);
     }
 
     @Override
     public void deleteById(UUID id) {
+        data.remove(id);
+    }
 
+    @Override
+    public void deleteByChannelId(UUID channelId) {
+        List<UUID> keysToRemove = data.values().stream()
+                .filter(readStatus -> readStatus.getChannelId().equals(channelId))
+                .map(ReadStatus::getId)
+                .toList();
+        keysToRemove.forEach(data::remove);
     }
 }
