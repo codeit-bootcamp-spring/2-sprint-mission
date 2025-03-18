0cff5d413ace283408f6969819e84680c9d133a6
Leekb0804 <lkb8713@gmail.com>
2025-03-18 11:13:18 +0900

    refactor(Service): MessageService의 createMessage(), findAllByChannelId(), updateMessage() 로직 변경

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -1,16 +1,22 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.dto.message.MessageCreateRequest;
+import com.sprint.mission.discodeit.dto.message.MessageUpdateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.provider.MessageUpdaterProvider;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.updater.MessageUpdater;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -21,19 +27,28 @@ public class BasicMessageService implements MessageService {
     private final UserRepository userRepository;
     private final ChannelRepository channelRepository;
     private final MessageRepository messageRepository;
+    private final BinaryContentRepository binaryContentRepository;
+    private final MessageUpdaterProvider messageUpdaterProvider;
 
     @Override
-    public Message createMessage(UUID senderId, String content, UUID channelId) {
-        UserService.validateUserId(senderId, this.userRepository);
-        ChannelService.validateChannelId(channelId, this.channelRepository);
-        Channel channel = channelRepository.findById(channelId);
+    public Message createMessage(MessageCreateRequest messageCreateRequest) {
+        UserService.validateUserId(messageCreateRequest.senderId(), this.userRepository);
+        ChannelService.validateChannelId(messageCreateRequest.channelId(), this.channelRepository);
+        Channel channel = channelRepository.findById(messageCreateRequest.channelId());
         // 해당 채널에 sender가 participant로 있는지 확인하는 코드 필요?
-        if (!channel.getParticipantIds().contains(senderId)) {
+        if (!channel.getParticipantIds().contains(messageCreateRequest.senderId())) {
             throw new NoSuchElementException("해당 senderId를 가진 사용자가 해당 channelId의 Channel에 참여하지 않았습니다.");
         }
-        Message newMessage = new Message(senderId, content, channelId);     // content에 대한 유효성 검증은 Message 생성자에게 맡긴다.
-        channel.setLastMessageTime(newMessage.getCreatedAt());              // 채널에 마지막 메세지 시간 초기화
+        Message newMessage = new Message(messageCreateRequest.senderId(), messageCreateRequest.content(), messageCreateRequest.channelId());     // content에 대한 유효성 검증은 Message 생성자에게 맡긴다.
+        if (messageCreateRequest.attachmentIds() != null && !messageCreateRequest.attachmentIds().isEmpty()) {
+            for (UUID attachmentId : messageCreateRequest.attachmentIds()) {
+                if (binaryContentRepository.existsById(attachmentId)) {
+                    newMessage.addAttachment(attachmentId);
+                }
+            }
+        }
         this.messageRepository.add(newMessage);
+        channel.setLastMessageTime(newMessage.getCreatedAt());              // 채널에 마지막 메세지 시간 초기화
         return newMessage;
     }
 
@@ -43,13 +58,20 @@ public class BasicMessageService implements MessageService {
     }
 
     @Override
-    public Map<UUID, Message> readAllMessages() {
-        return this.messageRepository.getAll();
+    public List<Message> findAllByChannelId(UUID channelId) {
+        return this.messageRepository.findMessageListByChannelId(channelId);
     }
 
     @Override
-    public void updateMessageContent(UUID messageId, String newContent) {
-        this.messageRepository.updateMessageContent(messageId, newContent);
+    public void updateMessage(MessageUpdateRequest messageUpdateRequest) {
+        Message findMessage = this.messageRepository.findById(messageUpdateRequest.messageId());
+        for(UUID attachmentId : messageUpdateRequest.attachmentIds()) {
+            if (!binaryContentRepository.existsById(attachmentId)) {
+                throw new NoSuchElementException("해당 Id가 binaryContentRepository에 존재하지 않습니다 : " + attachmentId);
+            }
+        }
+        List<MessageUpdater> applicableUpdaters = messageUpdaterProvider.getApplicableUpdaters(findMessage, messageUpdateRequest);
+        applicableUpdaters.forEach(updater -> updater.update(findMessage, messageUpdateRequest, this.messageRepository));
     }
 
     @Override
