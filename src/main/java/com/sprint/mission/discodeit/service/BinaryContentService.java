a913a11e6739ae57efd754bb73ac24875c61eb7b
Leekb0804 <lkb8713@gmail.com>
2025-03-18 13:56:15 +0900

    feat(Service): BinaryContentService 인터페이스 추가

---
+++ src/main/java/com/sprint/mission/discodeit/service/BinaryContentService.java
@@ -0,0 +1,14 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentFindResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BinaryContentService {
+    UUID create(BinaryContentCreateRequest binaryContentCreateRequest);
+    BinaryContentFindResponse findById(UUID id);
+    List<BinaryContentFindResponse> findAllByIdIn(List<UUID> ids);
+    void deleteByID(UUID id);
+}
