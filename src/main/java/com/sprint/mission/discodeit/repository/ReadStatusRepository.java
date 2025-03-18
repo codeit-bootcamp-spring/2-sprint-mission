2820327925510601b8ba3bcf8ab233e8756663f7
Leekb0804 <lkb8713@gmail.com>
2025-03-18 16:51:43 +0900

    refactor(Repository): find() 메서드들의 반환값 List<ReadStatus>로 수정

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -2,9 +2,10 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReadStatusRepository extends Repository<ReadStatus> {
-    ReadStatus findByUserId(UUID userId);
-    ReadStatus findByChannelId(UUID channelId);
+    List<ReadStatus> findByUserId(UUID userId);
+    List<ReadStatus> findByChannelId(UUID channelId);
 }
