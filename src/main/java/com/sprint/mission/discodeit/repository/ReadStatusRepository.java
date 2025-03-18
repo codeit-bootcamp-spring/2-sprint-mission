367fb3e4eef2917709d0f9138f8481e223db1a96
Leekb0804 <lkb8713@gmail.com>
2025-03-18 11:18:58 +0900

    feat(Entity): ReadStatus 인터페이스에 findByUserId(), findByChannelId() 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -2,5 +2,9 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
 
+import java.util.UUID;
+
 public interface ReadStatusRepository extends Repository<ReadStatus> {
+    ReadStatus findByUserId(UUID userId);
+    ReadStatus findByChannelId(UUID channelId);
 }
