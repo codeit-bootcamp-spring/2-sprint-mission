2021b97f90537dd317b8fbcddd1a2a8259b47fcd
Leekb0804 <lkb8713@gmail.com>
2025-03-18 11:09:43 +0900

    chore(Handler): GlobalExceptionHandler 클래스에 대한 설명

--- src/main/java/com/sprint/mission/discodeit/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/handler/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import com.sprint.mission.discodeit.exception.InvalidCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// 컨트롤러에 계층에서 발생한 예외를 처리
+// 현재 컨트롤러가 없어서 의미가 없을 듯
+// @Aspect, JoinPoint를 통해 처리를 할까 했지만 별 의미는 없어보임.
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateUserNameException.class)
