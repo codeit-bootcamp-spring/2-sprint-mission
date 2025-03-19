c36fbc222160e5bee49efe66de77003e61ad9632
manleKim <kk410910@naver.com>
2025-03-19 14:15:49 +0900

    test: test 코드 구현

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,7 +1,15 @@
 package com.sprint.mission.discodeit;
 
+import com.sprint.mission.discodeit.dto.channel.ChannelCreatePrivateDto;
+import com.sprint.mission.discodeit.dto.channel.ChannelCreatePublicDto;
+import com.sprint.mission.discodeit.dto.channel.ChannelResponseDto;
+import com.sprint.mission.discodeit.dto.channel.ChannelUpdateDto;
+import com.sprint.mission.discodeit.dto.message.MessageCreateDto;
+import com.sprint.mission.discodeit.dto.message.MessageUpdateDto;
+import com.sprint.mission.discodeit.dto.user.UserCreateDto;
+import com.sprint.mission.discodeit.dto.user.UserResponseDto;
+import com.sprint.mission.discodeit.dto.user.UserUpdateDto;
 import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.ChannelService;
@@ -10,6 +18,7 @@ import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.basic.BasicChannelService;
 import com.sprint.mission.discodeit.service.basic.BasicMessageService;
 import com.sprint.mission.discodeit.service.basic.BasicUserService;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.boot.SpringApplication;
@@ -21,7 +30,8 @@ public class DiscodeitApplication {
     static void userCRUDTest(UserService userService) {
         // 생성
         try {
-            User user = userService.create("sang", "sang@codeit.com", "sang1234");
+            UserCreateDto userCreateDto = new UserCreateDto("sang", "sang@codeit.com", "sang1234", null);
+            User user = userService.create(userCreateDto);
             System.out.println("유저 생성: " + user.getId());
         } catch (RuntimeException e) {
             System.out.println("유저 생성 실패: " + e.getMessage());
@@ -29,64 +39,86 @@ public class DiscodeitApplication {
 
         // 동일한 유저 email로 생성
         try {
-            User user = userService.create("park", "sang@codeit.com", "park1234");
+            UserCreateDto userCreateDto = new UserCreateDto("park", "sang@codeit.com", "park1234", null);
+            User user = userService.create(userCreateDto);
             System.out.println("유저 생성: " + user.getId());
         } catch (RuntimeException e) {
             System.out.println("유저 생성 실패: " + e.getMessage());
         }
 
         // 조회
-        List<User> foundUsers = userService.findAll();
+        List<UserResponseDto> foundUsers = userService.findAll();
         System.out.println("유저 조회(다건): " + foundUsers.size());
 
         try {
-            User user = userService.create("findTest", "findTest@codeit.com", "test1234");
-            User foundUser = userService.findById(user.getId());
-            System.out.println("유저 조회(단건): " + foundUser.getId());
+            UserCreateDto userCreateDto = new UserCreateDto("findTest", "findTest@codeit.com", "test1234", null);
+            User user = userService.create(userCreateDto);
+            UserResponseDto foundUser = userService.findById(user.getId());
+            System.out.println("유저 조회(단건): " + foundUser.id());
         } catch (RuntimeException e) {
             System.out.println("유저 조회(단건) 실패: " + e.getMessage());
         }
 
         // 존재하지 않는 ID 조회
         try {
-            User foundUser = userService.findById(UUID.randomUUID());
-            System.out.println("유저 조회(단건): " + foundUser.getId());
+            UserResponseDto foundUser = userService.findById(UUID.randomUUID());
+            System.out.println("유저 조회(단건): " + foundUser.id());
         } catch (RuntimeException e) {
             System.out.println("유저 조회(단건) 실패: " + e.getMessage());
         }
 
         // 수정
         try {
-            User user = userService.create("updateTest", "updateTest@codeit.com", "updateTest1234");
-            User updatedUser = userService.update(user.getId(), null, null, "woody5678");
+            UserCreateDto userCreateDto = new UserCreateDto("updateTest", "updateTest@codeit.com", "updateTest1234",
+                    null);
+            User user = userService.create(userCreateDto);
+            UserUpdateDto userUpdateDto = new UserUpdateDto(user.getId(), null, null, "woody5678", null);
+            User updatedUser = userService.update(userUpdateDto);
             System.out.println("유저 수정: " + String.join("/", updatedUser.getUsername(), updatedUser.getEmail(),
-                    updatedUser.getPassword()));
+                    updatedUser.getPassword(),
+                    updatedUser.getProfileId() == null ? "null" : updatedUser.getProfileId().toString()));
         } catch (RuntimeException e) {
             System.out.println("유저 수정 실패: " + e.getMessage());
         }
 
         // 존재하지 않는 ID로 수정
         try {
-            User updatedUser = userService.update(UUID.randomUUID(), null, null, "woody5678");
+            UserUpdateDto userUpdateDto = new UserUpdateDto(UUID.randomUUID(), null, null, "woody5678", null);
+            User updatedUser = userService.update(userUpdateDto);
             System.out.println("유저 수정: " + String.join("/", updatedUser.getUsername(), updatedUser.getEmail(),
-                    updatedUser.getPassword()));
+                    updatedUser.getPassword(),
+                    updatedUser.getProfileId() == null ? "null" : updatedUser.getProfileId().toString()));
         } catch (RuntimeException e) {
             System.out.println("유저 수정 실패: " + e.getMessage());
         }
 
         // 삭제
-        User deletedUser = userService.create("deletedTest", "deletedTest@codeit.com", "deletedTest1234");
-        List<User> foundUsersBeforeDelete = userService.findAll();
+        UserCreateDto userCreateDto = new UserCreateDto("deletedTest", "deletedTest@codeit.com", "deletedTest1234",
+                null);
+        User deletedUser = userService.create(userCreateDto);
+        List<UserResponseDto> foundUsersBeforeDelete = userService.findAll();
         System.out.println("유저 삭제 전: " + foundUsersBeforeDelete.size());
         userService.delete(deletedUser.getId());
-        List<User> foundUsersAfterDelete = userService.findAll();
+        List<UserResponseDto> foundUsersAfterDelete = userService.findAll();
         System.out.println("유저 삭제 후: " + foundUsersAfterDelete.size());
     }
 
-    static void channelCRUDTest(ChannelService channelService) {
+    static void channelCRUDTest(ChannelService channelService, UserService userService) {
         // 생성
+
+        // public 채널 생성
         try {
-            Channel channel = channelService.create(ChannelType.PUBLIC, "공지", "공지 채널입니다.");
+            ChannelCreatePublicDto channelCreatePublicDto = new ChannelCreatePublicDto("Public 채널", "Public 채널입니다.");
+            Channel channel = channelService.createPublic(channelCreatePublicDto);
+            System.out.println("채널 생성: " + channel.getId());
+        } catch (RuntimeException e) {
+            System.out.println("채널 생성 실패: " + e.getMessage());
+        }
+
+        // private 채널 생성
+        try {
+            ChannelCreatePrivateDto channelCreatePrivateDto = new ChannelCreatePrivateDto(new ArrayList<>());
+            Channel channel = channelService.createPrivate(channelCreatePrivateDto);
             System.out.println("채널 생성: " + channel.getId());
         } catch (RuntimeException e) {
             System.out.println("채널 생성 실패: " + e.getMessage());
@@ -94,36 +126,46 @@ public class DiscodeitApplication {
 
         // 동일한 채널 이름으로 생성
         try {
-            Channel channel = channelService.create(ChannelType.PRIVATE, "공지", "공지 채널입니다요.");
+            ChannelCreatePublicDto channelCreatePublicDto = new ChannelCreatePublicDto("Public 채널",
+                    "Public 채널 또 만듭니다.");
+            Channel channel = channelService.createPublic(channelCreatePublicDto);
             System.out.println("채널 생성: " + channel.getId());
         } catch (RuntimeException e) {
             System.out.println("채널 생성 실패: " + e.getMessage());
         }
 
         // 조회
-        List<Channel> foundChannels = channelService.findAll();
+        UserCreateDto userCreateDto = new UserCreateDto("test", "test@codeit.com", "test", null);
+        User user = userService.create(userCreateDto);
+
+        ChannelCreatePrivateDto channelCreatePrivateDto = new ChannelCreatePrivateDto(List.of(user));
+        Channel privateChannel = channelService.createPrivate(channelCreatePrivateDto);
+        ChannelCreatePublicDto channelCreatePublicDto = new ChannelCreatePublicDto("testPublic", "testPublic");
+        Channel publicChannel = channelService.createPublic(channelCreatePublicDto);
+
+        List<ChannelResponseDto> foundChannels = channelService.findAllByUserId(user.getId());
         System.out.println("채널 조회(다건): " + foundChannels.size());
 
         try {
-            Channel channel = channelService.create(ChannelType.PUBLIC, "조회 테스트", "조회 테스트 채널입니다.");
-            Channel foundChannel = channelService.findById(channel.getId());
-            System.out.println("채널 조회(단건): " + foundChannel.getId());
+            ChannelResponseDto foundChannel = channelService.findById(privateChannel.getId());
+            System.out.println("채널 조회(단건): " + foundChannel.id());
         } catch (RuntimeException e) {
             System.out.println("채널 조회(단건) 실패: " + e.getMessage());
         }
 
         // 존재하지 않는 ID 조회
         try {
-            Channel foundChannel = channelService.findById(UUID.randomUUID());
-            System.out.println("채널 조회(단건): " + foundChannel.getId());
+            ChannelResponseDto foundChannel = channelService.findById(UUID.randomUUID());
+            System.out.println("채널 조회(단건): " + foundChannel.id());
         } catch (RuntimeException e) {
             System.out.println("채널 조회(단건) 실패: " + e.getMessage());
         }
 
         // 수정
         try {
-            Channel channel = channelService.create(ChannelType.PUBLIC, "수정 테스트", "수정 테스트 채널입니다.");
-            Channel updatedChannel = channelService.update(channel.getId(), "수정 성공 테스트", "수정 성공 테스트 채널입니다.");
+            ChannelUpdateDto channelUpdateDto = new ChannelUpdateDto(publicChannel.getId(), "수정 성공 테스트",
+                    "수정 성공 테스트 채널입니다.");
+            Channel updatedChannel = channelService.update(channelUpdateDto);
             System.out.println("채널 수정: " + String.join("/", updatedChannel.getName(), updatedChannel.getDescription()));
         } catch (RuntimeException e) {
             System.out.println("채널 수정 실패: " + e.getMessage());
@@ -131,27 +173,34 @@ public class DiscodeitApplication {
 
         // 존재하지 않는 ID로 수정
         try {
-            Channel updatedChannel = channelService.update(UUID.randomUUID(), "수정 실패 테스트", "수정 실패 테스트 채널입니다.");
+            Channel updatedChannel = channelService.update(
+                    new ChannelUpdateDto(UUID.randomUUID(), "수정 실패 테스트", "수정 실패 테스트 채널입니다."));
             System.out.println("채널 수정: " + String.join("/", updatedChannel.getName(), updatedChannel.getDescription()));
         } catch (RuntimeException e) {
             System.out.println("채널 수정 실패: " + e.getMessage());
         }
 
         // 삭제
-        Channel deletedChannel = channelService.create(ChannelType.PUBLIC, "삭제 테스트", "삭제 테스트 채널입니다.");
-        List<Channel> foundChannelsBeforeDelete = channelService.findAll();
+        List<ChannelResponseDto> foundChannelsBeforeDelete = channelService.findAllByUserId(user.getId());
         System.out.println("채널 삭제 전: " + foundChannelsBeforeDelete.size());
-        channelService.delete(deletedChannel.getId());
-        List<Channel> foundChannelsAfterDelete = channelService.findAll();
+        channelService.delete(publicChannel.getId());
+        List<ChannelResponseDto> foundChannelsAfterDelete = channelService.findAllByUserId(user.getId());
         System.out.println("채널 삭제: " + foundChannelsAfterDelete.size());
     }
 
     static void messageCRUDTest(MessageService messageService, UserService userService, ChannelService channelService) {
-        User user = userService.create("메세지 기본 유저 1", "메세지 기본 유저 1@codeit.com", "메세지 기본 유저 1234");
-        Channel channel = channelService.create(ChannelType.PUBLIC, "메세지 테스트", "메세지 테스트 채널입니다.");
+        UserCreateDto userCreateDto = new UserCreateDto("MessageTester", "test@codeit.com", "MessageTester", null);
+        User user = userService.create(userCreateDto);
+
+        ChannelCreatePrivateDto channelCreatePrivateDto = new ChannelCreatePrivateDto(List.of(user));
+        Channel privateChannel = channelService.createPrivate(channelCreatePrivateDto);
+//        ChannelCreatePublicDto channelCreatePublicDto = new ChannelCreatePublicDto("testPublic", "testPublic");
+//        Channel publicChannel = channelService.createPublic(channelCreatePublicDto);
         // 생성
         try {
-            Message message = messageService.create(user.getId(), channel.getId(), "안녕하세요.");
+            MessageCreateDto messageCreateDto = new MessageCreateDto(user.getId(), privateChannel.getId(), "안녕하세요.",
+                    null);
+            Message message = messageService.create(messageCreateDto);
             System.out.println("메시지 생성: " + message.getId());
         } catch (RuntimeException e) {
             System.out.println("메시지 생성 실패: " + e.getMessage());
@@ -159,7 +208,8 @@ public class DiscodeitApplication {
 
         // 존재하지 않는 유저로 생성
         try {
-            Message message = messageService.create(UUID.randomUUID(), channel.getId(), "안녕하세요.");
+            Message message = messageService.create(
+                    new MessageCreateDto(UUID.randomUUID(), privateChannel.getId(), "안녕하세요.", null));
             System.out.println("메시지 생성: " + message.getId());
         } catch (RuntimeException e) {
             System.out.println("메시지 생성 실패: " + e.getMessage());
@@ -167,18 +217,26 @@ public class DiscodeitApplication {
 
         // 존재하지 않는 채널로 생성
         try {
-            Message message = messageService.create(user.getId(), UUID.randomUUID(), "안녕하세요.");
+            Message message = messageService.create(
+                    new MessageCreateDto(user.getId(), UUID.randomUUID(), "안녕하세요.", null));
             System.out.println("메시지 생성: " + message.getId());
         } catch (RuntimeException e) {
             System.out.println("메시지 생성 실패: " + e.getMessage());
         }
 
         // 조회
-        List<Message> foundMessages = messageService.findAll();
-        System.out.println("메시지 조회(다건): " + foundMessages.size());
+
+        // 채널 ID로 조회
+        List<Message> foundMessagesByChannelId = messageService.findAllByChannelId(privateChannel.getId());
+        System.out.println("채널 내 메시지 조회(다건): " + foundMessagesByChannelId.size());
+
+        // 유저 ID로 조회
+        List<Message> foundMessagesByAuthorId = messageService.findAllByAuthorId(user.getId());
+        System.out.println("유저가 쓴 메시지 조회(다건): " + foundMessagesByAuthorId.size());
 
         try {
-            Message message = messageService.create(user.getId(), channel.getId(), "메시지 조회 테스트");
+            Message message = messageService.create(
+                    new MessageCreateDto(user.getId(), privateChannel.getId(), "메시지 조회 테스트", null));
             Message foundMessage = messageService.findById(message.getId());
             System.out.println("메시지 조회(단건): " + foundMessage.getId());
         } catch (RuntimeException e) {
@@ -193,18 +251,11 @@ public class DiscodeitApplication {
             System.out.println("메시지 조회(단건) 실패: " + e.getMessage());
         }
 
-        // 채널 ID로 조회
-        List<Message> foundMessagesByChannelId = messageService.findByChannelId(channel.getId());
-        System.out.println("메시지 채널 ID로 조회: " + foundMessagesByChannelId.size());
-
-        // 유저 ID로 조회
-        List<Message> foundMessagesByAuthorId = messageService.findByAuthorId(user.getId());
-        System.out.println("메시지 유저 ID로 조회: " + foundMessagesByAuthorId.size());
-
         // 수정
         try {
-            Message message = messageService.create(user.getId(), channel.getId(), "메시지 수정 테스트");
-            Message updatedMessage = messageService.update(message.getId(), "메시지 수정 성공 테스트입니다.");
+            Message message = messageService.create(
+                    new MessageCreateDto(user.getId(), privateChannel.getId(), "메시지 수정 테스트", null));
+            Message updatedMessage = messageService.update(new MessageUpdateDto(message.getId(), "메시지 수정 성공 테스트입니다."));
             System.out.println("메시지 수정: " + updatedMessage.getContent());
         } catch (RuntimeException e) {
             System.out.println("메시지 수정 실패: " + e.getMessage());
@@ -212,18 +263,20 @@ public class DiscodeitApplication {
 
         // 존재하지 않는 ID로 수정
         try {
-            Message updatedMessage = messageService.update(UUID.randomUUID(), "메시지 수정 실패 테스트입니다.");
+            Message updatedMessage = messageService.update(
+                    new MessageUpdateDto(UUID.randomUUID(), "메시지 수정 실패 테스트입니다."));
             System.out.println("메시지 수정: " + updatedMessage.getContent());
         } catch (RuntimeException e) {
             System.out.println("메시지 수정 실패: " + e.getMessage());
         }
 
         // 삭제
-        Message deletedMessage = messageService.create(user.getId(), channel.getId(), "메시지 삭제 테스트");
-        List<Message> foundMessagesBeforeDelete = messageService.findAll();
+        Message deletedMessage = messageService.create(
+                new MessageCreateDto(user.getId(), privateChannel.getId(), "메시지 삭제 테스트", null));
+        List<Message> foundMessagesBeforeDelete = messageService.findAllByChannelId(privateChannel.getId());
         System.out.println("메시지 삭제 전: " + foundMessagesBeforeDelete.size());
         messageService.delete(deletedMessage.getId());
-        List<Message> foundMessagesAfterDelete = messageService.findAll();
+        List<Message> foundMessagesAfterDelete = messageService.findAllByChannelId(privateChannel.getId());
         System.out.println("메시지 삭제: " + foundMessagesAfterDelete.size());
     }
 
@@ -237,7 +290,7 @@ public class DiscodeitApplication {
 
         // 테스트
         userCRUDTest(userService);
-        channelCRUDTest(channelService);
+        channelCRUDTest(channelService, userService);
         messageCRUDTest(messageService, userService, channelService);
     }
 
