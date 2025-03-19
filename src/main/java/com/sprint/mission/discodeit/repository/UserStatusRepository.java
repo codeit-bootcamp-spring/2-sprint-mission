f32e77ba84da5c1ec95c498ce2886f07bd491df1
Leekb0804 <lkb8713@gmail.com>
2025-03-19 10:30:06 +0900

    feat(Repository): existsByUserId() 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -5,5 +5,6 @@ import com.sprint.mission.discodeit.entity.UserStatus;
 import java.util.UUID;
 
 public interface UserStatusRepository extends Repository<UserStatus> {
+    boolean existsByUserId(UUID userId);
     UserStatus findUserStatusIDByUserId(UUID userId);
 }
