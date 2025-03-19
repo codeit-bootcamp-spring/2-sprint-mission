c001a1f791c578ed4363e322aea2e09b9a504339
Leekb0804 <lkb8713@gmail.com>
2025-03-19 14:46:22 +0900

    refactor(Repository): @Repository Bean 설정 후 싱글톤 패턴 코드 제거

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -4,28 +4,16 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.model.ChannelType;
 import com.sprint.mission.discodeit.repository.AbstractRepository;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+@Repository
 public class JCFChannelRepository extends AbstractRepository<Channel> implements ChannelRepository {
-    private static volatile JCFChannelRepository instance;         // volatile을 사용하여 변수의 값을 JVM이 캐시하지 않도록 보장
-
     private JCFChannelRepository() {
         super(Channel.class, new ConcurrentHashMap<>());
-    }
-
-    public static JCFChannelRepository getInstance() {
-        // 첫 번째 null 체크 (성능 최적화)
-        if (instance == null) {
-            synchronized (JCFChannelRepository.class) {
-                // 두 번째 null 체크 (동기화 구간 안에서 중복 생성 방지)
-                if (instance == null) {
-                    instance = new JCFChannelRepository();
-                }
-            }
-        }
-        return instance;
     }
 
     @Override
