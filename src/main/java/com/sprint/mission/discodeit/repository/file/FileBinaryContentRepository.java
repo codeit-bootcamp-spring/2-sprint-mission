9d3b87f1a2a55f4c32f4c919ed0d31cb71e02d8c
Leekb0804 <lkb8713@gmail.com>
2025-03-19 13:52:38 +0900

    feat(Repository): findAllByIdIn() 메서드 구현

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileBinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileBinaryContentRepository.java
@@ -5,6 +5,9 @@ import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import org.springframework.stereotype.Repository;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class FileBinaryContentRepository extends AbstractFileRepository<BinaryContent> implements BinaryContentRepository {
@@ -12,5 +15,13 @@ public class FileBinaryContentRepository extends AbstractFileRepository<BinaryCo
         super(BinaryContent.class, Paths.get(System.getProperty("BinaryContent.dir")).resolve("src\\main\\java\\com\\sprint\\mission\\discodeit\\repository\\file\\BinaryContentData"));      // 현재 프로그램이 실행되고 있는 디렉토리로 설정);
     }
 
-    //
+    @Override
+    public List<BinaryContent> findAllByIdIn(List<UUID> ids) {
+        List<BinaryContent> responses = new ArrayList<>();
+        for(UUID id : ids) {
+            BinaryContent binaryContent = findById(id);
+            responses.add(binaryContent);
+        }
+        return responses;
+    }
 }
