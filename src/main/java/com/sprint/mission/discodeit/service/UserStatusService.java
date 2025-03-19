4f33d7f4a0212c0a6f039e7baa3cb1f91c784ca5
Leekb0804 <lkb8713@gmail.com>
2025-03-19 11:23:42 +0900

    refactor(Service): updateUserStatusByUserId() 메서드 파라미터 DTO로 변경

--- src/main/java/com/sprint/mission/discodeit/service/UserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserStatusService.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.userstatus.UserStatusCreateRequest;
 import com.sprint.mission.discodeit.dto.userstatus.UserStatusFindResponse;
+import com.sprint.mission.discodeit.dto.userstatus.UserStatusUpdateByUserIdRequest;
 import com.sprint.mission.discodeit.dto.userstatus.UserStatusUpdateRequest;
 
 import java.util.List;
@@ -12,6 +13,6 @@ public interface UserStatusService {
     UserStatusFindResponse findUserStatus(UUID id);
     List<UserStatusFindResponse> findAllUserStatus();
     void updateUserStatus(UserStatusUpdateRequest userStatusUpdateRequest);
-    void updateUserStatusByUserId(UUID userId, UserStatusUpdateRequest userStatusUpdateRequest);
+    void updateUserStatusByUserId(UserStatusUpdateByUserIdRequest userStatusUpdateByUserIdRequest);
     void deleteUserStatus(UUID id);
 }
