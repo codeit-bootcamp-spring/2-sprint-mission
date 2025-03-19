3b45e2c94a6baac4752e3b89328c1385ca5f4fd2
Leekb0804 <lkb8713@gmail.com>
2025-03-19 14:48:03 +0900

    refactor(Repository): @Repository Bean 설정 후 싱글톤 패턴 코드 제거

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -3,30 +3,18 @@ package com.sprint.mission.discodeit.repository.jcf;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.repository.AbstractRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+@Repository
 public class JCFMessageRepository extends AbstractRepository<Message> implements MessageRepository {
-    private static volatile JCFMessageRepository instance;         // volatile을 사용하여 변수의 값을 JVM이 캐시하지 않도록 보장
     private Map<UUID, NavigableSet<Message>> channelIdMessages;
 
     private JCFMessageRepository() {
         super(Message.class, new ConcurrentHashMap<>());
-    }
-
-    public static JCFMessageRepository getInstance() {
-        // 첫 번째 null 체크 (성능 최적화)
-        if (instance == null) {
-            synchronized (JCFMessageRepository.class) {
-                // 두 번째 null 체크 (동기화 구간 안에서 중복 생성 방지)
-                if (instance == null) {
-                    instance = new JCFMessageRepository();
-                }
-            }
-        }
-        return instance;
     }
 
     @Override
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -3,28 +3,16 @@ package com.sprint.mission.discodeit.repository.jcf;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.AbstractRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class JCFUserRepository extends AbstractRepository<User> implements UserRepository {
-    private static volatile JCFUserRepository instance;         // volatile을 사용하여 변수의 값을 JVM이 캐시하지 않도록 보장
 
     private JCFUserRepository() {
         super(User.class, new ConcurrentHashMap<>());
-    }
-
-    public static JCFUserRepository getInstance() {
-        // 첫 번째 null 체크 (성능 최적화)
-        if (instance == null) {
-            synchronized (JCFUserRepository.class) {
-                // 두 번째 null 체크 (동기화 구간 안에서 중복 생성 방지)
-                if (instance == null) {
-                    instance = new JCFUserRepository();
-                }
-            }
-        }
-        return instance;
     }
 
     @Override
