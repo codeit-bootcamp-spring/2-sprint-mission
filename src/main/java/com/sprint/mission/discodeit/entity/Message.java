3b9cbf54f59d456af340955e93af9ca28ad67994
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-03-20 14:10:03 +0900

    refactor: Message에 attachmentIds List추가

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -3,6 +3,8 @@ package com.sprint.mission.discodeit.entity;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -12,17 +14,23 @@ public class Message extends BaseEntity implements Serializable {
     private final UUID senderId;  // User의 UUID 저장
     private final UUID channelId; // Channel의 UUID 저장
     private String content;
+    private List<UUID> attachmentIds;
 
     public Message(UUID senderId, UUID channelId, String content) {
         super();
         this.senderId = senderId;
         this.channelId = channelId;
         this.content = content;
+        this.attachmentIds = new ArrayList<>();
     }
 
     public void updateContent(String content) {
         this.content = content;
         updateTimestamp();
+    }
+
+    public void addAttachment(UUID attachmentId) {
+        this.attachmentIds.add(attachmentId);
     }
 
     @Override
