115413ba5e021761f3fe5130cabb8099640078f4
정종현 <bellhyunforgithub@gmail.com>
2025-03-20 18:21:44 +0900

    Feat: UserStatus, ReadStatus, BinaryContent Repository 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFReadStatusRepository.java
@@ -0,0 +1,46 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.domain.ReadStatus;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+@ConditionalOnProperty(name = "discodeit.repository.type", havingValue = "jcf", matchIfMissing = true)
+public class JCFReadStatusRepository implements ReadStatusRepository {
+    private final Map<UUID, ReadStatus> data;
+
+    public JCFReadStatusRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public ReadStatus save(ReadStatus readStatus) {
+        this.data.put(readStatus.getId(), readStatus);
+        return readStatus;
+    }
+
+    @Override
+    public Optional<ReadStatus> findById(UUID id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    @Override
+    public List<ReadStatus> findByUserId(UUID userId) {
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getUserId().equals(userId))
+                .toList();
+    }
+
+    @Override
+    public List<ReadStatus> findAll() {
+        return data.values().stream().toList();
+    }
+
+    @Override
+    public void delete(UUID id) {
+        data.remove(id);
+    }
+}
