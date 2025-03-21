ecc1b046c2448e5d391b31d2e60e63ed2ba20701
seonseon933 <seonseon933@gmail.com>
2025-03-21 10:17:26 +0900

    Refactor : AuthService인터페이스 생성

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/AuthService.java
@@ -1,23 +1,9 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.dto.user.LoginRequest;
-import java.util.NoSuchElementException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class AuthService {
-    private final UserRepository userRepository;
 
-    public User login(LoginRequest loginRequest) {
-        User user = userRepository.findByUsername(loginRequest.username())
-                .orElseThrow(() -> new NoSuchElementException(loginRequest.username() + " 와 동일한 username이 없음"));
-        if (!user.getPassword().equals(loginRequest.password())) {
-            throw new IllegalArgumentException("login 실패");
-        }
-        return user;
-    }
+public interface AuthService {
+    User login(LoginRequest loginRequest);
 }
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.dto.user.LoginRequest;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BasicAuthService implements AuthService {
+    private final UserRepository userRepository;
+
+    public User login(LoginRequest loginRequest) {
+        User user = userRepository.findByUsername(loginRequest.username())
+                .orElseThrow(() -> new NoSuchElementException(loginRequest.username() + " 와 동일한 username이 없음"));
+        if (!user.getPassword().equals(loginRequest.password())) {
+            throw new IllegalArgumentException("login 실패");
+        }
+        return user;
+    }
+}
