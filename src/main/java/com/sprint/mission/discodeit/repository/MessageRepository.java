10ec740fb3911d755dbd36bf8142fdbd12919d78
ddday366 <ddday366@gmail.com>
2025-03-24 01:22:08 +0900

    fix: MessageRepository 수정

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -1,13 +1,15 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Message;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MessageRepository {
     void save(Message message);
-    Message findById(UUID id);
+    Optional<Message> findById(UUID id);
     List<Message> findAll();
     void delete(UUID id);
 }
