dbf84fd7e63a0a3494a7fb6610e6771c1dbf6307
ddday366 <ddday366@gmail.com>
2025-03-24 01:24:06 +0900

    fix: FileUserService 수정

--- src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -1,9 +1,10 @@
 package com.sprint.mission.discodeit.service.file;
 
+import com.sprint.mission.discodeit.DTO.UserDTO;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,27 +12,21 @@ import java.util.UUID;
 public class FileUserService implements UserService {
     private final UserRepository userRepository;
 
-    public FileUserService() {
-        this.userRepository = new FileUserRepository();
+    public FileUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
-    public User createUser(String userName) {
-        User user = new User(userName);
-        if (user.getId() == null) {
-            user.setId(UUID.randomUUID());
-        }
+    public User createUser(UserDTO userDTO) {
+        User user = new User(userDTO.username());
         userRepository.save(user);
         return user;
     }
 
     @Override
     public User getUser(UUID id) {
-        User user = userRepository.findById(id);
-        if (user == null) {
-            throw new IllegalArgumentException("존재하지 않는 유저입니다.");
-        }
-        return user;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
     }
 
     @Override
