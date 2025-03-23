7bb2d368e9edc023d5a5f79d7ca04541878cae40
ddday366 <ddday366@gmail.com>
2025-03-24 01:24:28 +0900

    fix: FileMessageService 수정

--- src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
@@ -1,34 +1,36 @@
 package com.sprint.mission.discodeit.service.file;
 
+import com.sprint.mission.discodeit.DTO.MessageDTO;
 import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
 import com.sprint.mission.discodeit.service.MessageService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 public class FileMessageService implements MessageService {
-    private final MessageRepository messageRepository;
+    private final FileMessageRepository messageRepository;
 
     public FileMessageService() {
         this.messageRepository = new FileMessageRepository();
     }
 
     @Override
-    public Message createMessage(UUID channelId, UUID userId, String content) {
-        Message message = new Message(channelId, userId, content);
+    public Message createMessage(MessageDTO messageDTO) {
+        Message message = new Message(
+                messageDTO.senderId(),
+                messageDTO.channelId(),
+                messageDTO.content()
+        );
         messageRepository.save(message);
         return message;
     }
 
     @Override
     public Message getMessage(UUID id) {
-        Message message = messageRepository.findById(id);
-        if (message == null) {
-            throw new IllegalArgumentException("존재하지 않는 메세지입니다.");
-        }
-        return message;
+        return messageRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메시지입니다."));
     }
 
     @Override
