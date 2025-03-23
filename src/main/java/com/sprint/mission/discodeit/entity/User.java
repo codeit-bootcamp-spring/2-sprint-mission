015c0cb23be1a8a230b89ca0b7109af98955fc83
ddday366 <ddday366@gmail.com>
2025-03-24 01:21:47 +0900

    fix: User 수정

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,49 +1,71 @@
 package com.sprint.mission.discodeit.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    protected final UUID id;
+    protected Instant createAt;
+    protected Instant updateAt;
+
     private String userName;
-    protected UUID id;
-    protected Long createAt;
-    protected Long updateAt;
+    private String email;
+    private UserStatus status;
+    private BinaryContent profileImage;
 
     public User() {
         this.id = UUID.randomUUID();
-        this.createAt = System.currentTimeMillis();
+        this.createAt = Instant.now();
         this.updateAt = createAt;
         this.userName = "Unknown";
     }
 
-    public UUID getId() {
-        return id;
+    public User(String userName, String email, UserStatus status) {
+        this();
+        this.userName = userName;
+        this.email = email;
+        this.status = status;
     }
 
-    public void setId(UUID id) {
+    public User(UUID id, String userName, String email, Instant createAt, Instant updateAt, UserStatus status) {
         this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.status = status;
     }
 
-    public Long getCreateAt() {
-        return createAt;
-    }
-
-    public Long getUpdateAt() {
-        return updateAt;
-    }
-
-
-    public User (String userName) {
+    public User(String userName) {
         this();
         this.userName = userName;
     }
 
-    public String getUsername() {
-        return userName;
-    }
-
     public void updateUsername(String newUserName) {
         this.userName = newUserName;
+    }
+
+    public long getCreateAtMillis() {
+        return createAt.toEpochMilli();
+    }
+
+    public void setCreateAtMillis(long createAtMillis) {
+        this.createAt = Instant.ofEpochMilli(createAtMillis);
+    }
+
+    public long getUpdateAtMillis() {
+        return updateAt.toEpochMilli();
+    }
+
+    public void setUpdateAtMillis(long updateAtMillis) {
+        this.updateAt = Instant.ofEpochMilli(updateAtMillis);
     }
 }
