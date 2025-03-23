b686de01b8f15b76b175ad1f909ea3f5c809be25
ddday366 <ddday366@gmail.com>
2025-03-24 01:25:23 +0900

    fix: ChannelRepository 수정

--- src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -1,13 +1,15 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Channel;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ChannelRepository {
     void save(Channel channel);
-    Channel findById(UUID id);
+    Optional<Channel> findById(UUID id);
     List<Channel> findAll();
     void delete(UUID id);
 }
