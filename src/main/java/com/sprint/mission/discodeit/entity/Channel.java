38ddd8015e62eb313527c04bbc9ff7052eecab29
ddday366 <ddday366@gmail.com>
2025-03-24 01:25:32 +0900

    fix: Channel 수정

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,44 +1,41 @@
 package com.sprint.mission.discodeit.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Channel implements Serializable {
     private static final long serialVersionUID = 1L;
     private String channelName;
-    protected UUID id;
-    protected Long createAt;
-    protected Long updateAt;
+    protected final UUID id;
+    protected final Instant createAt;
+    protected Instant updateAt;
+    private boolean isPublic;
+    private Set<User> participants = new HashSet<>();
 
     public Channel() {
         this.id = UUID.randomUUID();
-        this.createAt = System.currentTimeMillis();
+        this.createAt = Instant.now();
         this.updateAt = createAt;
         this.channelName = "Unknown";
+        this.isPublic = true;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public Long getCreateAt() {
-        return createAt;
-    }
-
-    public Long getUpdateAt() {
-        return updateAt;
-    }
-
-    public Channel(String channelName) {
+    public Channel(String channelName, boolean isPublic) {
         this();
         this.channelName = channelName;
-    }
-
-    public String getChannelName() {
-        return channelName;
+        this.isPublic = isPublic;
     }
 
     public void updateChannelName(String newChannelName) {
         this.channelName = newChannelName;
+        this.updateAt = Instant.now();
     }
 }
