5a7605cf1d9fdf02b8afb450b459c0181449628d
ddday366 <ddday366@gmail.com>
2025-03-24 01:21:32 +0900

    fix: UserService 수정

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,12 +1,14 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.DTO.UserDTO;
 import com.sprint.mission.discodeit.entity.User;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    User createUser(String username);
+    User createUser(UserDTO userDTO);
     User getUser(UUID id);
     List<User> getAllUsers();
 
