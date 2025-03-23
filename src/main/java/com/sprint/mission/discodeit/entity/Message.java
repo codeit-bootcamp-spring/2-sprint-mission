1c7805e29dc8f9649b9d7b3a226d7695fc38e690
ddday366 <ddday366@gmail.com>
2025-03-24 01:22:18 +0900

    fix: Message 수정

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,16 +1,20 @@
 package com.sprint.mission.discodeit.entity;
 
+import lombok.Getter;
+
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
+@Getter
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
-    private UUID userId;
-    private UUID channelId;
+    private final UUID userId;
+    private final UUID channelId;
     private String content;
-    protected UUID id;
-    protected Long createAt;
-    protected Long updateAt;
+    protected final UUID id;
+    protected final Instant createAt;
+    protected Instant updateAt;
 
     public Message(UUID userId, UUID channelId, String content) {
         if (content == null || content.trim().isEmpty()) {
@@ -20,32 +24,8 @@ public class Message implements Serializable {
         this.userId = userId;
         this.channelId = channelId;
         this.content = content;
-        this.createAt = System.currentTimeMillis();
+        this.createAt = Instant.now();
         this.updateAt = createAt;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public Long getCreateAt() {
-        return createAt;
-    }
-
-    public Long getUpdateAt() {
-        return updateAt;
-    }
-
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public UUID getChannelId() {
-        return channelId;
-    }
-
-    public String getContent() {
-        return content;
     }
 
     public void updateContent(String newContent) {
@@ -53,6 +33,6 @@ public class Message implements Serializable {
             throw new IllegalArgumentException("Content cannot be null or empty");
         }
         this.content = newContent;
-        this.updateAt = System.currentTimeMillis();
+        this.updateAt = Instant.now();
     }
 }
