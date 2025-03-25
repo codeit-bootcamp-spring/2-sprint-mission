bb16bf6880bbcab2ad67fd6210c30e41644276be
sangyeobhan <sangyeobhandev@gmail.com>
2025-03-25 18:15:58 +0900

    feat: user 컨트롤러 완성

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -3,16 +3,19 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.common.ApiResponse;
 import com.sprint.mission.discodeit.dto.user.UserCreateRequest;
 import com.sprint.mission.discodeit.dto.user.UserCreateResponse;
+import com.sprint.mission.discodeit.dto.user.UserResponse;
+import com.sprint.mission.discodeit.dto.userstatus.UserStatusUpdateResponse;
+import com.sprint.mission.discodeit.entity.user.UserStatus;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.UserStatusService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,7 +27,44 @@ public class UserController {
     @PostMapping
     public ResponseEntity<ApiResponse<UserCreateResponse>> create(@Valid @RequestBody UserCreateRequest request) {
         UserCreateResponse response = userService.create(request);
-        ApiResponse<UserCreateResponse> apiResponse = new ApiResponse<>("유저 생성", response);
+        ApiResponse<UserCreateResponse> apiResponse = new ApiResponse<>("유저 생성 성송", response);
         return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ApiResponse<UserResponse>> find(@PathVariable UUID userId) {
+        UserResponse response = userService.find(userId);
+        ApiResponse<UserResponse> apiResponse = new ApiResponse<>("유저 조회 성공", response);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @GetMapping({"", "/"})
+    public ResponseEntity<ApiResponse<List<UserResponse>>> findAll() {
+        List<UserResponse> response = userService.findAll();
+        ApiResponse<List<UserResponse>> apiResponse = new ApiResponse<>("유저 목록 조회 성공", response);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable UUID userId) {
+        userService.delete(userId);
+        ApiResponse<Void> apiResponse = new ApiResponse<>("유저 삭제 성공", null);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PutMapping("/{userId}/status")
+    public ResponseEntity<ApiResponse<UserStatusUpdateResponse>> updateStatus(@PathVariable UUID userId) {
+        UserStatus updatedStatus = userStatusService.updateByUserId(userId);
+        UserResponse userResponse = userService.find(userId);
+
+        UserStatusUpdateResponse response = new UserStatusUpdateResponse(
+                userId,
+                userResponse.username(),
+                userResponse.isOnline(),
+                updatedStatus.getLastLoginAt()
+        );
+
+        ApiResponse<UserStatusUpdateResponse> apiResponse = new ApiResponse<>("사용자 온라인 상태 업데이트 성공", response);
+        return ResponseEntity.ok(apiResponse);
     }
 }
