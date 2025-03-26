4525fe4aa684e0ee90b3de39d8b071c47f09893d
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-03-26 14:47:25 +0900

    feat: 인증 및 토큰 관리 기능 구현

---
+++ src/main/java/com/sprint/mission/discodeit/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.config;
+
+import com.sprint.mission.discodeit.jwt.JwtAuthInterceptor;
+import io.micrometer.common.lang.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtAuthInterceptor jwtAuthInterceptor;
+
+    public WebConfig(JwtAuthInterceptor jwtAuthInterceptor) {
+        this.jwtAuthInterceptor = jwtAuthInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(jwtAuthInterceptor)
+                .addPathPatterns("/api/**");
+    }
+}
