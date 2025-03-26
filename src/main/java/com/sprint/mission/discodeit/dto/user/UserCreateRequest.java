9f3206d1025897c714ad0a24b30d17c57b036df4
Leekb0804 <lkb8713@gmail.com>
2025-03-26 10:43:05 +0900

    refactor(DTO): UserCreateRequest 의 BinaryContentCreateRequest 필드를 profileId 로 변경

--- src/main/java/com/sprint/mission/discodeit/dto/user/UserCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/UserCreateRequest.java
@@ -1,11 +1,11 @@
 package com.sprint.mission.discodeit.dto.user;
 
-import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentCreateRequest;
+import java.util.UUID;
 
 public record UserCreateRequest(
         String userName,
         String userEmail,
         String password,
-        BinaryContentCreateRequest profileImage
+        UUID profileId
 ) {
 }
