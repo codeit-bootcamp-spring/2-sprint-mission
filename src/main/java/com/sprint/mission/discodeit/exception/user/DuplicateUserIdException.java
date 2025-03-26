9ef9a90354a57ced6ef86f389140821ea9702c2b
Leekb0804 <lkb8713@gmail.com>
2025-03-26 14:46:03 +0900

    refactor(Exception) : message의 일관성을 위해 수정

--- src/main/java/com/sprint/mission/discodeit/exception/user/DuplicateUserIdException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/user/DuplicateUserIdException.java
@@ -4,6 +4,6 @@ import java.util.UUID;
 
 public class DuplicateUserIdException extends RuntimeException {
     public DuplicateUserIdException(UUID userId) {
-        super("이미 등록된 userId 입니다 : " + userId.toString());
+        super("이미 사용중인 userId 입니다 : " + userId.toString());
     }
 }
