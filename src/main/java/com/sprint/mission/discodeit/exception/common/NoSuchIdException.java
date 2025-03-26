e6da97627a0c40ad7097b6cbfaceb0ac2a5fe148
Leekb0804 <lkb8713@gmail.com>
2025-03-26 15:22:40 +0900

    feat(Exception) : NoSuchUserNameException, NoSuchUserEmailException, NoSuchIdException 추가

---
+++ src/main/java/com/sprint/mission/discodeit/exception/common/NoSuchIdException.java
@@ -0,0 +1,7 @@
+package com.sprint.mission.discodeit.exception.common;
+
+public class NoSuchIdException extends RuntimeException {
+    public NoSuchIdException(String message) {
+        super(message);
+    }
+}
