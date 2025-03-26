41133ae85d98f644dd8ca4f8e130970b62128a81
JaeKwan AN <jaekwan99@gmail.com>
2025-03-26 22:13:36 +0900

    Feat: 사용자 관리 API구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.user.CreateUserRequest;
+import com.sprint.mission.discodeit.dto.user.UpdateUserRequest;
+import com.sprint.mission.discodeit.dto.user.UserResponse;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final UserStatusService userStatusService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
+        return userService.getUserById(userService.createUser(request).getId())
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/{userId}")
+    public ResponseEntity<Void> updateUser(@PathVariable UUID userId, @RequestBody UpdateUserRequest request) {
+        userService.updateUser(new UpdateUserRequest(
+                userId,
+                request.newName(),
+                request.newEmail(),
+                request.profileImageFileName(),
+                request.profileImageFilePath()
+        ));
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "{/userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, path = "/{userId}/status")
+    public ResponseEntity<Void> updateOnlineStatus(@PathVariable UUID userId) {
+        userStatusService.updateByUserId(userId);
+        return ResponseEntity.ok().build();
+    }
+}
