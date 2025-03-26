1d6927fd8f4be9971e8f0a0b25e6400eded950ce
Leekb0804 <lkb8713@gmail.com>
2025-03-26 09:18:31 +0900

    feat(DTO): BinaryContentUploadResponse DTO 추가

---
+++ src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentUploadResponse.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.dto.binarycontent;
+
+import java.util.UUID;
+
+public record BinaryContentUploadResponse(
+        UUID fileId,
+        String filePath,
+        String fileName,
+        String fileType,
+        long fileSize
+){
+}
