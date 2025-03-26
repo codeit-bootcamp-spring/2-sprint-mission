9c3a9294446c01151f06227a5c6688cfb1afa313
Leekb0804 <lkb8713@gmail.com>
2025-03-26 11:55:22 +0900

    refactor(DTO) : @NotNull, @NotBlank 추가

--- src/main/java/com/sprint/mission/discodeit/dto/user/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/UserUpdateRequest.java
@@ -1,11 +1,21 @@
 package com.sprint.mission.discodeit.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 public record UserUpdateRequest (
+        @NotNull
         UUID userId,
+
+        @NotBlank
         String userName,
+
+        @NotBlank
         String password,
+
+        @NotNull
         UUID profileId
 ) {
 }
