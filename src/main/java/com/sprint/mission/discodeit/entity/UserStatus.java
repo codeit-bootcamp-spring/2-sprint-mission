1e634080e2823f2e6a971856fade430b934d2fb1
Leekb0804 <lkb8713@gmail.com>
2025-03-27 11:09:10 +0900

    feat(Entity) : UserStatusType 필드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -1,14 +1,18 @@
 package com.sprint.mission.discodeit.entity;
 
+import com.sprint.mission.discodeit.model.UserStatusType;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
 
 @Getter
+@Setter
 public class UserStatus extends BaseEntity {
     private final UUID userId;
+    private UserStatusType userStatusType;
 
     public UserStatus(UUID userId) {
         super();
