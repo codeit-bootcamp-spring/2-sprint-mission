75eb72090f0314cb90d73e9cde2032b85a40fbf2
Leekb0804 <lkb8713@gmail.com>
2025-03-27 15:32:23 +0900

    feat(Controller) : delete, findMessageList api 구현

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -1,10 +1,7 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.common.ApiResponse;
-import com.sprint.mission.discodeit.dto.message.MessageCreateRequest;
-import com.sprint.mission.discodeit.dto.message.MessageCreateResponse;
-import com.sprint.mission.discodeit.dto.message.MessageUpdateRequest;
-import com.sprint.mission.discodeit.dto.message.MessageUpdateResponse;
+import com.sprint.mission.discodeit.dto.message.*;
 import com.sprint.mission.discodeit.service.MessageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -36,5 +35,19 @@ public class MessageController {
                 .body(response);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<MessageDeleteResponse>> deleteMessage(@PathVariable UUID id) {
+        messageService.deleteMessage(id);
+        ApiResponse<MessageDeleteResponse> response = new ApiResponse<>(true, "메세지 삭제 성공", new MessageDeleteResponse(id, Instant.now()));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
 
+    @GetMapping("/{channelId}/messages")
+    public ResponseEntity<ApiResponse<List<MessageReadResponse>>> findMessageList(@PathVariable UUID channelId) {
+        List<MessageReadResponse> readResponses=  messageService.findAllByChannelId(channelId);
+        ApiResponse<List<MessageReadResponse>> response = new ApiResponse<>(true, "메세지 리스트 불러오기 성공", readResponses);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
 }
