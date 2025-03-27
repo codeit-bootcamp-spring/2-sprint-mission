5a4b61f63a489eb3f25ab0b415840cc6aa1e3fe4
Leekb0804 <lkb8713@gmail.com>
2025-03-27 14:51:14 +0900

    feat(Controller) : updateMessage api 구현

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -3,6 +3,8 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.common.ApiResponse;
 import com.sprint.mission.discodeit.dto.message.MessageCreateRequest;
 import com.sprint.mission.discodeit.dto.message.MessageCreateResponse;
+import com.sprint.mission.discodeit.dto.message.MessageUpdateRequest;
+import com.sprint.mission.discodeit.dto.message.MessageUpdateResponse;
 import com.sprint.mission.discodeit.service.MessageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +23,16 @@ public class MessageController {
     @PostMapping
     public ResponseEntity<ApiResponse<MessageCreateResponse>> sendMessage(@Valid @RequestBody MessageCreateRequest request) {
         UUID messageId = messageService.createMessage(request);
-        ApiResponse<MessageCreateResponse> response = new ApiResponse<>(true, "메세지 전송 선공", new MessageCreateResponse(messageId));
+        ApiResponse<MessageCreateResponse> response = new ApiResponse<>(true, "메세지 전송 성공", new MessageCreateResponse(messageId));
         return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<MessageUpdateResponse>> updateMessage(@PathVariable UUID id, @Valid @RequestBody MessageUpdateRequest request) {
+        messageService.updateMessage(id, request);
+        ApiResponse<MessageUpdateResponse> response = new ApiResponse<>(true, "메세지 업데이트 성공", new MessageUpdateResponse(id));
+        return ResponseEntity.status(HttpStatus.OK)
                 .body(response);
     }
 
