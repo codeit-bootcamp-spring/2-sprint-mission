b60a35a97e752aaa696111ae9067ffec76dbbbd6
Leekb0804 <lkb8713@gmail.com>
2025-03-27 16:37:17 +0900

    feat(Exception) : DuplicateReadStatusException 추가

---
+++ src/main/java/com/sprint/mission/discodeit/exception/readstatus/DuplicateReadStatusException.java
@@ -0,0 +1,7 @@
+package com.sprint.mission.discodeit.exception.readstatus;
+
+public class DuplicateReadStatusException extends RuntimeException {
+    public DuplicateReadStatusException(String message) {
+        super(message);
+    }
+}
