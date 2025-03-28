38865836453eb8bfdf150508c3634b574e13e4c7
Leekb0804 <lkb8713@gmail.com>
2025-03-28 18:56:16 +0900

    refactor(Service) : create() 메서드를 private으로 바꾸고 uploadSingle() 메서드 추가
    - FileStorageService 를 BinaryContentService 에서 실행되게 하는게 좋아보여 수정함

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -2,11 +2,14 @@ package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentFindResponse;
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentUploadResponse;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.service.FileStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,12 +18,20 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BasicBinaryContentService implements BinaryContentService {
     private final BinaryContentRepository binaryContentRepository;
+    private final FileStorageService fileStorageService;
 
     @Override
-    public UUID create(BinaryContentCreateRequest binaryContentCreateRequest) {
+    public BinaryContentUploadResponse uploadSingle(MultipartFile file) {
+        UUID fileId = UUID.randomUUID();
+        BinaryContentCreateRequest request = fileStorageService.uploadFile(file, fileId);
+        // 메타데이터 저장
+        create(request);
+        return new BinaryContentUploadResponse(request.fileId(), request.filePath(), request.fileName(), request.fileType(), request.fileSize());
+    }
+
+    private void create(BinaryContentCreateRequest binaryContentCreateRequest) {
         BinaryContent newBinaryContent = new BinaryContent(binaryContentCreateRequest.fileId(), binaryContentCreateRequest.filePath(), binaryContentCreateRequest.fileName(), binaryContentCreateRequest.fileType(), binaryContentCreateRequest.fileSize());
         this.binaryContentRepository.add(newBinaryContent);
-        return newBinaryContent.getId();
     }
 
     @Override
