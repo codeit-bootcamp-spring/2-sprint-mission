fb41ca39df0e93e53e8b272fdbea89ca71ab347e
ddday366 <ddday366@gmail.com>
2025-03-30 01:21:23 +0900

    fix: JCFBinaryContentRepository 수정

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
@@ -2,30 +2,41 @@ package com.sprint.mission.discodeit.repository.jcf;
 
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 public class JCFBinaryContentRepository implements BinaryContentRepository {
-    private final Map<UUID, BinaryContent> storage = new HashMap<>();
+    private final Map<UUID, BinaryContent> data;
+
+    public JCFBinaryContentRepository() {
+        this.data = new HashMap<>();
+    }
 
     @Override
-    public void save(BinaryContent binaryContent) {
-        storage.put(binaryContent.getId(), binaryContent);
+    public BinaryContent save(BinaryContent binaryContent) {
+        this.data.put(binaryContent.getId(), binaryContent);
+        return binaryContent;
     }
 
     @Override
     public Optional<BinaryContent> findById(UUID id) {
-        return Optional.ofNullable(storage.get(id));
+        return Optional.ofNullable(this.data.get(id));
     }
 
     @Override
-    public List<BinaryContent> findAll() {
-        return new ArrayList<>(storage.values());
+    public List<BinaryContent> findAllByIdIn(List<UUID> ids) {
+        return this.data.values().stream()
+                .filter(content -> ids.contains(content.getId()))
+                .toList();
     }
 
     @Override
-    public void delete(UUID id) {
-        storage.remove(id);
+    public boolean existsById(UUID id) {
+        return this.data.containsKey(id);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        this.data.remove(id);
     }
 }
