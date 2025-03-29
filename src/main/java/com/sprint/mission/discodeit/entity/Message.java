b4945cd80bf348c0e4f4b0028e7a3fe9bf0ecaee
ddday366 <ddday366@gmail.com>
2025-03-30 01:22:25 +0900

    fix: Message 수정

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -4,35 +4,42 @@ import lombok.Getter;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
-    private final UUID userId;
-    private final UUID channelId;
-    private String content;
-    protected final UUID id;
-    protected final Instant createAt;
-    protected Instant updateAt;
 
-    public Message(UUID userId, UUID channelId, String content) {
-        if (content == null || content.trim().isEmpty()) {
-            throw new IllegalArgumentException("Content cannot be null or empty");
-        }
+    private UUID id;
+    private Instant createdAt;
+    private Instant updatedAt;
+    //
+    private String content;
+    //
+    private UUID channelId;
+    private UUID authorId;
+    private List<UUID> attachmentIds;
+
+    public Message(String content, UUID channelId, UUID authorId, List<UUID> attachmentIds) {
         this.id = UUID.randomUUID();
-        this.userId = userId;
-        this.channelId = channelId;
+        this.createdAt = Instant.now();
+        //
         this.content = content;
-        this.createAt = Instant.now();
-        this.updateAt = createAt;
+        this.channelId = channelId;
+        this.authorId = authorId;
+        this.attachmentIds = attachmentIds;
     }
 
-    public void updateContent(String newContent) {
-        if (newContent == null || newContent.trim().isEmpty()) {
-            throw new IllegalArgumentException("Content cannot be null or empty");
+    public void update(String newContent) {
+        boolean anyValueUpdated = false;
+        if (newContent != null && !newContent.equals(this.content)) {
+            this.content = newContent;
+            anyValueUpdated = true;
         }
-        this.content = newContent;
-        this.updateAt = Instant.now();
+
+        if (anyValueUpdated) {
+            this.updatedAt = Instant.now();
+        }
     }
 }
