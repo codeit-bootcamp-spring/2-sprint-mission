bc86b82c2859a3bcf7f685a197849d5f6df0104a
ddday366 <ddday366@gmail.com>
2025-03-30 01:24:19 +0900

    fix: UserStatusRepository 수정

--- src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -1,16 +1,17 @@
 package com.sprint.mission.discodeit.repository;
 
-import com.sprint.mission.discodeit.entity.ReadStatus;
-import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserStatusRepository {
-    void save(UserStatus userStatus);
-    User findById(UUID userId);
-    List<ReadStatus> findAllByUserId(UUID userId);
-    void delete(UUID userId);
+    UserStatus save(UserStatus userStatus);
+    Optional<UserStatus> findById(UUID id);
+    Optional<UserStatus> findByUserId(UUID userId);
+    List<UserStatus> findAll();
+    boolean existsById(UUID id);
+    void deleteById(UUID id);
+    void deleteByUserId(UUID userId);
 }
