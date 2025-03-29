225d7a0f8aac608cfe7e44b9409a2c5b2bac265d
ddday366 <ddday366@gmail.com>
2025-03-30 01:25:02 +0900

    Delete: UserStatusDTO 삭제

--- src/main/java/com/sprint/mission/discodeit/DTO/UserStatusDTO.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.DTO;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-public record UserStatusDTO(
-        UUID userId,
-        LocalDateTime lastOnlineAt) {
-}
