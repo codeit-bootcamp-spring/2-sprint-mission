1ad92b0950350305216b1a643cf2ea979e0b2e34
ddday366 <ddday366@gmail.com>
2025-03-30 01:27:57 +0900

    Delete: AuthService.java 삭제

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.service;
-
-import com.sprint.mission.discodeit.DTO.AuthRequestDTO;
-import com.sprint.mission.discodeit.DTO.AuthResponseDTO;
-import org.springframework.stereotype.Service;
-
-public interface AuthService {
-    AuthResponseDTO login(AuthRequestDTO authRequest);
-}
