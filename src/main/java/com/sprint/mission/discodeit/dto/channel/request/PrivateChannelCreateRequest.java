dd4e410f52ae7714d9754e44a55a1af0cc6a6033
정종현 <bellhyunforgithub@gmail.com>
2025-03-30 00:00:38 +0900

    Feat: GlobalException 구현

--- src/main/java/com/sprint/mission/discodeit/dto/channel/request/PrivateChannelCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.dto.channel.request;
-
-import com.sprint.mission.discodeit.entity.User;
-
-import java.util.List;
-
-public record PrivateChannelCreateRequest(
-        List<User> users
-) {}
