de97e2015d5a2d91d63ac19a4f9d8e2b5f593226
ddday366 <ddday366@gmail.com>
2025-03-30 01:10:40 +0900

    feat: UserController 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -0,0 +1,62 @@
+
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
+import com.sprint.mission.discodeit.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<UserDto> createUser(@RequestBody UserCreateRequest request) {
+        return ResponseEntity.ok(userService.find(userService.create(request, Optional.empty()).getId()));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable UUID userId, @RequestBody UserUpdateRequest request) {
+        return ResponseEntity.ok(userService.find(userService.update(userId, request, Optional.empty()).getId()));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID userId) {
+        userService.delete(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @CrossOrigin(origins = "http://localhost:5500")
+    @RequestMapping(method = RequestMethod.GET, value = "/findAll")
+    public ResponseEntity<List<UserDto>> findAllUsers() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{userId}")
+    public ResponseEntity<UserDto> getUser(@PathVariable UUID userId) {
+        return ResponseEntity.ok(userService.find(userId));
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "/{userId}/status")
+    public ResponseEntity<Void> updateUserStatus(@PathVariable UUID userId, @RequestParam boolean online) {
+        userService.updateUserStatus(userId, online);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok("로그인 성공");
+    }
+}
