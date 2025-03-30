2d690d8010001fc52e6024f39c691504d737ed9f
hyorim <tg305@naver.com>
2025-03-30 22:54:28 +0900

    feat: 심화 요구 사항 화면을 서빙하기 위해 필요한 파일 삽입

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
