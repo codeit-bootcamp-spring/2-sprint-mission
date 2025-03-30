fc92ae72a17740c64a605b73123a2233be1b5d21
Leekb0804 <lkb8713@gmail.com>
2025-03-30 16:10:45 +0900

    refactor(DTO) : BinaryContentCreateRequest DTO에 유효성 검증 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentCreateRequest.java
@@ -1,12 +1,25 @@
 package com.sprint.mission.discodeit.dto.binarycontent;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.util.UUID;
 
 public record BinaryContentCreateRequest(
+        @NotNull
         UUID fileId,
+
+        @NotBlank
         String filePath,
+
+        @NotBlank
         String fileName,
+
+        @NotBlank
         String contentType,
+
+        @Positive
         long fileSize
 ) {
 }
