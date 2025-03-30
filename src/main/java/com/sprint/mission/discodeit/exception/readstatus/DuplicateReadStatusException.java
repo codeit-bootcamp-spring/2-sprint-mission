344fd919a9881e3d47d265c117e524d03de4117f
Leekb0804 <lkb8713@gmail.com>
2025-03-30 18:40:26 +0900

    refactor(Exception) : HttpStatus 필드, Throwable cause 파라미터 추가

--- src/main/java/com/sprint/mission/discodeit/exception/readstatus/DuplicateReadStatusException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/readstatus/DuplicateReadStatusException.java
@@ -1,7 +1,19 @@
 package com.sprint.mission.discodeit.exception.readstatus;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class DuplicateReadStatusException extends RuntimeException {
-    public DuplicateReadStatusException(String message) {
+    private final HttpStatus status;
+
+    public DuplicateReadStatusException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
+    }
+
+    public DuplicateReadStatusException(String message, HttpStatus status, Throwable cause) {
+        super(message, cause);
+        this.status = status;
     }
 }
