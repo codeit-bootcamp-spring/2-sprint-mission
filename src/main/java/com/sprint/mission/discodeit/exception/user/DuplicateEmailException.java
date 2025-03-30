a2f2272018e83861474afba2b5072be7e8e8b22d
Leekb0804 <lkb8713@gmail.com>
2025-03-30 19:07:51 +0900

    refactor(Exception) : HttpStatus status 필드 추가, 생성자 파라미터에 Throwable cause 추가

--- src/main/java/com/sprint/mission/discodeit/exception/user/DuplicateEmailException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/user/DuplicateEmailException.java
@@ -1,7 +1,19 @@
 package com.sprint.mission.discodeit.exception.user;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String email) {
+    private final HttpStatus status;
+
+    public DuplicateEmailException(String email, HttpStatus status) {
         super("이미 사용 중인 이메일입니다 : " + email);
+        this.status = status;
+    }
+
+    public DuplicateEmailException(String email, HttpStatus status, Throwable cause) {
+        super("이미 사용 중인 이메일입니다 : " + email, cause);
+        this.status = status;
     }
 }
--- src/main/java/com/sprint/mission/discodeit/exception/user/DuplicateUserNameException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/user/DuplicateUserNameException.java
@@ -1,7 +1,19 @@
 package com.sprint.mission.discodeit.exception.user;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class DuplicateUserNameException extends RuntimeException {
-    public DuplicateUserNameException(String userName) {
+    private final HttpStatus status;
+
+    public DuplicateUserNameException(String userName, HttpStatus status) {
         super("이미 사용 중인 사용자 이름입니다 : " + userName);
+        this.status = status;
+    }
+
+    public DuplicateUserNameException(String userName, HttpStatus status, Throwable cause) {
+        super("이미 사용 중인 사용자 이름입니다 : " + userName, cause);
+        this.status = status;
     }
 }
