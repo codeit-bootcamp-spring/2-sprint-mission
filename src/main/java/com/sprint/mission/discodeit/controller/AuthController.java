3b7bfe940752851d93c7004b3adb277b56a72ae5
황지환 <hth99131@gmail.com>
2025-03-30 23:01:02 +0900

    refactor: AuthController 추가

---
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.application.dto.auth.LoginRequest;
+import com.sprint.mission.discodeit.application.dto.user.UserResponse;
+import com.sprint.mission.discodeit.application.dto.user.UserResult;
+import com.sprint.mission.discodeit.application.dto.userstatus.UserStatusResult;
+import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+    private final UserStatusService userStatusService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(@RequestBody LoginRequest loginRequest) {
+        UserResult userResult = authService.login(loginRequest);
+        UserStatusResult userStatus = userStatusService.findByUserId(userResult.id());
+        UserResponse response = UserResponse.of(userResult, userStatus.isLogin());
+        return ResponseEntity.ok(response);
+    }
+}
