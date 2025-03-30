e2e23b0044774ee9e1a0cf5cc2960dd3efd1db31
Leekb0804 <lkb8713@gmail.com>
2025-03-30 15:56:20 +0900

    feat(Exception) : HttpStatus 필드 추가

--- src/main/java/com/sprint/mission/discodeit/exception/auth/LoginFailedException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/auth/LoginFailedException.java
@@ -1,10 +1,18 @@
 package com.sprint.mission.discodeit.exception.auth;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class LoginFailedException extends RuntimeException {
-    public LoginFailedException(String message) {
+    private final HttpStatus status;
+
+    public LoginFailedException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
     }
-    public LoginFailedException(String message, Throwable cause) {
+    public LoginFailedException(String message, HttpStatus status, Throwable cause) {
         super(message, cause);
+        this.status = status;
     }
 }
