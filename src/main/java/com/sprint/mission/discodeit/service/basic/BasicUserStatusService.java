f31ca75f595e79feb54fd6195bc2cab692221ba2
Leekb0804 <lkb8713@gmail.com>
2025-03-30 22:18:01 +0900

    refactor(Service) : updateUserStatusByUserId() 메서드에 예외처리 로직 추가

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
@@ -2,15 +2,19 @@ package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.userstatus.*;
 import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.exception.common.NoSuchIdException;
 import com.sprint.mission.discodeit.exception.user.DuplicateUserIdException;
+import com.sprint.mission.discodeit.exception.userstatus.UpdateUserStatusException;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -55,7 +59,13 @@ public class BasicUserStatusService implements UserStatusService {
 
     @Override
     public void updateUserStatusByUserId(UUID id, UserStatusUpdateByUserIdRequest userStatusUpdateByUserIdRequest) {
-        this.userStatusRepository.updateUserStatusByUserId(id, userStatusUpdateByUserIdRequest.type());
+        try {
+            this.userStatusRepository.updateUserStatusByUserId(id, userStatusUpdateByUserIdRequest.type());
+        } catch (NoSuchIdException e) {
+            throw new UpdateUserStatusException(e.getMessage(), e.getStatus(), e);
+        } catch (Exception e) {
+            throw new UpdateUserStatusException("userStatus 업데이트 중 예상치 못한 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR, e);
+        }
     }
 
     @Override
