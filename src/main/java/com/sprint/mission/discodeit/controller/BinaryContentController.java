b53220ed38da0759a10620e2a3adab9f0399a348
황지환 <hth99131@gmail.com>
2025-03-30 23:28:02 +0900

    refactor: BinaryContentController API 추가

---
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/binary")
+@RequiredArgsConstructor
+public class BinaryContentController {
+    private final BinaryContentService binaryContentService;
+
+    @GetMapping("/{fileId}")
+    public ResponseEntity<Resource> downloadSingleFile(@PathVariable UUID fileId) {
+        byte[] fileData = binaryContentService.findById(fileId).bytes();
+
+        ByteArrayResource resource = new ByteArrayResource(fileData);
+
+        return ResponseEntity.ok()
+                .body(resource);
+    }
+}
