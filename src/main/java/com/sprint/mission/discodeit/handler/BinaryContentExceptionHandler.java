89ac7ddb5004edbcdeeab20525ed659dfbb6d73f
Leekb0804 <lkb8713@gmail.com>
2025-03-30 16:12:08 +0900

    refactor(Handler) : handleFileUploadExceptionException() 메서드 추가 및 HttpStatus 로직 추가

--- src/main/java/com/sprint/mission/discodeit/handler/BinaryContentExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/handler/BinaryContentExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.sprint.mission.discodeit.handler;
 
 import com.sprint.mission.discodeit.dto.common.ApiResponse;
-import com.sprint.mission.discodeit.exception.binarycontent.FileIdNotFoundException;
+import com.sprint.mission.discodeit.exception.binarycontent.FileFindException;
+import com.sprint.mission.discodeit.exception.binarycontent.FileUploadException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class BinaryContentExceptionHandler {
-    @ExceptionHandler(FileIdNotFoundException.class)
-    public ResponseEntity<ApiResponse<Void>> handleFileIdNotFoundException(FileIdNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse<Void>> handleFileUploadExceptionException(FileUploadException e) {
+        return ResponseEntity.status(e.getStatus())
+                .body(new ApiResponse<>(false, e.getMessage(), null));
+    }
+
+    @ExceptionHandler(FileFindException.class)
+    public ResponseEntity<ApiResponse<Void>> handleFileFindException(FileFindException e) {
+        return ResponseEntity.status(e.getStatus())
                 .body(new ApiResponse<>(false, e.getMessage(), null));
     }
 }
