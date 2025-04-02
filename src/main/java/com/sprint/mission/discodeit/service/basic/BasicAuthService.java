590a4a24c45ecd133d62d41d2e41d19d2f7a4734
manleKim <kk410910@naver.com>
2025-04-02 18:41:54 +0900

    feat: 로그인 시 에러 세분화
    - 유저 오류
    - 비밀번호 오류

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.service.basic;
 import com.sprint.mission.discodeit.dto.auth.AuthLoginDto;
 import com.sprint.mission.discodeit.dto.userStatus.UserStatusUpdateByUserIdDto;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.exception.custom.auth.InvalidPasswordException;
 import com.sprint.mission.discodeit.exception.custom.user.UserNotFoundException;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
@@ -20,13 +21,20 @@ public class BasicAuthService implements AuthService {
 
     @Override
     public User login(AuthLoginDto authLoginDto) {
+        String username = authLoginDto.username();
+        String password = authLoginDto.password();
+
         User foundUser = userRepository.findAll().stream()
-                .filter(user -> user.getUsername().equals(authLoginDto.username()) && user.getPassword()
-                        .equals(authLoginDto.password())).findFirst()
+                .filter(user -> user.getUsername().equals(username))
+                .findFirst()
                 .orElse(null);
 
         if (foundUser == null) {
             throw new UserNotFoundException("로그인 실패: 유저를 찾을 수 없습니다.");
+        }
+
+        if (!foundUser.getPassword().equals(password)) {
+            throw new InvalidPasswordException("비밀번호가 일치하지 않습니다.");
         }
 
         UserStatusUpdateByUserIdDto userStatusUpdateByUserIdDto = new UserStatusUpdateByUserIdDto(Instant.now());
