f936fe56e69dc97974512e61ca46b2080a2c6087
Seong-taeHan <kmljjn22@gmail.com>
2025-04-02 10:55:47 +0900

    SpringDocConfig 구현

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -12,42 +12,43 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+  private final UserService userService;
 
-    @PostMapping("")
-    public ResponseEntity<ApiResponse<Void>> save(
-            @ModelAttribute SaveUserRequestDto saveUserRequestDto,
-            @RequestParam(value =  "file", required = false) MultipartFile file
-    ) throws IOException {
-        userService.save(saveUserRequestDto, SaveBinaryContentRequestDto.nullableFrom(file));
-        return ResponseEntity.ok(ApiResponse.success());
-    }
+  @PostMapping("")
+  public ResponseEntity<ApiResponse<Void>> save(
+      @ModelAttribute SaveUserRequestDto saveUserRequestDto,
+      @RequestParam(value = "file", required = false) MultipartFile file
+  ) throws IOException {
+    userService.save(saveUserRequestDto, SaveBinaryContentRequestDto.nullableFrom(file));
+    return ResponseEntity.ok(ApiResponse.success());
+  }
 
-    @PutMapping("/{userId}")
-    public ResponseEntity<ApiResponse<Void>> update(
-            @PathVariable UUID userId,
-            @ModelAttribute UpdateUserRequestDto updateUserRequestDto,
-            @RequestParam("file") MultipartFile file
-    ) throws IOException {
-        userService.update(userId, updateUserRequestDto, SaveBinaryContentRequestDto.nullableFrom(file));
-        return ResponseEntity.ok(ApiResponse.success());
-    }
+  @PutMapping("/{userId}")
+  public ResponseEntity<ApiResponse<Void>> update(
+      @PathVariable UUID userId,
+      @ModelAttribute UpdateUserRequestDto updateUserRequestDto,
+      @RequestParam("file") MultipartFile file
+  ) throws IOException {
+    userService.update(userId, updateUserRequestDto,
+        SaveBinaryContentRequestDto.nullableFrom(file));
+    return ResponseEntity.ok(ApiResponse.success());
+  }
 
-    @DeleteMapping("/{userId}")
-    public ResponseEntity<ApiResponse<Void>> delete(
-            @PathVariable UUID userId
-    ) {
-        userService.delete(userId);
-        return ResponseEntity.ok(ApiResponse.success());
-    }
+  @DeleteMapping("/{userId}")
+  public ResponseEntity<ApiResponse<Void>> delete(
+      @PathVariable UUID userId
+  ) {
+    userService.delete(userId);
+    return ResponseEntity.ok(ApiResponse.success());
+  }
 
-    @GetMapping("/findAll")
-    public ResponseEntity<ApiResponse<List<FindUserDto>>> findAll() {
-        List<FindUserDto> findUserDtoList = userService.findAllUser();
-        return ResponseEntity.ok(ApiResponse.success(findUserDtoList));
-    }
+  @GetMapping("/findAll")
+  public ResponseEntity<ApiResponse<List<FindUserDto>>> findAll() {
+    List<FindUserDto> findUserDtoList = userService.findAllUser();
+    return ResponseEntity.ok(ApiResponse.success(findUserDtoList));
+  }
 }
