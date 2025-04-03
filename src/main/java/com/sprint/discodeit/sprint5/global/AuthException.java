b15c24c474bafe85a00d46b15cababc4b5a2b901
leejunnyeop <daffodil8620@naver.com>
2025-04-03 11:27:59 +0900

    [chore] AuthException 범용 에러 처리하기 위해 접근제어자 변경

--- src/main/java/com/sprint/discodeit/sprint5/global/AuthException.java
+++ src/main/java/com/sprint/discodeit/sprint5/global/AuthException.java
@@ -2,7 +2,7 @@ package com.sprint.discodeit.sprint5.global;
 
 public class AuthException extends BaseException{
 
-    protected AuthException(ErrorCode errorCode) {
+    public AuthException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
