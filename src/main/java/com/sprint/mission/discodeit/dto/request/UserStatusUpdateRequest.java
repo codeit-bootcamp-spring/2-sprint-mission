82f8f8c2503d03ab5441174f6f2acd269101d06f
Leekb0804 <lkb8713@gmail.com>
2025-04-03 17:38:09 +0900

    refactor(DTO) : 유효성 검증 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/UserStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/UserStatusUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 
 @Schema(description = "변경할 User 온라인 상태 정보")
 public record UserStatusUpdateRequest(
+    @NotNull(message = "newLastActiveAt에 null값이 들어올 수 없습니다.")
     Instant newLastActiveAt
 ) {
 
--- src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
@@ -1,11 +1,18 @@
 package com.sprint.mission.discodeit.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "수정할 User 정보")
 public record UserUpdateRequest(
+    @NotBlank(message = "newUsername이 공백일 수 없습니다.")
     String newUsername,
+
+    @Email(message = "newEmail이 유효한 이메일 형식이 아닙니다.")
     String newEmail,
+
+    @NotBlank(message = "newPassword가 공백일 수 없습니다.")
     String newPassword
 ) {
 
