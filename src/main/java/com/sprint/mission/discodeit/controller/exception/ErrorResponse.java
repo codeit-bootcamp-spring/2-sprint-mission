2b35e12438a2a01beae5e12e0099ede909288050
황지환 <hth99131@gmail.com>
2025-04-03 18:44:37 +0900

    refactor: 컨트롤러 전역 예외 핸들러 반환형식 통일

--- src/main/java/com/sprint/mission/discodeit/controller/exception/ErrorResponse.java
+++ src/main/java/com/sprint/mission/discodeit/controller/exception/ErrorResponse.java
@@ -1,9 +1,21 @@
 package com.sprint.mission.discodeit.controller.exception;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 
 public record ErrorResponse(String error, String message, int status, String timestamp) {
-    public static ErrorResponse of(String error, String message, int status) {
-        return new ErrorResponse(error, message, status, LocalDateTime.now().toString());
-    }
+
+  public static ErrorResponse of(String error, String message, int status) {
+    return new ErrorResponse(error, message, status, LocalDateTime.now().toString());
+  }
+
+  public static List<ErrorResponse> of(List<FieldError> fieldErrors) {
+    return fieldErrors.stream()
+        .map(fieldError -> new ErrorResponse(fieldError.getField(),
+            fieldError.getDefaultMessage(), HttpStatus.BAD_REQUEST.value(),
+            LocalDateTime.now().toString()))
+        .toList();
+  }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/controller/exception/GlobalControllerExceptionHandler.java
@@ -1,41 +1,42 @@
 package com.sprint.mission.discodeit.controller.exception;
 
-import java.util.stream.Collectors;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
-    String errorMessage = ex.getBindingResult().getFieldErrors().stream()
-        .map(error -> error.getField() + ": " + error.getDefaultMessage())
-        .collect(Collectors.joining(", "));
+  public ResponseEntity<List<ErrorResponse>> handleValidationErrors(
+      MethodArgumentNotValidException ex) {
+    List<FieldError> fieldErrors = ex.getBindingResult()
+        .getFieldErrors();
 
-    return ResponseEntity.badRequest().body(errorMessage);
+    return ResponseEntity.badRequest()
+        .body(ErrorResponse.of(fieldErrors));
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
     log.error("Illegal argument: {}", ex.getMessage());
+
     return ResponseEntity.badRequest()
         .body(ErrorResponse.of("Bad Request", ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
   }
 
   @ExceptionHandler(Exception.class)
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ResponseEntity<ErrorResponse> handleNotSpecificException(Exception ex) {
     log.error("Not SpecificException: {}", ex.getMessage());
-    return ResponseEntity.badRequest()
-        .body(ErrorResponse.of("Bad Request", ex.getMessage(),
+
+    return ResponseEntity.internalServerError()
+        .body(ErrorResponse.of("InterServerError", ex.getMessage(),
             HttpStatus.INTERNAL_SERVER_ERROR.value()));
   }
 }
