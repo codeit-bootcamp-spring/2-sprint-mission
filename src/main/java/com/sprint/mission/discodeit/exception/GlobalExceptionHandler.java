80b0f8d3cd95560b7724500a6c52361f2306caaa
Leekb0804 <lkb8713@gmail.com>
2025-04-03 19:14:16 +0900

    feat(ExceptionHandler) : @Valid 예외 처리 handle 추가

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package com.sprint.mission.discodeit.exception;
 
+import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,5 +39,25 @@ public class GlobalExceptionHandler {
     return ResponseEntity
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(e.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException e) {
+    BindingResult bindingResult = e.getBindingResult();
+
+    Field[] fields = UserCreateRequest.class.getDeclaredFields();
+    List<String> fieldOrder = Arrays.stream(fields).map(Field::getName)
+        .toList();   //응답 필드를 DTO에 작성된 순서대로 나열
+
+    Map<String, String> errorResponse = new LinkedHashMap<>();
+    fieldOrder.forEach(field -> {
+      bindingResult.getFieldErrors(field).forEach(error ->
+          errorResponse.put(field, error.getDefaultMessage())
+      );
+    });
+
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(errorResponse);
   }
 }
