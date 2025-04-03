a781b582ee01ac6d0fefda0821edbff7cef71e2a
Bokyung Kim <chloeee0228@gmail.com>
2025-04-03 11:21:46 +0900

    feat: Swagger 어노테이션 추가한 ReadStatusController

--- src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@Schema(description = "읽음 상태 생성 정보")
 public record ReadStatusCreateRequest(
         UUID userId,
         UUID channelId,
