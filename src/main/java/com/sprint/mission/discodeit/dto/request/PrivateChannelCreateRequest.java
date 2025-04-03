6b6ddfd5d10dfdfef5cecb1f5dba9eeeff0f4045
Bokyung Kim <chloeee0228@gmail.com>
2025-04-03 11:43:41 +0900

    feat: Swagger 어노테이션 추가한 ChannelController

--- src/main/java/com/sprint/mission/discodeit/dto/request/PrivateChannelCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/PrivateChannelCreateRequest.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 import java.util.UUID;
 
+@Schema(description = "비공개 채널 생성 요청")
 public record PrivateChannelCreateRequest(
         List<UUID> participantIds
 ) {
--- src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "공개 채널 업데이트 요청")
 public record PublicChannelUpdateRequest(
         String newName,
         String newDescription
