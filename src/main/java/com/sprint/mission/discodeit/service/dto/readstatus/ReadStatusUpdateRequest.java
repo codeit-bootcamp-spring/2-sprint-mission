af87499804401425c577d921cff77aed31673b1a
seonseon933 <seonseon933@gmail.com>
2025-04-03 10:42:46 +0900

    Feat: ReadStatus - API 문서 작성

--- src/main/java/com/sprint/mission/discodeit/service/dto/readstatus/ReadStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/service/dto/readstatus/ReadStatusUpdateRequest.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.service.dto.readstatus;
 import java.time.Instant;
 
 public record ReadStatusUpdateRequest(
-        Instant lastReadAt
+    Instant newLastReadAt
 ) {
+
 }
