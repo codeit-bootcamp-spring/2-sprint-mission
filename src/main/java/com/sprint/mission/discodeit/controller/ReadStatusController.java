95e06e58970755f484f89c61e670fc9d79beed25
황지환 <hth99131@gmail.com>
2025-04-05 04:08:08 +0900

    test(ReadStatusControllerTest): 읽음상태 컨트롤러 테스트 코드 추가

--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -4,44 +4,46 @@ import com.sprint.mission.discodeit.application.dto.readstatus.ReadStatusCreateR
 import com.sprint.mission.discodeit.application.dto.readstatus.ReadStatusResult;
 import com.sprint.mission.discodeit.service.ReadStatusService;
 import jakarta.validation.Valid;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/readStatuses")
+@RequestMapping("/api/readStatuses")
 @RequiredArgsConstructor
 public class ReadStatusController {
 
-  private final ReadStatusService readStatusService;
+    private final ReadStatusService readStatusService;
 
-  @PostMapping
-  public ResponseEntity<ReadStatusResult> createReadStatus(
-      @Valid @RequestBody ReadStatusCreateRequest request) {
-    ReadStatusResult readStatusResult = readStatusService.create(request);
+    @PostMapping
+    public ResponseEntity<ReadStatusResult> createReadStatus(
+            @Valid @RequestBody ReadStatusCreateRequest request) {
+        ReadStatusResult readStatusResult = readStatusService.create(request);
 
-    return ResponseEntity.ok(readStatusResult);
-  }
+        return ResponseEntity.ok(readStatusResult);
+    }
 
-  @PutMapping("/{readStatusId}")
-  public ResponseEntity<ReadStatusResult> updateReadStatus(@PathVariable UUID readStatusId) {
-    ReadStatusResult readStatusResult = readStatusService.updateLastReadTime(readStatusId);
+    @PutMapping("/{readStatusId}")
+    public ResponseEntity<ReadStatusResult> updateReadStatus(@PathVariable UUID readStatusId) {
+        ReadStatusResult readStatusResult = readStatusService.updateLastReadTime(readStatusId);
 
-    return ResponseEntity.ok(readStatusResult);
-  }
+        return ResponseEntity.ok(readStatusResult);
+    }
 
-  @GetMapping("/{userId}")
-  public ResponseEntity<List<ReadStatusResult>> getUserReadStatus(@PathVariable UUID userId) {
-    List<ReadStatusResult> readStatusResults = readStatusService.getAllByUserId(userId);
+    @GetMapping
+    public ResponseEntity<List<ReadStatusResult>> getUserReadStatus(@RequestParam UUID userId) {
+        List<ReadStatusResult> readStatusResults = readStatusService.getAllByUserId(userId);
 
-    return ResponseEntity.ok(readStatusResults);
-  }
+        return ResponseEntity.ok(readStatusResults);
+    }
+
+    @DeleteMapping("/{readStatusId}")
+    public ResponseEntity<Void> delete(@PathVariable UUID readStatusId) {
+        readStatusService.delete(readStatusId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
