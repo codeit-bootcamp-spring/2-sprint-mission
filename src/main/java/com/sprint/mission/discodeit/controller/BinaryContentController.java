465b31e736a24adc94547ffbe4350ec02bf306a1
JaeKwan AN <jaekwan99@gmail.com>
2025-04-04 09:16:49 +0900

    fix: BinaryContentController API 명세에 맞게 수정

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -4,8 +4,9 @@ import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,20 +14,22 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/binaryContent")
+@RequestMapping("/api/binaryContents")
 @RequiredArgsConstructor
 public class BinaryContentController {
-    private final BinaryContentService binaryContentService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/find")
-    public ResponseEntity<BinaryContent> findById(@RequestParam("binaryContentId") UUID binaryContentId) {
-        return binaryContentService.find(binaryContentId)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
+  private final BinaryContentService binaryContentService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/findAll")
-    public ResponseEntity<List<BinaryContent>> findAll(@RequestParam List<UUID> ids) {
-        return ResponseEntity.ok(binaryContentService.findAllByIdIn(ids));
-    }
+  @GetMapping("/{binaryContentId}")
+  public ResponseEntity<BinaryContent> findById(@PathVariable UUID binaryContentId) {
+    return binaryContentService.find(binaryContentId)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @GetMapping
+  public ResponseEntity<List<BinaryContent>> findAll(
+      @RequestParam("binaryContentIds") List<UUID> ids) {
+    return ResponseEntity.ok(binaryContentService.findAllByIdIn(ids));
+  }
 }
