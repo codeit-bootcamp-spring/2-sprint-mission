d43bb22dc10d6d34ad1612560dee8c0a06b588bc
황지환 <hth99131@gmail.com>
2025-04-05 11:49:56 +0900

    test(UserControllerTest): 유저 컨트롤러 테스트 코드 추가

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -6,81 +6,71 @@ import com.sprint.mission.discodeit.application.dto.userstatus.UserStatusResult;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.UserStatusService;
 import jakarta.validation.Valid;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Slf4j
+import java.util.List;
+import java.util.UUID;
+
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
-  private final UserService userService;
-  private final UserStatusService userStatusService;
+    private final UserService userService;
+    private final UserStatusService userStatusService;
 
-  @PostMapping
-  public ResponseEntity<UserResult> register(@Valid @RequestPart UserCreateRequest userRequest,
-      @RequestPart(required = false) MultipartFile profileImage) {
-    UserResult user = userService.register(userRequest, profileImage);
+    @PostMapping
+    public ResponseEntity<UserResult> register(@Valid @RequestPart UserCreateRequest userRequest,
+                                               @RequestPart(required = false) MultipartFile profileImage) {
+        UserResult user = userService.register(userRequest, profileImage);
 
-    return ResponseEntity.ok(user);
-  }
+        return ResponseEntity.ok(user);
+    }
 
-  @GetMapping("/{userId}")
-  public ResponseEntity<UserResult> getById(@PathVariable UUID userId) {
-    UserResult user = userService.getById(userId);
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResult> getById(@PathVariable UUID userId) {
+        UserResult user = userService.getById(userId);
 
-    return ResponseEntity.ok(user);
-  }
+        return ResponseEntity.ok(user);
+    }
 
-  @GetMapping
-  public ResponseEntity<List<UserResult>> getAll() {
-    return ResponseEntity.ok(userService.getAll());
-  }
+    @GetMapping
+    public ResponseEntity<List<UserResult>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
+    }
 
-  @PutMapping(value = "/{userId}")
-  public ResponseEntity<UserResult> updateUser(@PathVariable UUID userId,
-      @RequestBody UserCreateRequest userRequest) {
-    UserResult updatedUser = userService.updateName(userId, userRequest.name());
+    @PutMapping(value = "/{userId}")
+    public ResponseEntity<UserResult> updateUserName(@PathVariable UUID userId,
+                                                     @RequestBody UserCreateRequest userRequest) {
+        UserResult updatedUser = userService.updateName(userId, userRequest.name());
 
-    return ResponseEntity.ok(updatedUser);
-  }
+        return ResponseEntity.ok(updatedUser);
+    }
 
 
-  @PutMapping("/{userId}/profile-image")
-  public ResponseEntity<UserResult> updateProfileImage(@PathVariable UUID userId,
-      @RequestPart MultipartFile profileImage) {
-    UserResult user = userService.updateProfileImage(userId, profileImage);
+    @PutMapping("/{userId}/profile-image")
+    public ResponseEntity<UserResult> updateProfileImage(@PathVariable UUID userId,
+                                                         @RequestPart MultipartFile profileImage) {
+        UserResult user = userService.updateProfileImage(userId, profileImage);
 
-    return ResponseEntity.ok(user);
-  }
+        return ResponseEntity.ok(user);
+    }
 
-  @PutMapping("/{userId}/status")
-  public ResponseEntity<UserStatusResult> updateOnlineStatus(@PathVariable UUID userId) {
-    UserStatusResult status = userStatusService.updateByUserId(userId);
+    @PutMapping("/{userId}/status")
+    public ResponseEntity<UserStatusResult> updateOnlineStatus(@PathVariable UUID userId) {
+        UserStatusResult status = userStatusService.updateByUserId(userId);
 
-    return ResponseEntity.ok(status);
-  }
+        return ResponseEntity.ok(status);
+    }
 
-  @DeleteMapping("/{userId}")
-  public ResponseEntity<Void> delete(@PathVariable UUID userId) {
-    userService.delete(userId);
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> delete(@PathVariable UUID userId) {
+        userService.delete(userId);
 
-    return ResponseEntity.noContent()
-        .build();
-  }
-
+        return ResponseEntity.noContent()
+                .build();
+    }
 }
--- src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
+++ src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
@@ -1,5 +1,151 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.application.dto.user.UserCreateRequest;
+import com.sprint.mission.discodeit.application.dto.user.UserResult;
+import com.sprint.mission.discodeit.application.dto.userstatus.UserStatusResult;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import com.sprint.mission.discodeit.util.JsonConvertor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import static com.sprint.mission.discodeit.util.mock.file.FileInfo.IMAGE_NAME_DOG;
+import static com.sprint.mission.discodeit.util.mock.file.MockFile.createMockImageFile;
+import static com.sprint.mission.discodeit.util.mock.user.UserInfo.LOGIN_USER;
+import static com.sprint.mission.discodeit.util.mock.user.UserInfo.OTHER_USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
 
+    @Autowired
+    private MockMvcTester mockMvc;
+    @MockitoBean
+    private UserService userService;
+    @MockitoBean
+    private UserStatusService userStatusService;
+
+    @Test
+    void register() {
+        User user = new User(LOGIN_USER.getName(), LOGIN_USER.getEmail(), LOGIN_USER.getPassword(), null);
+        UserResult stubResult = UserResult.fromEntity(user, false);
+        when(userService.register(any(), any())).thenReturn(stubResult);
+
+        assertThat(mockMvc.post()
+                .uri("/api/users")
+                .multipart()
+                .file(new MockMultipartFile("userRequest", null, MediaType.APPLICATION_JSON_VALUE, JsonConvertor.asString(new UserCreateRequest(LOGIN_USER.getName(), LOGIN_USER.getEmail(), LOGIN_USER.getPassword())).getBytes()))
+                .contentType(MediaType.MULTIPART_FORM_DATA))
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.id")
+                .isEqualTo(user.getId().toString());
+    }
+
+    @Test
+    void getById() {
+        User user = new User(LOGIN_USER.getName(), LOGIN_USER.getEmail(), LOGIN_USER.getPassword(), null);
+        UserResult stubResult = UserResult.fromEntity(user, false);
+
+        when(userService.getById(any())).thenReturn(stubResult);
+
+        assertThat(mockMvc.get()
+                .uri("/api/users/{userId}", user.getId())
+                .queryParam("userId", user.getId().toString()))
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.id")
+                .isEqualTo(user.getId().toString());
+    }
+
+    @Test
+    void getAll() {
+        User user = new User(LOGIN_USER.getName(), LOGIN_USER.getEmail(), LOGIN_USER.getPassword(), null);
+        UserResult stubResult = UserResult.fromEntity(user, false);
+        User otherUser = new User(OTHER_USER.getName(), OTHER_USER.getEmail(), OTHER_USER.getPassword(), null);
+        UserResult otherUserStubResult = UserResult.fromEntity(otherUser, false);
+
+        when(userService.getAll()).thenReturn(List.of(stubResult, otherUserStubResult));
+
+        assertThat(mockMvc.get()
+                .uri("/api/users"))
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$[*].id")
+                .isEqualTo(List.of(user.getId().toString(), otherUser.getId().toString()));
+    }
+
+    @Test
+    void updateUserName() {
+        User user = new User(LOGIN_USER.getName(), LOGIN_USER.getEmail(), LOGIN_USER.getPassword(), null);
+        user.updateName(OTHER_USER.getName());
+        UserResult stubResult = UserResult.fromEntity(user, false);
+
+        when(userService.updateName(any(), any())).thenReturn(stubResult);
+
+        assertThat(mockMvc.put()
+                .uri("/api/users/{userId}", user.getId())
+                .content(JsonConvertor.asString(new UserCreateRequest(OTHER_USER.getName(), OTHER_USER.getEmail(), OTHER_USER.getPassword())))
+                .contentType(MediaType.APPLICATION_JSON))
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.name")
+                .isEqualTo(OTHER_USER.getName());
+    }
+
+    @Test
+    void updateProfileImage() throws IOException {
+        UUID profileId = UUID.randomUUID();
+        User user = new User(LOGIN_USER.getName(), LOGIN_USER.getEmail(), LOGIN_USER.getPassword(), profileId);
+        user.updateName(OTHER_USER.getName());
+        UserResult stubResult = UserResult.fromEntity(user, false);
+
+        when(userService.updateProfileImage(any(), any())).thenReturn(stubResult);
+
+        assertThat(mockMvc.put()
+                .uri("/api/users/{userId}/profile-image", user.getId())
+                .multipart()
+                .file("profileImage", createMockImageFile(IMAGE_NAME_DOG).getBytes()))
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.profileId")
+                .isEqualTo(profileId.toString());
+    }
+
+    @Test
+    void updateOnlineStatus() {
+        User user = new User(LOGIN_USER.getName(), LOGIN_USER.getEmail(), LOGIN_USER.getPassword(), UUID.randomUUID());
+        UserStatus userStatus = new UserStatus(user.getId());
+        UserStatusResult stubResult = UserStatusResult.fromEntity(userStatus, true);
+
+        when(userStatusService.updateByUserId(any())).thenReturn(stubResult);
+
+        assertThat(mockMvc.put()
+                .uri("/api/users/{userId}/status", user.getId()))
+                .hasStatusOk()
+                .bodyJson()
+                .extractingPath("$.isLogin")
+                .isEqualTo(true);
+    }
+
+    @Test
+    void delete() {
+        UUID userId = UUID.randomUUID();
+        assertThat(mockMvc.delete().uri("/api/users/{userId}", userId))
+                .hasStatus(HttpStatus.NO_CONTENT);
+    }
 }
