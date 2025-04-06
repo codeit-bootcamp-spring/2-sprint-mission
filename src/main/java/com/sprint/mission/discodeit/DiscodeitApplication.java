aff8661cb1bd06b41089b1465bac0c487c102707
윤영로 <yryun00@gmail.com>
2025-04-06 19:15:32 +0900

    mission5 final commit

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,11 +1,15 @@
 package com.sprint.mission.discodeit;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DiscodeitApplication.class, args);
-	}
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiscodeitApplication.class, args);
+  }
 }
