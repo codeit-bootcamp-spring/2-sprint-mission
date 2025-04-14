28fa410b50b88d6bc495938f2761ccd349173cb2
leejunnyeop <daffodil8620@naver.com>
2025-04-14 14:51:12 +0900

    [refactor] : BinaryContentRepository jpa 로 개선

---
+++ src/main/java/com/sprint/discodeit/sprint/repository/BinaryContentRepository.java
@@ -0,0 +1,7 @@
+package com.sprint.discodeit.sprint.repository;
+
+import com.sprint.discodeit.sprint.domain.entity.BinaryContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BinaryContentRepository extends JpaRepository<BinaryContent, Long> {
+}
