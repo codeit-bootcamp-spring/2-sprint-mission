85a1e8e2fbf3ede25c9aba1e85b1f5ab8767f5a0
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-15 16:23:53 +0900

    Feat: UserStatus 엔티티 JPA 설정

--- src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -1,18 +1,32 @@
 package com.sprint.mission.discodeit.entity;
 
 import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.io.Serializable;
 import java.time.Duration;
 import java.time.Instant;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "user_statuses")
 @Getter
+@NoArgsConstructor
 public class UserStatus extends BaseUpdatableEntity implements Serializable, Identifiable {
 
   private static final long serialVersionUID = 1L;
+
+  @OneToOne
+  @JoinColumn(name = "user_id")
   private User user;
+
+  @Column(name = "last_active_at", nullable = false)
   private Instant lastActiveAt; // 유저의 마지막 접속시간
 
   @Builder
