f9b80ac6e7bb2333d705bceb4dbe4cca0266d9df
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-15 17:02:20 +0900

    Refactor: MessageRepository Spring Data JPA 설정

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -1,17 +1,20 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Message;
-
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MessageRepository {
-    Message save(Message message);
-    Optional<Message> findById(UUID id);
-    List<Message> findAllByChannelId(UUID channelId);
-    void deleteById(UUID id);
-    void deleteByChannelId(UUID channelId);
-    Optional<Instant> findLatestMessageTimeByChannelId(UUID channelId);
+public interface MessageRepository extends JpaRepository<Message, UUID> {
+
+  List<Message> findAllByChannelId(UUID channelId);
+
+  void deleteByChannelId(UUID channelId);
+
+  @Query("select max(m.createdAt) from Message m where m.channel.id = : channelId")
+  Optional<Instant> findLatestMessageTimeByChannelId(@Param("channelId") UUID channelId);
 }
