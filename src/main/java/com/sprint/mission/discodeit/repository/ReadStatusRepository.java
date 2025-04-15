10a154aaa4f637c4466423d4d3895c932bd29c43
Seong-taeHan <kmljjn22@gmail.com>
2025-04-15 15:25:04 +0900

    ReadStatusService 트렌잭션 적용

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -13,4 +13,6 @@ public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
   List<ReadStatus> findByUserId(UUID userUUID);
 
   List<ReadStatus> findByChannelId(UUID channelUUID);
+
+  Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId);
 }
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
@@ -9,6 +9,7 @@ import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.ReadStatusService;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -35,9 +36,8 @@ public class BasicReadStatusService implements ReadStatusService {
             () -> new NoSuchElementException(
                 readStatusRequest.channelId() + "에 해당하는 채널을 찾을 수 없습니다."));
 
-    if (readStatusRepository.findByUserId(readStatusRequest.userId()).stream()
-        .anyMatch(
-            readStatus -> readStatus.getChannel().getId().equals(readStatusRequest.channelId()))) {
+    if (readStatusRepository.findByUserIdAndChannelId(
+        readStatusRequest.userId(), readStatusRequest.channelId()).isPresent()) {
       throw new IllegalArgumentException("해당 사용자 및 채널의 읽음 상태가 이미 존재합니다.");
     }
 
@@ -60,15 +60,16 @@ public class BasicReadStatusService implements ReadStatusService {
   }
 
   @Override
+  @Transactional
   public ReadStatus update(UUID readStatusId, ReadStatusUpdateRequest readStatusUpdateRequest) {
     ReadStatus readStatus = readStatusRepository.findById(readStatusId)
         .orElseThrow(() -> new NoSuchElementException(readStatusId + "에 대한 읽은 상태를 찾을 수 없습니다."));
     readStatus.updateLastReadAt(readStatusUpdateRequest.newLastReadAt());
-    readStatusRepository.save(readStatus);
     return readStatus;
   }
 
   @Override
+  @Transactional
   public void delete(UUID readStatusId) {
     ReadStatus readStatus = readStatusRepository.findById(readStatusId)
         .orElseThrow(() -> new NoSuchElementException(readStatusId + "에 대한 읽은 상태를 찾을 수 없습니다."));
