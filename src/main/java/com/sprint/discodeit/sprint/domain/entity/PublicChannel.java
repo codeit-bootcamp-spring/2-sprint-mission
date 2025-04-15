c0e2fd858085e3bcc096d7895e32bece2b85079e
leejunnyeop <daffodil8620@naver.com>
2025-04-15 14:57:01 +0900

    [chore] : PublicChannel 단순 코드정리
    -

--- src/main/java/com/sprint/discodeit/sprint/domain/entity/PublicChannel.java
+++ src/main/java/com/sprint/discodeit/sprint/domain/entity/PublicChannel.java
@@ -4,14 +4,12 @@ import static lombok.AccessLevel.PROTECTED;
 
 import com.sprint.discodeit.sprint.domain.ChannelType;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-@AllArgsConstructor
 public class PublicChannel extends Channel{
 
     private PublicChannel(String name, String description) {
