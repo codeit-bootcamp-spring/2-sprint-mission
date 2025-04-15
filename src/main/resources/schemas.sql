7ba701980b52b1e4932737b94695c1ec73974b42
sangyeobhan <sangyeobhandev@gmail.com>
2025-04-15 10:39:23 +0900

    feat: discodeit ERD 참고하여 DDL 작성

---
+++ src/main/resources/schemas.sql
@@ -0,0 +1,111 @@
+CREATE TABLE IF NOT EXISTS binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ  NOT NULL,
+    file_name    VARCHAR(255) NOT NULL,
+    size         BIGINT       NOT NULL,
+    content_type VARCHAR(100) NOT NULL,
+    bytes        BYTEA        NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ  NOT NULL,
+    updated_at TIMESTAMPTZ,
+    username   VARCHAR(50)  NOT NULL UNIQUE,
+    email      VARCHAR(100) NOT NULL UNIQUE,
+    password   VARCHAR(60)  NOT NULL,
+    profile_id UUID,
+
+    CONSTRAINT fk_users__profile_id
+        FOREIGN KEY (profile_id)
+            REFERENCES binary_contents (id)
+            ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_statuses
+(
+    id             UUID PRIMARY KEY,
+    created_at     TIMESTAMPTZ NOT NULL,
+    updated_at     TIMESTAMPTZ,
+    user_id        UUID        NOT NULL UNIQUE,
+    last_active_at TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT fk_user_statuses__user_id
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE
+);
+
+CREATE TYPE channel_type AS ENUM ('PUBLIC', 'PRIVATE');
+
+CREATE TABLE IF NOT EXISTS channels
+(
+    id          UUID PRIMARY KEY,
+    created_at  TIMESTAMPTZ  NOT NULL,
+    updated_at  TIMESTAMPTZ,
+    name        VARCHAR(100) NOT NULL,
+    description VARCHAR(500),
+    type        channel_type NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS read_statuses
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ NOT NULL,
+    updated_at   TIMESTAMPTZ NOT NULL,
+    user_id      UUID        NOT NULL,
+    channel_id   UUID        NOT NULL,
+    last_read_at TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT uq_read_statuses__user_id__channel_id UNIQUE (user_id, channel_id),
+
+    CONSTRAINT fk_read_statuses__user_id
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_read_statuses__channel_id
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS messages
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ,
+    content    TEXT,
+    channel_id UUID        NOT NULL,
+    author_id  UUID,
+
+    CONSTRAINT fk_messages__channel_id
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_messages__author_id
+        FOREIGN KEY (author_id)
+            REFERENCES users (id)
+            ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS message_attachments
+(
+    message_id    UUID NOT NULL,
+    attachment_id UUID NOT NULL,
+
+    PRIMARY KEY (message_id, attachment_id),
+
+    CONSTRAINT fk_message_attachments__message_id
+        FOREIGN KEY (message_id)
+            REFERENCES messages (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_message_attachments__attachment_id
+        FOREIGN KEY (attachment_id)
+            REFERENCES binary_contents (id)
+            ON DELETE CASCADE
+);
