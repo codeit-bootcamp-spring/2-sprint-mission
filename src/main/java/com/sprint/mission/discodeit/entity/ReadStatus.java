96ad63d2aa4d8635d310d07bc594b5528a1e572b
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-15 16:24:00 +0900

    Feat: ReadStatus 엔티티 JPA 설정

--- src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -1,19 +1,37 @@
 package com.sprint.mission.discodeit.entity;
 
 import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.UUID;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Table(name = "read_statuses")
 @Getter
+@NoArgsConstructor
 public class ReadStatus extends BaseUpdatableEntity implements Serializable, Identifiable {
 
   private static final long serialVersionUID = 1L;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", unique = true)
   private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "channel_id", unique = true)
   private Channel channel;
+
+  @Column(name = "last_read_at", nullable = false)
   private Instant lastReadAt; // 마지막으로 메시지를 읽은 시간
 
   @Builder
