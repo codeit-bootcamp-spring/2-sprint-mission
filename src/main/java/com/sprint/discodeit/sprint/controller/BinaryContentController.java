cc985f5975bc7a327286b492a8c873f919188858
leejunnyeop <daffodil8620@naver.com>
2025-04-15 21:12:58 +0900

    [chore] BinaryContentController import 정리

--- src/main/java/com/sprint/discodeit/sprint/controller/BinaryContentController.java
+++ src/main/java/com/sprint/discodeit/sprint/controller/BinaryContentController.java
@@ -1,16 +1,13 @@
 package com.sprint.discodeit.sprint.controller;
 
 import com.sprint.discodeit.sprint.domain.dto.binaryContentDto.BinaryContentDto;
-import com.sprint.discodeit.sprint.domain.entity.BinaryContent;
 import com.sprint.discodeit.sprint.domain.storage.BinaryContentStorage;
 import com.sprint.discodeit.sprint.service.basic.util.BinaryContentService;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
