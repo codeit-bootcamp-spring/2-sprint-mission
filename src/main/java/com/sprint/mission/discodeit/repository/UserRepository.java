9fcd386df18ec9bde44a9fe64195f02c4705ca4b
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-15 17:01:38 +0900

    Refactor: UserRepository Spring Data JPA 설정

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,17 +1,15 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
-
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
-    User save(User user);
-    Optional<User> findById(UUID id);
-    Optional<User> findByUsername(String username);
-    List<User> findAll();
-    void deleteById(UUID id);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByUsername(String username);
+
+  boolean existsByUsername(String username);
+
+  boolean existsByEmail(String email);
 }
