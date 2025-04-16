4f4fb83b4c6e569117b7c9b41e1845b77707e648
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-16 15:10:06 +0900

    Feat: Repository JPA 도입에 따른 BasicUserStatusService 수정
    - 변경감지, 트랜잭션, 연관관계 등을 이용하여 로직 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
@@ -12,83 +12,93 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BasicUserStatusService implements UserStatusService {
-    private final UserRepository userRepository;
-    private final UserStatusRepository userStatusRepository;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Override
-    public UserStatus create(UserStatus userStatus) {
-        checkUserExists(userStatus);
-        checkDuplicateUser(userStatus);
-        return userStatusRepository.save(userStatus);
+  private final UserRepository userRepository;
+  private final UserStatusRepository userStatusRepository;
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Override
+  @Transactional
+  public UserStatus create(UserStatus userStatus) {
+    checkUserExists(userStatus);
+    checkDuplicateUser(userStatus);
+    return userStatusRepository.save(userStatus);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public UserStatus findById(UUID id) {
+    return findUserStatusById(id);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public UserStatus findByUserId(UUID userId) {
+    return findUserStatusByUserId(userId);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<UserStatus> findAll() {
+    List<UserStatus> userStatuses = userStatusRepository.findAll();
+    return userStatuses;
+  }
+
+  @Override
+  @Transactional
+  public UserStatus updateByUserId(UUID userId) {
+    UserStatus userStatus = findUserStatusByUserId(userId);
+    userStatus.updateUserStatus();
+    return userStatus;
+  }
+
+  @Override
+  @Transactional
+  public void delete(UUID id) {
+    userStatusRepository.deleteById(id);
+  }
+
+  @Override
+  @Transactional
+  public void deleteByUserId(UUID userId) {
+    userStatusRepository.deleteByUserId(userId);
+  }
+
+
+  private void checkUserExists(UserStatus userStatus) {
+    userRepository.findById(userStatus.getUser().getId())
+        .orElseThrow(() -> {
+          logger.error("유저상태 생성 중 유저 찾기 실패: {}", userStatus.getUser().getId());
+          return RestExceptions.USER_NOT_FOUND;
+        });
+  }
+
+  private void checkDuplicateUser(UserStatus userStatus) {
+    if (userStatusRepository.existsByUserId(userStatus.getUser().getId())) {
+      logger.error("유저상태 생성 중 유저상태 중복 발생: {}", userStatus.getId());
+      throw RestExceptions.DUPLICATE_USER_STATUS;
     }
-
-    @Override
-    public UserStatus findById(UUID id) {
-        return findUserStatusById(id);
-    }
-
-    @Override
-    public UserStatus findByUserId(UUID userId) {
-        return findUserStatusByUserId(userId);
-    }
-
-    @Override
-    public List<UserStatus> findAll() {
-        List<UserStatus> userStatuses = userStatusRepository.findAll();
-        return userStatuses;
-    }
-
-    @Override
-    public UserStatus updateByUserId(UUID userId) {
-        UserStatus userStatus = findUserStatusByUserId(userId);
-        userStatus.updateUserStatus();
-        return userStatusRepository.save(userStatus);
-    }
-
-    @Override
-    public void delete(UUID id) {
-        userStatusRepository.deleteById(id);
-    }
-
-    @Override
-    public void deleteByUserId(UUID userId) {
-        userStatusRepository.deleteByUserId(userId);
-    }
-
-    private void checkUserExists(UserStatus userStatus) {
-        userRepository.findById(userStatus.getUserId())
-                .orElseThrow(() -> {
-                    logger.error("유저상태 생성 중 유저 찾기 실패: {}", userStatus.getUserId());
-                    return RestExceptions.USER_NOT_FOUND;
-                });
-    }
-
-    private void checkDuplicateUser(UserStatus userStatus) {
-        if (userStatusRepository.existsByUserId(userStatus.getUserId())) {
-            logger.error("유저상태 생성 중 유저상태 중복 발생: {}", userStatus.getId());
-            throw RestExceptions.DUPLICATE_USER_STATUS;
-        }
-    }
+  }
 
 
-    private UserStatus findUserStatusByUserId(UUID userId) {
-        return userStatusRepository.findByUserId(userId)
-                .orElseThrow(() -> {
-                    logger.error("유저상태 조회 실패 - userId: {}", userId);
-                    return RestExceptions.USER_STATUS_NOT_FOUND;
-                });
-    }
+  private UserStatus findUserStatusByUserId(UUID userId) {
+    return userStatusRepository.findByUserId(userId)
+        .orElseThrow(() -> {
+          logger.error("유저상태 조회 실패 - userId: {}", userId);
+          return RestExceptions.USER_STATUS_NOT_FOUND;
+        });
+  }
 
-    private UserStatus findUserStatusById(UUID id) {
-        return userStatusRepository.findById(id)
-                .orElseThrow(() -> {
-                    logger.error("유저상태 조회 실패: {}", id);
-                    return RestExceptions.USER_STATUS_NOT_FOUND;
-                });
-    }
+  private UserStatus findUserStatusById(UUID id) {
+    return userStatusRepository.findById(id)
+        .orElseThrow(() -> {
+          logger.error("유저상태 조회 실패: {}", id);
+          return RestExceptions.USER_STATUS_NOT_FOUND;
+        });
+  }
 }
