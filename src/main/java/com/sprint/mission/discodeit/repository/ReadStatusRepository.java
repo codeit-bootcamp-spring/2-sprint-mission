e381411d138525b0fa31eb316f10addc09727a2e
Leekb0804 <lkb8713@gmail.com>
2025-04-16 15:34:33 +0900

    feat(Repository) : ChannelMapper 에서 필요했던 findAllUsersByChannelId 메서드 추가
    - N+1 문제를 방지하기위해

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -2,15 +2,21 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
 
+import com.sprint.mission.discodeit.entity.User;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
 
   List<ReadStatus> findAllByUserId(UUID userId);
 
   List<ReadStatus> findAllByChannelId(UUID channelId);
+
+  @Query("SELECT rs.user FROM ReadStatus rs WHERE rs.channel.id = :channelId")
+  List<User> findAllUsersByChannelId(@Param("channelId") UUID channelId);
 
   Boolean existsByUserIdAndChannelId(UUID userId, UUID channelId);
 
