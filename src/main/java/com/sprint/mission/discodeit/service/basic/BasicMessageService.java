265f75710471b0fb7af017461b5979ed0e3adb69
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-16 15:10:14 +0900

    Feat: Repository JPA 도입에 따른 BasicMessageService 수정
    - 변경감지, 트랜잭션, 연관관계 등을 이용하여 로직 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -1,32 +1,27 @@
 package com.sprint.mission.discodeit.service.basic;
 
-import com.sprint.mission.discodeit.dto.service.binarycontent.BinaryContentDTO;
 import com.sprint.mission.discodeit.dto.service.message.CreateMessageParam;
 import com.sprint.mission.discodeit.dto.service.message.MessageDTO;
 import com.sprint.mission.discodeit.dto.service.message.UpdateMessageDTO;
 import com.sprint.mission.discodeit.dto.service.message.UpdateMessageParam;
-import com.sprint.mission.discodeit.dto.service.user.UserDTO;
 import com.sprint.mission.discodeit.entity.*;
 import com.sprint.mission.discodeit.exception.RestExceptions;
 import com.sprint.mission.discodeit.mapper.MessageMapper;
-import com.sprint.mission.discodeit.mapper.UserMapper;
-import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -37,40 +32,40 @@ public class BasicMessageService implements MessageService {
 
   private final MessageRepository messageRepository;
   private final ChannelRepository channelRepository;
-  private final UserStatusService userStatusService;
   private final UserRepository userRepository;
   private final BinaryContentService binaryContentService;
-  private final UserMapper userMapper;
   private final MessageMapper messageMapper;
   private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
   @Override
+  @Transactional
   public MessageDTO create(CreateMessageParam createMessageParam,
       List<MultipartFile> multipartFiles) {
     // 유저와 채널이 실제로 존재하는지 검증
-    findUserById(createMessageParam.authorId());
-    findChannelById(createMessageParam.channelId());
+    User user = findUserById(createMessageParam.authorId());
+    Channel channel = findChannelById(createMessageParam.channelId());
 
     List<BinaryContent> binaryContentList = createBinaryContentList(multipartFiles);
     if (!binaryContentList.isEmpty() && binaryContentList != null) {
       binaryContentList.forEach(binaryContentService::create);
     }
 
-    Message message = messageMapper.toEntity(createMessageParam, binaryContentList);
+    Message message = messageMapper.toEntity(createMessageParam, user, channel, binaryContentList);
     messageRepository.save(message);
 
     return messageMapper.toMessageDTO(message);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public MessageDTO find(UUID messageId) {
     Message message = findMessageById(messageId);
-    UserDTO userDTO = createUserDTO(message.getAuthorId());
     return messageMapper.toMessageDTO(message);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public List<MessageDTO> findAllByChannelId(UUID channelId) {
     List<Message> messages = messageRepository.findAllByChannelId(channelId);
     return messages.stream()
@@ -79,43 +74,45 @@ public class BasicMessageService implements MessageService {
   }
 
   @Override
+  @Transactional
   public UpdateMessageDTO update(UUID id, UpdateMessageParam updateMessageParam,
       List<MultipartFile> multipartFiles) {
     Message message = findMessageById(id);
     message.updateMessageInfo(updateMessageParam.newContent());
-    replaceMessageAttachments(message, multipartFiles);
-    Message updatedMessage = messageRepository.save(message);
-    UserDTO userDTO = createUserDTO(updatedMessage.getAuthorId());
-    return messageMapper.toUpdateMessageDTO(updatedMessage, userDTO);
+    replaceAttachments(message, multipartFiles);
+    return messageMapper.toUpdateMessageDTO(message);
   }
 
+
   @Override
+  @Transactional
   public void delete(UUID messageId) {
     Message message = findMessageById(messageId);
-    if (message.getAttachmentIds() != null && !message.getAttachmentIds().isEmpty()) {
-      message.getAttachmentIds()
-          .forEach(binaryContentService::delete);
+    if (message.getAttachments() != null) {
+      message.getAttachments()
+          .forEach(
+              attachment -> binaryContentService.delete(attachment.getId()));
     }
     messageRepository.deleteById(messageId);
   }
 
   // multipartFiles 있을 시 원래 있던 binaryContent 삭제 후 수정 내용으로 재생성
-  private void replaceMessageAttachments(Message message, List<MultipartFile> multipartFiles) {
+  private void replaceAttachments(Message message, List<MultipartFile> multipartFiles) {
     // 디버깅 했을 때, multipartFile이 없어도 filename ="", size=0으로 들어오는 것을 발견하여, 이 경우 처리 X
     // 들어온 multipartFile이 없다면, 처리가 안될 것이고, 기존에 있던 이미지 유지
     if (multipartFiles.get(0).getSize() == 0) {
       return;
     }
-    message.getAttachmentIds().forEach(binaryContentService::delete);
+
+    message.getAttachments()
+        .forEach(attachment -> binaryContentService.delete(attachment.getId()));
 
     List<BinaryContent> binaryContentList = createBinaryContentList(multipartFiles);
     binaryContentList.forEach(binaryContentService::create);
 
-    List<UUID> binaryContentIdList = binaryContentList.stream()
-        .map(BinaryContent::getId)
-        .collect(Collectors.toList());
-
-    message.updateMessageAttachment(binaryContentIdList);
+    // 변경감지로 commit 시점에 message 테이블에 Update 쿼리 날아감
+    // + message가 JoinTable을 관리하므로, commit 시점에 message_attachments 테이블에도 Insert 쿼리 날아감
+    message.updateBinaryContent(binaryContentList);
   }
 
   private List<BinaryContent> createBinaryContentList(List<MultipartFile> multipartFiles) {
@@ -138,15 +135,6 @@ public class BasicMessageService implements MessageService {
           }
         })
         .collect(Collectors.toList());
-  }
-
-
-  private UserDTO createUserDTO(UUID userId) {
-    User user = findUserById(userId);
-    UserStatus userStatus = userStatusService.findByUserId(user.getId());
-    BinaryContentDTO binaryContentDTO =
-        user.getProfileId() != null ? binaryContentService.find(user.getProfileId()) : null;
-    return userMapper.toUserDTO(user, userStatus);
   }
 
 
