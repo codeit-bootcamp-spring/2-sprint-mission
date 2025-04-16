95168468617e50d61309bf0c9ed02574fd0240e8
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-16 15:10:46 +0900

    Feat: Repository JPA 도입에 따른 BasicBinaryContentService 수정
    - 변경감지, 트랜잭션, 연관관계 등을 이용하여 로직 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -1,7 +1,6 @@
 package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.service.binarycontent.BinaryContentDTO;
-import com.sprint.mission.discodeit.dto.service.binarycontent.CreateBinaryContentParam;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.exception.RestExceptions;
 import com.sprint.mission.discodeit.mapper.BinaryContentMapper;
@@ -13,47 +12,49 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BasicBinaryContentService implements BinaryContentService {
-    private final BinaryContentRepository binaryContentRepository;
-    private final BinaryContentMapper binaryContentMapper;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    // BinaryContentService는 Controller에서 호출되는 것이 아닌, MessageService나 UserService에서 호출되는 구조
-    // => 서비스간 호출이므로 엔티티를 받거나 반환해도 상관없다
-    @Override
-    public BinaryContent create(BinaryContent binaryContent) {
-        binaryContentRepository.save(binaryContent);
-        return binaryContent;
-    }
+  private final BinaryContentRepository binaryContentRepository;
+  private final BinaryContentMapper binaryContentMapper;
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Override
-    public BinaryContentDTO find(UUID id) {
-        BinaryContent binaryContent = findBinaryContentById(id);
-        return binaryContentMapper.toBinaryContentDTO(binaryContent);
-    }
+  @Override
+  @Transactional
+  public BinaryContent create(BinaryContent binaryContent) {
+    return binaryContentRepository.save(binaryContent);
+  }
 
-    @Override
-    // attachmentIds를 받아서 리스트를 조회
-    public List<BinaryContentDTO> findAllByIdIn(List<UUID> attachmentsId) {
-        return  attachmentsId.stream()
-                .map(id -> findBinaryContentById(id)) // attachmentsId로 BinaryContent 찾아서
-                .map(bc -> binaryContentMapper.toBinaryContentDTO(bc)) // DTO로 변환
-                .toList();
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public BinaryContentDTO find(UUID id) {
+    BinaryContent binaryContent = findBinaryContentById(id);
+    return binaryContentMapper.toBinaryContentDTO(binaryContent);
+  }
 
-    @Override
-    public void delete(UUID id) {
-        binaryContentRepository.deleteById(id);
-    }
+  @Override
+  @Transactional(readOnly = true)
+  // attachmentIds를 받아서 리스트를 조회
+  public List<BinaryContentDTO> findAllByIdIn(List<UUID> attachmentsId) {
+    return attachmentsId.stream()
+        .map(id -> findBinaryContentById(id)) // attachmentsId로 BinaryContent 찾아서
+        .map(bc -> binaryContentMapper.toBinaryContentDTO(bc)) // DTO로 변환
+        .toList();
+  }
 
-    private BinaryContent findBinaryContentById(UUID id) {
-        return binaryContentRepository.findById(id)
-                .orElseThrow(() -> {
-                    logger.error("binaryContent 찾기 실패: {}", id);
-                    return RestExceptions.BINARY_CONTENT_NOT_FOUND;
-                });
-    }
+  @Override
+  public void delete(UUID id) {
+    binaryContentRepository.deleteById(id);
+  }
+
+  private BinaryContent findBinaryContentById(UUID id) {
+    return binaryContentRepository.findById(id)
+        .orElseThrow(() -> {
+          logger.error("binaryContent 찾기 실패: {}", id);
+          return RestExceptions.BINARY_CONTENT_NOT_FOUND;
+        });
+  }
 }
