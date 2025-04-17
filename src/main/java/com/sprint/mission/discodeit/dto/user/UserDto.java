5ac74a6cdb7ca7740e9ef3dd8bfb5493e1faf452
manleKim <kk410910@naver.com>
2025-04-17 09:46:14 +0900

    feat: API 스펙 맞추기 -> DTO 수정 및 추가

--- src/main/java/com/sprint/mission/discodeit/dto/user/UserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/UserDto.java
@@ -1,20 +1,47 @@
 package com.sprint.mission.discodeit.dto.user;
 
+import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentDto;
+import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.User;
-import java.time.Instant;
 import java.util.UUID;
 
 public record UserDto(
         UUID id,
         String username,
         String email,
-        UUID profileId,
-        Instant createdAt,
-        Instant updatedAt,
+        BinaryContentDto profile,
         boolean online
 ) {
+
+    public UserDto(User user, BinaryContentDto profile, boolean online) {
+        this(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                profile,
+                online
+        );
+    }
+
     public UserDto(User user, boolean online) {
-        this(user.getId(), user.getUsername(), user.getEmail(), user.getProfileId(),
-                user.getCreatedAt(), user.getUpdatedAt(), online);
+        this(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                toBinaryContentDto(user.getProfile()),
+                online
+        );
+    }
+
+    private static BinaryContentDto toBinaryContentDto(BinaryContent profile) {
+        if (profile == null) {
+            return null;
+        }
+        return new BinaryContentDto(
+                profile.getId(),
+                profile.getFileName(),
+                profile.getSize(),
+                profile.getContentType()
+        );
     }
 }
