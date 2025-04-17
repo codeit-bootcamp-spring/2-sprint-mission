29b944f286363043527f61225c0afc2f328aa1b3
Leekb0804 <lkb8713@gmail.com>
2025-04-17 12:06:15 +0900

    refactor(Service) : 메서드가 UserStatus Entity가 아닌 UserStatusDto를 반환하도록 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
@@ -1,9 +1,11 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.dto.data.UserStatusDto;
 import com.sprint.mission.discodeit.dto.request.UserStatusCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.mapper.UserStatusMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.UserStatusService;
@@ -23,50 +25,49 @@ public class BasicUserStatusService implements UserStatusService {
 
   private final UserStatusRepository userStatusRepository;
   private final UserRepository userRepository;
+  private final UserStatusMapper userStatusMapper;
 
   @Override
-  public UserStatus create(UserStatusCreateRequest request) {
+  public UserStatusDto create(UserStatusCreateRequest request) {
     UUID userId = request.userId();
     User user = userRepository.findById(userId)
         .orElseThrow(
             () -> new NoSuchElementException("User with id " + userId + " not found")
         );
 
-    if (userStatusRepository.findByUserId(userId).isPresent()) {
+    if (!userStatusRepository.existsByUserId(userId)) {
       throw new IllegalArgumentException("UserStatus with id " + userId + " already exists");
     }
 
     Instant lastActiveAt = request.lastActiveAt();
     UserStatus userStatus = new UserStatus(user, lastActiveAt);
-    return userStatusRepository.save(userStatus);
+    return userStatusMapper.toDto(userStatusRepository.save(userStatus));
   }
 
   @Override
-  public UserStatus find(UUID userStatusId) {
-    return userStatusRepository.findById(userStatusId)
-        .orElseThrow(
-            () -> new NoSuchElementException("UserStatus with id " + userStatusId + " not found")
-        );
+  public UserStatusDto find(UUID userStatusId) {
+    return userStatusMapper.toDto(this.findUserStatus(userStatusId));
   }
 
   @Override
-  public List<UserStatus> findAll() {
+  public List<UserStatusDto> findAll() {
     return userStatusRepository.findAll().stream()
+        .map(userStatusMapper::toDto)
         .toList();
   }
 
   @Override
-  public UserStatus update(UUID userStatusId, UserStatusUpdateRequest request) {
+  public UserStatusDto update(UUID userStatusId, UserStatusUpdateRequest request) {
     Instant newLastActiveAt = request.newLastActiveAt();
 
-    UserStatus userStatus = this.find(userStatusId);
+    UserStatus userStatus = this.findUserStatus(userStatusId);
     userStatus.update(newLastActiveAt);
 
-    return userStatus;
+    return userStatusMapper.toDto(userStatus);
   }
 
   @Override
-  public UserStatus updateByUserId(UUID userId, UserStatusUpdateRequest request) {
+  public UserStatusDto updateByUserId(UUID userId, UserStatusUpdateRequest request) {
     Instant newLastActiveAt = request.newLastActiveAt();
 
     UserStatus userStatus = userStatusRepository.findByUserId(userId)
@@ -74,12 +75,19 @@ public class BasicUserStatusService implements UserStatusService {
             () -> new NoSuchElementException("UserStatus with userId " + userId + " not found"));
     userStatus.update(newLastActiveAt);
 
-    return userStatus;
+    return userStatusMapper.toDto(userStatus);
   }
 
   @Override
   public void delete(UUID userStatusId) {
-    UserStatus userStatus = this.find(userStatusId);
+    UserStatus userStatus = this.findUserStatus(userStatusId);
     userStatusRepository.delete(userStatus);
+  }
+
+  private UserStatus findUserStatus(UUID userStatusId) {
+    return userStatusRepository.findById(userStatusId)
+        .orElseThrow(
+            () -> new NoSuchElementException("UserStatus with id " + userStatusId + " not found")
+        );
   }
 }
