59567af5c777679330b49afe65645c4c44ad2f32
Leekb0804 <lkb8713@gmail.com>
2025-04-17 17:28:47 +0900

    refactor(PageResponseMapper) : Spring Bean으로 등록

--- src/main/java/com/sprint/mission/discodeit/mapper/PageResponseMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/PageResponseMapper.java
@@ -3,7 +3,9 @@ package com.sprint.mission.discodeit.mapper;
 import com.sprint.mission.discodeit.dto.response.PageResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PageResponseMapper {
 
   public <T> PageResponse<T> fromSlice(Slice<T> slice) {
