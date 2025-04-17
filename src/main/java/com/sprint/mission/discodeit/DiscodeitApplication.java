64fb59c9367e037211109ac10759e52b6b231a3a
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-17 13:15:11 +0900

    Refactor: @CreatedDate, @LastModifiedDate 관련 설정 추가
    - @CreatedDate와 @LastModifiedDate 적용을 위함

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,15 +1,16 @@
 package com.sprint.mission.discodeit;
-import com.sprint.mission.discodeit.entity.BinaryContent;
-import com.sprint.mission.discodeit.repository.*;
-import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DiscodeitApplication {
-    public static void main(String[] args) {
-        ApplicationContext context =  SpringApplication.run(DiscodeitApplication.class, args);
-    }
+
+  public static void main(String[] args) {
+    ApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+  }
 }
--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.entity.base;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,10 +11,12 @@ import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @NoArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
   @Id
