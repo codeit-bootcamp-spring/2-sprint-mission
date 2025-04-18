cc043403221af745a10f758ba9a9060a73866af2
manleKim <kk410910@naver.com>
2025-04-18 17:26:34 +0900

    modify: bytes를 로컬로 분리하면서 DB 속성에서 제외

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,8 +19,7 @@ CREATE TABLE binary_contents
     created_at   TIMESTAMPTZ  NOT NULL,
     file_name    VARCHAR(255) NOT NULL,
     size         BIGINT       NOT NULL,
-    content_type VARCHAR(100) NOT NULL,
-    bytes        BYTEA        NOT NULL
+    content_type VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE user_statuses
