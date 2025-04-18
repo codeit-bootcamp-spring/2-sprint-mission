b25149ee2b0ecf837898410acce5a61be870fb5e
Leekb0804 <lkb8713@gmail.com>
2025-04-18 14:38:04 +0900

    feat(MessageRespository) : 커서 페이지네이션을 위한 findAllByChannelIdAndCreatedAtLessThan() 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -2,7 +2,7 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Message;
 
-import java.util.List;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +12,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MessageRepository extends JpaRepository<Message, UUID> {
 
   Slice<Message> findAllByChannelId(UUID channelId, Pageable pageable);
+
+  Slice<Message> findAllByChannelIdAndCreatedAtLessThan(UUID channelId, Instant cratedAt,
+      Pageable pageable);
 
   Optional<Message> findFirstByChannelIdOrderByCreatedAtDesc(UUID channelId);
 
