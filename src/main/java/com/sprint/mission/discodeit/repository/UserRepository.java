d1cf3426b519303dd84e4662c40ec450268cdb2f
Leekb0804 <lkb8713@gmail.com>
2025-04-20 16:48:54 +0900

    feat(User Repository) : findAllWithProfileAndStatus() 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -2,13 +2,18 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
   Optional<User> findByUsername(String username);
+
+  @Query("select u from User u left join fetch u.profile left join fetch u.status")
+  List<User> findAllWithProfileAndStatus();
 
   boolean existsByEmail(String email);
 
