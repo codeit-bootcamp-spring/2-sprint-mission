fb183aec7d7c9c1c1cf6ae40b9f64514f9fa3672
Leekb0804 <lkb8713@gmail.com>
2025-04-20 16:51:26 +0900

    refactor(ReadStatus Entity) : user, channel 필드에 LAZY fetch 적용

--- src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -21,11 +22,17 @@ import java.time.Instant;
 @Getter
 public class ReadStatus extends BaseUpdatableEntity {
 
-  @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @ManyToOne(
+      fetch = FetchType.LAZY,
+      cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+  )
   @JoinColumn(name = "user_id")
   private User user;
 
-  @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @ManyToOne(
+      fetch = FetchType.LAZY,
+      cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+  )
   @JoinColumn(name = "channel_id")
   private Channel channel;
 
