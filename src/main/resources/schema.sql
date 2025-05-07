ba05ac5588ae01d2bdb6858816deff317380e2b7
Seong-taeHan <kmljjn22@gmail.com>
2025-05-07 16:27:03 +0900

    refactor: 마일스톤 변경사항 적용

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,20 +9,20 @@
 CREATE TABLE IF NOT EXISTS binary_contents
 (
     id           UUID PRIMARY KEY,
-    created_at   TIMESTAMPTZ  NOT NULL,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
     file_name    VARCHAR(255),
-    size         BIGINT       NOT NULL,
-    content_type varchar(100) NOT NULL
+    size         BIGINT                   NOT NULL,
+    content_type varchar(100)             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
     id         UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ         NOT NULL,
-    updated_at TIMESTAMPTZ,
-    username   VARCHAR(50) UNIQUE  NOT NULL,
-    email      VARCHAR(100) UNIQUE NOT NULL,
-    password   VARCHAR(60)         NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    username   VARCHAR(50) UNIQUE       NOT NULL,
+    email      VARCHAR(100) UNIQUE      NOT NULL,
+    password   VARCHAR(60)              NOT NULL,
     profile_id UUID,
     constraint fk_users_binary_contents FOREIGN KEY (profile_id) REFERENCES binary_contents (id)
 );
@@ -30,18 +30,18 @@ CREATE TABLE IF NOT EXISTS users
 CREATE TABLE IF NOT EXISTS user_statuses
 (
     id             UUID PRIMARY KEY,
-    created_at     TIMESTAMPTZ NOT NULL,
-    updated_at     TIMESTAMPTZ,
+    created_at     TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at     TIMESTAMP WITH TIME ZONE,
     user_id        UUID UNIQUE,
-    last_active_at TIMESTAMPTZ NOT NULL,
+    last_active_at TIMESTAMP WITH TIME ZONE NOT NULL,
     constraint fk_user_status_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS channels
 (
     id          UUID PRIMARY KEY,
-    created_at  TIMESTAMPTZ                                        NOT NULL,
-    updated_at  TIMESTAMPTZ,
+    created_at  TIMESTAMP WITH TIME ZONE                           NOT NULL,
+    updated_at  TIMESTAMP WITH TIME ZONE,
     name        VARCHAR(100),
     description VARCHAR(500),
     type        VARCHAR(10) CHECK ( type IN ('PUBLIC', 'PRIVATE')) NOT NULL
@@ -50,11 +50,11 @@ CREATE TABLE IF NOT EXISTS channels
 CREATE TABLE IF NOT EXISTS read_statuses
 (
     id           UUID PRIMARY KEY,
-    created_at   TIMESTAMPTZ NOT NULL,
-    update_at    TIMESTAMPTZ,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    update_at    TIMESTAMP WITH TIME ZONE,
     user_id      UUID,
     channel_id   UUID,
-    last_read_at TIMESTAMPTZ NOT NULL,
+    last_read_at TIMESTAMP WITH TIME ZONE NOT NULL,
     CONSTRAINT fk_read_status_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_read_status_channels FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE
 );
@@ -62,8 +62,8 @@ CREATE TABLE IF NOT EXISTS read_statuses
 CREATE TABLE IF NOT EXISTS messages
 (
     id         UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
     content    TEXT,
     channel_id UUID,
     author_id  UUID,
