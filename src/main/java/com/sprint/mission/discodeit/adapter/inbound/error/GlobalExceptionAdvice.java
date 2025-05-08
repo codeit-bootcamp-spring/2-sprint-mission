02d651841ee03358827f30be8bd3e38c5e08c7d1
JongwonLee <gudtksfl@gmail.com>
2025-05-08 11:34:17 +0900

    feat: Method Argument 전역 예외 핸들러 처리

--- src/main/java/com/sprint/mission/discodeit/adapter/inbound/error/GlobalExceptionAdvice.java
+++ src/main/java/com/sprint/mission/discodeit/adapter/inbound/error/GlobalExceptionAdvice.java
@@ -1,26 +1,26 @@
 package com.sprint.mission.discodeit.adapter.inbound.error;
 
+import com.sprint.mission.discodeit.core.channel.exception.ChannelUnmodifiableException;
 import com.sprint.mission.discodeit.core.user.exception.UserAlreadyExistsException;
-import com.sprint.mission.discodeit.exception.ErrorCode;
 import com.sprint.mission.discodeit.core.user.exception.UserLoginFailedException;
 import com.sprint.mission.discodeit.core.user.exception.UserNotFoundException;
-import com.sprint.mission.discodeit.core.channel.exception.ChannelUnmodifiableException;
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import java.time.Instant;
 import java.util.Locale;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
+@Slf4j
 public class GlobalExceptionAdvice {
 
   private final MessageSource messageSource;
-
-  private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionAdvice.class);
 
   @ExceptionHandler(UserNotFoundException.class)
   public ResponseEntity<ErrorResponse> handleNotFoundException(UserNotFoundException ex,
@@ -29,7 +29,7 @@ public class GlobalExceptionAdvice {
 
     String message = messageSource.getMessage(errorCode.getMessage(), ex.getArgs(), locale);
     ErrorResponse response = ErrorResponse.of(ex.getTimestamp(), errorCode, message, ex.toString());
-    logger.error(message);
+    log.error(message);
     return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
   }
 
@@ -40,7 +40,7 @@ public class GlobalExceptionAdvice {
 
     String message = messageSource.getMessage(errorCode.getMessage(), ex.getArgs(), locale);
     ErrorResponse response = ErrorResponse.of(ex.getTimestamp(), errorCode, message, ex.toString());
-    logger.error(message);
+    log.error(message);
     return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
   }
 
@@ -51,7 +51,7 @@ public class GlobalExceptionAdvice {
 
     String message = messageSource.getMessage(errorCode.getMessage(), ex.getArgs(), locale);
     ErrorResponse response = ErrorResponse.of(ex.getTimestamp(), errorCode, message, ex.toString());
-    logger.error(message);
+    log.error(message);
     return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
   }
 
@@ -62,8 +62,18 @@ public class GlobalExceptionAdvice {
 
     String message = messageSource.getMessage(errorCode.getMessage(), ex.getArgs(), locale);
     ErrorResponse response = ErrorResponse.of(ex.getTimestamp(), errorCode, message, ex.toString());
-    logger.error(message);
+    log.error(message);
     return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
   }
 
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleArgumentNotValidException(
+      MethodArgumentNotValidException ex, Locale locale) {
+    ErrorCode errorCode = ErrorCode.METHOD_ARGUMENT_NOT_VALID;
+    String message = messageSource.getMessage(errorCode.getMessage(),
+        null, locale);
+    ErrorResponse response = ErrorResponse.of(Instant.now(), errorCode, message, ex.toString());
+    log.error(message);
+    return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
+  }
 }
