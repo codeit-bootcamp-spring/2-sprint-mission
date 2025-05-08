a3030177492191d19e30daf0bf83179c8af2c420
Seong-taeHan <kmljjn22@gmail.com>
2025-05-08 09:02:53 +0900

    fix: 사용자 수정 오류 해결 완료
    - update메소드 실행 시 이미지 아이디가 null값으로
      출력되던 문제 해결
    - 줄간격 4칸으로 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -24,126 +24,136 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class BasicUserService implements UserService {
 
-  private final UserRepository userRepository;
-  private final UserMapper userMapper;
-  private final BinaryContentStorage binaryContentStorage;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final BinaryContentStorage binaryContentStorage;
 
-  @Override
-  @Transactional
-  public UserDto save(UserCreateRequest userCreateRequest, MultipartFile profile)
-      throws IOException {
-    log.info("사용자 생성 진행: username = {}, email = {}", userCreateRequest.username(),
-        userCreateRequest.email());
+    @Override
+    @Transactional
+    public UserDto save(UserCreateRequest userCreateRequest, MultipartFile profile)
+        throws IOException {
+        log.info("사용자 생성 진행: username = {}, email = {}", userCreateRequest.username(),
+            userCreateRequest.email());
 
-    if (userRepository.findByUsername(userCreateRequest.username()).isPresent()) {
-      log.warn("사용자 생성 중 이미 존재하는 사용자 이름 발견: username = {}", userCreateRequest.username());
-      throw new IllegalArgumentException(
-          String.format("User with username %s already exists", userCreateRequest.username()));
+        if (userRepository.findByUsername(userCreateRequest.username()).isPresent()) {
+            log.warn("사용자 생성 중 이미 존재하는 사용자 이름 발견: username = {}", userCreateRequest.username());
+            throw new IllegalArgumentException(
+                String.format("User with username %s already exists",
+                    userCreateRequest.username()));
+        }
+
+        if (userRepository.findByEmail(userCreateRequest.email()).isPresent()) {
+            log.warn("사용자 생성 중 이미 존재하는 이메일: email = {}", userCreateRequest.email());
+            throw new IllegalArgumentException(
+                String.format("User with email %s already exists", userCreateRequest.email()));
+        }
+
+        BinaryContent binaryContent = null;
+        if (profile != null && !profile.isEmpty()) {
+            log.debug("사용자 생성 중 프로필 이미지 메타데이터 저장: filename = {}, contentType = {}, size = {}",
+                profile.getOriginalFilename(), profile.getContentType(), profile.getSize());
+            binaryContent = BinaryContent.builder()
+                .fileName(profile.getOriginalFilename())
+                .contentType(profile.getContentType())
+                .size((long) profile.getBytes().length)
+                .build();
+        }
+
+        User user = new User(
+            userCreateRequest.username(),
+            userCreateRequest.password(),
+            userCreateRequest.email(),
+            binaryContent);
+
+        userRepository.save(user);
+
+        if (binaryContent != null) {
+            log.debug("사용자 생성 중 프로필 저장: profileId = {}", binaryContent.getId());
+            binaryContentStorage.put(binaryContent.getId(), profile.getBytes());
+        }
+
+        log.info("사용자 생성 완료: userId = {}", user.getId());
+        return userMapper.toDto(user);
     }
 
-    if (userRepository.findByEmail(userCreateRequest.email()).isPresent()) {
-      log.warn("사용자 생성 중 이미 존재하는 이메일: email = {}", userCreateRequest.email());
-      throw new IllegalArgumentException(
-          String.format("User with email %s already exists", userCreateRequest.email()));
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserDto> findAllUser() {
+        return userRepository.findAllWithProfileAndStatus().stream()
+            .map(userMapper::toDto)
+            .toList();
     }
 
-    BinaryContent binaryContent = null;
-    if (profile != null && !profile.isEmpty()) {
-      log.debug("사용자 생성 중 프로필 이미지 메타데이터 저장: filename = {}, contentType = {}, size = {}",
-          profile.getOriginalFilename(), profile.getContentType(), profile.getSize());
-      binaryContent = BinaryContent.builder()
-          .fileName(profile.getOriginalFilename())
-          .contentType(profile.getContentType())
-          .size((long) profile.getBytes().length)
-          .build();
+    @Override
+    @Transactional
+    public UserDto update(UUID userId, UserUpdateRequest userUpdateRequest,
+        MultipartFile profile) throws IOException {
+        log.info("사용자 수정 진행: userId={}", userId);
+
+        User user = userRepository.findById(userId).
+            orElseThrow(() -> {
+                log.error("사용자 수정 중 대상 사용자를 찾을 수 없음: userId={}", userId);
+                return new NoSuchElementException(
+                    String.format("User with id %s not found", userId));
+            });
+
+        if (userRepository.findByUsername(userUpdateRequest.newUsername())
+            .filter(otherUser -> !otherUser.getId().equals(user.getId()))
+            .isPresent()) {
+            log.warn("사용자 수정중 중복된 사용자 이름 발견: newUsername = {}", userUpdateRequest.newUsername());
+            throw new IllegalArgumentException(
+                String.format("User with username %s already exists",
+                    userUpdateRequest.newUsername()));
+        }
+
+        if (userRepository.findByEmail(userUpdateRequest.newEmail())
+            .filter(otherUser -> !otherUser.getId().equals(user.getId()))
+            .isPresent()) {
+            log.warn("사용자 수정중 중복된 이메일 발견: newEmail = {}", userUpdateRequest.newEmail());
+            throw new IllegalArgumentException(
+                String.format("User with email %s already exists", userUpdateRequest.newEmail()));
+        }
+
+        BinaryContent binaryContent = null;
+        if (profile != null && !profile.isEmpty()) {
+            log.debug("사용자 수정 중 프로필 이미지 메타데이터 저장: filename = {}, contentType = {}, size = {}",
+                profile.getOriginalFilename(), profile.getContentType(), profile.getSize());
+            binaryContent = BinaryContent.builder()
+                .fileName(profile.getOriginalFilename())
+                .contentType(profile.getContentType())
+                .size((long) profile.getBytes().length)
+                .build();
+
+            user.updateProfile(binaryContent);
+        }
+
+        user.updateUsername(userUpdateRequest.newUsername());
+        user.updatePassword(userUpdateRequest.newPassword());
+        user.updateEmail(userUpdateRequest.newEmail());
+
+        User updatedUser = userRepository.saveAndFlush(user);
+
+        log.info("사용자 수정 완료: userId = {}", user.getId());
+
+        if (updatedUser.getProfile() != null) {
+            System.out.println(updatedUser.getProfile().getId());
+            log.debug("프로필 이미지 파일 저장: profileId = {}", updatedUser.getProfile().getId());
+            binaryContentStorage.put(updatedUser.getProfile().getId(), profile.getBytes());
+        }
+
+        return userMapper.toDto(updatedUser);
     }
 
-    User user = new User(
-        userCreateRequest.username(),
-        userCreateRequest.password(),
-        userCreateRequest.email(),
-        binaryContent);
-
-    userRepository.save(user);
-
-    if (binaryContent != null) {
-      log.debug("사용자 생성 중 프로필 저장: profileId = {}", binaryContent.getId());
-      binaryContentStorage.put(binaryContent.getId(), profile.getBytes());
+    @Override
+    @Transactional
+    public void delete(UUID userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> {
+                log.error("사용자 삭제 중 사용자를 찾을 수 없음: userId = {}", userId);
+                return new NoSuchElementException(
+                    String.format("User with id %s not found", userId));
+            });
+        log.info("사용자 삭제 완료: userId = {}", userId);
+        userRepository.delete(user);
     }
-
-    log.info("사용자 생성 완료: userId = {}", user.getId());
-    return userMapper.toDto(user);
-  }
-
-  @Override
-  @Transactional(readOnly = true)
-  public List<UserDto> findAllUser() {
-    return userRepository.findAllWithProfileAndStatus().stream()
-        .map(userMapper::toDto)
-        .toList();
-  }
-
-  @Override
-  @Transactional
-  public UserDto update(UUID userId, UserUpdateRequest userUpdateRequest,
-      MultipartFile profile) throws IOException {
-    log.info("사용자 수정 진행: userId={}", userId);
-
-    User user = userRepository.findById(userId).
-        orElseThrow(() -> {
-          log.error("사용자 수정 중 대상 사용자를 찾을 수 없음: userId={}", userId);
-          return new NoSuchElementException(String.format("User with id %s not found", userId));
-        });
-
-    if (userRepository.findByUsername(userUpdateRequest.newUsername())
-        .filter(otherUser -> !otherUser.getId().equals(user.getId()))
-        .isPresent()) {
-      log.warn("사용자 수정중 중복된 사용자 이름 발견: newUsername = {}", userUpdateRequest.newUsername());
-      throw new IllegalArgumentException(
-          String.format("User with username %s already exists", userUpdateRequest.newUsername()));
-    }
-
-    if (userRepository.findByEmail(userUpdateRequest.newEmail())
-        .filter(otherUser -> !otherUser.getId().equals(user.getId()))
-        .isPresent()) {
-      log.warn("사용자 수정중 중복된 이메일 발견: newEmail = {}", userUpdateRequest.newEmail());
-      throw new IllegalArgumentException(
-          String.format("User with email %s already exists", userUpdateRequest.newEmail()));
-    }
-
-    BinaryContent binaryContent = null;
-    if (profile != null && !profile.isEmpty()) {
-      log.debug("사용자 수정 중 프로필 이미지 메타데이터 저장: filename = {}, contentType = {}, size = {}",
-          profile.getOriginalFilename(), profile.getContentType(), profile.getSize());
-      binaryContent = BinaryContent.builder()
-          .fileName(profile.getOriginalFilename())
-          .contentType(profile.getContentType())
-          .size((long) profile.getBytes().length)
-          .build();
-
-      log.debug("사용자 수정중 프로필 저장: profileId = {}", binaryContent.getId());
-      binaryContentStorage.put(binaryContent.getId(), profile.getBytes());
-    }
-
-    user.updateUsername(userUpdateRequest.newUsername());
-    user.updatePassword(userUpdateRequest.newPassword());
-    user.updateEmail(userUpdateRequest.newEmail());
-    user.updateProfile(binaryContent);
-
-    log.info("사용자 수정 완료: userId = {}", user.getId());
-
-    return userMapper.toDto(user);
-  }
-
-  @Override
-  @Transactional
-  public void delete(UUID userId) {
-    User user = userRepository.findById(userId)
-        .orElseThrow(() -> {
-          log.error("사용자 삭제 중 사용자를 찾을 수 없음: userId = {}", userId);
-          return new NoSuchElementException(String.format("User with id %s not found", userId));
-        });
-    log.info("사용자 삭제 완료: userId = {}", userId);
-    userRepository.delete(user);
-  }
 }
