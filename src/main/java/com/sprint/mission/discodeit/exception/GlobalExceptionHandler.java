e3b5da3f231e1bafe6232f6547ac9024b8046986
Bokyung Kim <chloeee0228@gmail.com>
2025-05-08 15:46:50 +0900

    feat: 예외 처리 고도화 - ErrorCode enum 생성 - DiscodeitException 클래스 구현 - ErrorResponse 클래스 구현 - GlobalExceptionHandler 수정

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,33 +1,46 @@
 package com.sprint.mission.discodeit.exception;
 
+import com.sprint.mission.discodeit.dto.error.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<String> handleException(IllegalArgumentException e) {
+  @ExceptionHandler(DiscodeitException.class)
+  public ResponseEntity<ErrorResponse> handleDiscodeitException(DiscodeitException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    ErrorResponse errorResponse = new ErrorResponse(
+            e.getTimestamp(),
+            errorCode.getHttpStatus().value(),
+            errorCode.name(),
+            errorCode.getMessage(),
+            e.getDetails(),
+            e.getClass().getSimpleName()
+    );
     return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(e.getMessage());
-  }
-
-  @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<String> handleException(NoSuchElementException e) {
-    return ResponseEntity
-        .status(HttpStatus.NOT_FOUND)
-        .body(e.getMessage());
+            .status(errorCode.getHttpStatus())
+            .body(errorResponse);
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleException(Exception e) {
+  public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    ErrorResponse errorResponse = new ErrorResponse(
+            Instant.now(),
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            ErrorCode.INTERNAL_SERVER_ERROR.name(),
+            ErrorCode.INTERNAL_SERVER_ERROR.getMessage(),
+            Map.of("exception", e.getClass().getSimpleName()),
+            e.getClass().getSimpleName()
+    );
     return ResponseEntity
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(e.getMessage());
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(errorResponse);
   }
 }
