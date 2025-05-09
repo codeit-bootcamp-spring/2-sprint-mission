590045b2cdb43c7bc32463077e42e3cd0cbb2316
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-10 03:29:53 +0900

    Feat: Message 도메인 예외 세분화

---
+++ src/main/java/com/sprint/mission/discodeit/exception/message/MessageExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.exception.message;
+
+import com.sprint.mission.discodeit.exception.ResponseErrorBody;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MessageExceptionHandler {
+
+  @ExceptionHandler(MessageNotFoundException.class)
+  public ResponseEntity<ResponseErrorBody> handleMessageNotFoundException(
+      MessageNotFoundException e) {
+    log.warn("MessageNotFoundException handled: {}, details: {}", e.getMessage(), e.getDetails());
+    return ResponseEntity.status(e.getResultCode().getStatus()).body(new ResponseErrorBody(e));
+  }
+
+}
