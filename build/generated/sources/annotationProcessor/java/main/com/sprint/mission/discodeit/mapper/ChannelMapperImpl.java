cb558779cde728c79463576bacb0ba2828b8cfa4
Jaeseok Heo <jasonheo1998@gmail.com>
2025-05-12 17:50:10 +0900

    BasicChannelServiceTest 추가구현

--- build/generated/sources/annotationProcessor/java/main/com/sprint/mission/discodeit/mapper/ChannelMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/sprint/mission/discodeit/mapper/ChannelMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-09T14:09:44+0900",
+    date = "2025-05-12T17:49:09+0900",
     comments = "version: 1.6.3, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.11.1.jar, environment: Java 17.0.14 (Amazon.com Inc.)"
 )
 @Component
