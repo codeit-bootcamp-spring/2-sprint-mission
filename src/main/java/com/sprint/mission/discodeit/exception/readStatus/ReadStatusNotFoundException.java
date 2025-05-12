aba848108bb37319b24216d7536e6aa84bbf10f0
manleKim <kk410910@naver.com>
2025-05-12 17:43:59 +0900

    feat: ReadStatusException들 정의

---
+++ src/main/java/com/sprint/mission/discodeit/exception/readStatus/ReadStatusNotFoundException.java
@@ -0,0 +1,19 @@
+package com.sprint.mission.discodeit.exception.readStatus;
+
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import java.util.Map;
+import java.util.UUID;
+
+public class ReadStatusNotFoundException extends ReadStatusException {
+    public ReadStatusNotFoundException(UUID readStatusId) {
+        super(ErrorCode.READ_STATUS_NOT_FOUND,
+                Map.of("readStatusId", readStatusId)
+        );
+    }
+
+    public ReadStatusNotFoundException(String userId, String channelId) {
+        super(ErrorCode.READ_STATUS_NOT_FOUND,
+                Map.of("userId", userId, "channelId", channelId)
+        );
+    }
+}
