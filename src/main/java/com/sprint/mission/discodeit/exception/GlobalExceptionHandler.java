fd7a13da6bf6bfe8cd1ca6a8b80f73f3efda4236
Leekb0804 <lkb8713@gmail.com>
2025-05-12 10:29:04 +0900

    feat: @ExceptionHandler에 StorageException.class 추가

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.sprint.mission.discodeit.exception.binaryContent.BinaryContentExcepti
 import com.sprint.mission.discodeit.exception.channel.ChannelException;
 import com.sprint.mission.discodeit.exception.message.MessageException;
 import com.sprint.mission.discodeit.exception.readStatus.ReadStatusException;
+import com.sprint.mission.discodeit.exception.storage.StorageException;
 import com.sprint.mission.discodeit.exception.user.UserException;
 import com.sprint.mission.discodeit.exception.userStatus.UserStatusException;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,7 @@ public class GlobalExceptionHandler {
       ChannelException.class,
       MessageException.class,
       ReadStatusException.class,
+      StorageException.class,
       UserException.class,
       UserStatusException.class
   })
