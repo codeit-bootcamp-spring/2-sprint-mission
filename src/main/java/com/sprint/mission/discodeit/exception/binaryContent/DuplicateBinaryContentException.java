1c496301580bf6888c2a0d9952389c9b6140729b
Leekb0804 <lkb8713@gmail.com>
2025-05-13 10:10:08 +0900

    refactor: 사용하지않는 import 삭제

--- src/main/java/com/sprint/mission/discodeit/exception/binaryContent/DuplicateBinaryContentException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/binaryContent/DuplicateBinaryContentException.java
@@ -3,7 +3,6 @@ package com.sprint.mission.discodeit.exception.binaryContent;
 import com.sprint.mission.discodeit.exception.ErrorCode;
 import java.time.Instant;
 import java.util.Map;
-import jdk.incubator.vector.VectorOperators.Binary;
 
 public class DuplicateBinaryContentException extends BinaryContentException {
   public DuplicateBinaryContentException(
