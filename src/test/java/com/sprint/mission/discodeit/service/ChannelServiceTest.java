c35467e9acf2723214e901ffab397a17c101dcbf
hyorim <tg305@naver.com>
2025-05-13 10:33:58 +0900

    test: ChannelService update 메서드 테스트 추가
    - Public 채널 업데이트 성공 테스트
    - 존재하지 않는 channelId로 업데이트 시 예외 발생 테스트
    - Private 채널 업데이트 시 예외 발생 테스트

--- src/test/java/com/sprint/mission/discodeit/service/ChannelServiceTest.java
+++ src/test/java/com/sprint/mission/discodeit/service/ChannelServiceTest.java
@@ -3,9 +3,12 @@ package com.sprint.mission.discodeit.service;
 import com.sprint.mission.discodeit.dto.channel.ChannelDto;
 import com.sprint.mission.discodeit.dto.channel.PrivateChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.channel.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.channel.PublicChannelUpdateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.exception.channel.ChannelNotFoundException;
+import com.sprint.mission.discodeit.exception.channel.PrivateChannelUpdateException;
 import com.sprint.mission.discodeit.mapper.ChannelMapper;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
@@ -18,10 +21,10 @@ import org.mockito.Mock;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -95,7 +98,7 @@ public class ChannelServiceTest {
                 null
         );
 
-        List<User> users = List.of(user1,user2);
+        List<User> users = List.of(user1, user2);
         given(userRepository.findAllById(request.participantIds())).willReturn(users);
         given(channelMapper.toDto(any())).willReturn(expectedDto);
 
@@ -109,5 +112,55 @@ public class ChannelServiceTest {
 
         verify(channelRepository).save(any());
         verify(readStatusRepository).saveAll(any());
+    }
+
+    @Test
+    @DisplayName("정상적인 Channel update 테스트")
+    void updateChannel_WithValidInput_Success() {
+        Channel channel = new Channel(ChannelType.PUBLIC, "공지 사항", "전체 공지 채널입니다.");
+        ReflectionTestUtils.setField(channel, "id", UUID.randomUUID());
+        PublicChannelUpdateRequest request = new PublicChannelUpdateRequest("전체 공지", "전체 공지 채널입니다.");
+
+        ChannelDto expectedDto = new ChannelDto(
+                channel.getId(),
+                channel.getType(),
+                "전체 공지",
+                "전체 공지 채널입니다.",
+                null,
+                null
+        );
+
+        given(channelRepository.findById(channel.getId())).willReturn(Optional.of(channel));
+        given(channelMapper.toDto(any())).willReturn(expectedDto);
+
+        ChannelDto channelDto = channelService.updateChannel(channel.getId(), request);
+
+        assertEquals(ChannelType.PUBLIC, channelDto.type());
+        assertEquals("전체 공지", channelDto.name());
+        assertEquals("전체 공지 채널입니다.", channelDto.description());
+
+        verify(channelMapper).toDto(channel);
+    }
+    @Test
+    @DisplayName("존재하지 않는 channelId로 업데이트 시 예외 발생 테스트")
+    void updateChannel_WithInvalidChannelId_ThrowException() {
+        UUID noExistId = UUID.randomUUID();
+        PublicChannelUpdateRequest request = new PublicChannelUpdateRequest("전체 공지","전체 공지 채널입니다.");
+
+        given(channelRepository.findById(noExistId)).willReturn(Optional.empty());
+
+        assertThrows(ChannelNotFoundException.class, () -> channelService.updateChannel(noExistId, request));
+    }
+    @Test
+    @DisplayName("Private 채널을 수정하려 할 때 예외 발생 테스트")
+    void update_PrivateChannel_ThrowException() {
+        Channel privateChannel = new Channel(ChannelType.PRIVATE, null, null);
+        ReflectionTestUtils.setField(privateChannel, "id", UUID.randomUUID());
+
+        PublicChannelUpdateRequest request = new PublicChannelUpdateRequest("개인 채널","개인 채널입니다.");
+
+        given(channelRepository.findById(privateChannel.getId())).willReturn(Optional.of(privateChannel));
+
+        assertThrows(PrivateChannelUpdateException.class, () -> channelService.updateChannel(privateChannel.getId(), request));
     }
 }
