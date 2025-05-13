b0c72c611c2dc7e189fb0f57393be8692781e2a8
JJHyunDev <bellhyunforgithub@gmail.com>
2025-05-13 13:14:53 +0900

    feat: UserStatus Custom Exception 정의, GlobalExceptionHandler 정의

---
+++ src/main/java/com/sprint/mission/discodeit/exception/userStatus/UserStatusException.java
@@ -0,0 +1,22 @@
+package com.sprint.mission.discodeit.exception.userStatus;
+
+import com.sprint.mission.discodeit.exception.DiscodeitException;
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import java.util.Map;
+
+public class UserStatusException extends DiscodeitException {
+
+
+  public UserStatusException(ErrorCode errorCode) {
+    super(errorCode);
+  }
+
+  public UserStatusException(ErrorCode errorCode, String message) {
+    super(errorCode, message);
+  }
+
+  public UserStatusException(ErrorCode errorCode, String message,
+      Map<String, Object> details) {
+    super(errorCode, message, details);
+  }
+}
