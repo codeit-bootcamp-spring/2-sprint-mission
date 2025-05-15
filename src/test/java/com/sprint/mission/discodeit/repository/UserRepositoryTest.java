a6a8eed375d137a1c67ebea3702b9211cc18e2c0
hyorim <tg305@naver.com>
2025-05-15 10:30:41 +0900

    test: UserRepository findAllWithProfileAndStatus 테스트 추가

--- src/test/java/com/sprint/mission/discodeit/repository/UserRepositoryTest.java
+++ src/test/java/com/sprint/mission/discodeit/repository/UserRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +11,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @EnableJpaAuditing
@@ -18,6 +23,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Test
     @DisplayName("username으로 유저 조회 성공")
@@ -71,5 +78,22 @@ public class UserRepositoryTest {
     void existsByUsername_false() {
         boolean result = userRepository.existsByUsername("notFoundUser");
         assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("findAllWithProfileAndStatus: 유저와 상태, 프로필을 함께 조회")
+    void findAllWithProfileAndStatus_success() {
+        User user = new User("user1", "user1@email.com", "1234", null);
+        UserStatus status = new UserStatus(user, Instant.now());
+        entityManager.persist(user);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        List<User> result = userRepository.findAllWithProfileAndStatus();
+
+        assertThat(result).hasSize(1);
+        User found = result.get(0);
+        assertThat(found.getStatus().getLastActiveAt()).isNotNull();
     }
 }
