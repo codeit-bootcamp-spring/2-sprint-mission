a83b89071f849810fb4e060bee87822087f86469
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-16 03:03:50 +0900

    Chore: Test 환경에서도 @EnableJpaAuditing 설정을 사용하기 위해 Config 파일로 분리

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@EnableJpaAuditing
-@EnableCaching
 @SpringBootApplication
+@EnableCaching
 public class DiscodeitApplication {
 
   public static void main(String[] args) {
