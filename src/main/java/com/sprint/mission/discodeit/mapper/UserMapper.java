4d2b6cd8d2e7808705e80f97c824ac52ffc1ff34
hyorim <tg305@naver.com>
2025-05-16 14:40:10 +0900

    fix: 테스트 설정 수정 및 UserMapper 매핑 오류 수정

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -5,7 +5,7 @@ import com.sprint.mission.discodeit.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {BinaryContentMapper.class, UserMapper.class})
+@Mapper(componentModel = "spring", uses = {BinaryContentMapper.class, UserStatusMapper.class})
 public interface UserMapper {
 
     @Mapping(target = "online", expression = "java(user.getStatus().isOnline())")
