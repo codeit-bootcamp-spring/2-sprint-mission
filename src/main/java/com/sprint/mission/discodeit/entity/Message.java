942e537fed155c16c94ee98abd2d81f6a68b1e10
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-16 21:54:54 +0900

    Refactor: 테스트 환경에서 H2 데이터베이스가 @Lob 필드를 인식못하는 문제 해결

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.entity;
 
 import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
@@ -36,6 +37,7 @@ public class Message extends BaseUpdatableEntity implements Serializable, Identi
   private List<BinaryContent> attachments;
 
   @Lob
+  @Column(columnDefinition = "TEXT")
   private String content;
 
   @ManyToOne
