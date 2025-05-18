ab0a91eb62e2590a0603c2fa6de5920b1eecf2fc
hanna kong <khn1346@gmail.com>
2025-05-18 23:52:41 +0900

    feat: findAllByTypeOrIdIn 추가

--- src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -1,9 +1,13 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.domain.Channel;
+import com.sprint.mission.discodeit.domain.ChannelType;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChannelRepository extends JpaRepository<Channel, UUID> {
 
+  //추후 사용 예정
+  List<Channel> findAllByTypeOrIdIn(ChannelType type, List<UUID> ids);
 }
