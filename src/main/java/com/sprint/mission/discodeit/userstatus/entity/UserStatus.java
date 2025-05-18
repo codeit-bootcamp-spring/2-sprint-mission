661faaef10d68823ca92065be549611bb41acbcc
황지환 <hth99131@gmail.com>
2025-05-19 00:18:36 +0900

    refactor: userStatus 엔티티 수정

--- src/main/java/com/sprint/mission/discodeit/userstatus/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/userstatus/entity/UserStatus.java
@@ -1,30 +1,24 @@
 package com.sprint.mission.discodeit.userstatus.entity;
 
+import com.sprint.mission.common.entity.base.BaseUpdatableEntity;
 import com.sprint.mission.discodeit.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.UUID;
 
 @Getter
 @Entity
 @Table(name = "user_statuses")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class UserStatus {
+public class UserStatus extends BaseUpdatableEntity {
 
     private static final int DEFAULT_DURATION = 5 * 60 * 1000;
-
-    @Id
-    @Column(name = "id", nullable = false)
-    private UUID id;
 
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
@@ -32,12 +26,6 @@ public class UserStatus {
 
     @Column(name = "last_active_at")
     private Instant lastActiveAt;
-
-    @CreatedDate
-    private Instant createdAt;
-
-    @LastModifiedDate
-    private Instant updatedAt;
 
     public UserStatus(User user, Instant lastActiveAt) {
         this.user = user;
