402b281300fa581721e0fdd0b59653e731cb5564
Seong-taeHan <kmljjn22@gmail.com>
2025-05-18 22:15:23 +0900

    test: user api 통합 테스트 진행
    - 추상 클래스를 이용해 공통 설정 분리

---
+++ src/test/java/com/sprint/mission/discodeit/integration/base/BaseIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.integration.base;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+public abstract class BaseIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected EntityManager entityManager;
+
+}
