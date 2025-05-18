028aae13ed0cf4683af542a34174da94bfb9bea0
hanna kong <khn1346@gmail.com>
2025-05-18 23:52:26 +0900

    feat: existsByEmail, existsByUsername 추가

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -13,6 +13,10 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
   Optional<User> findByEmail(String email);
 
+  boolean existsByEmail(String email);
+
+  boolean existsByUsername(String username);
+
   @EntityGraph(attributePaths = {"status", "profile"})
   List<User> findAll();
 }
