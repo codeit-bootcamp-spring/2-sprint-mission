660e1d55716b1ab5f3d386b71672cb50e591f6bd
Yangjinho <yangjionho826@naver.com>
2025-05-21 19:05:59 +0900

    feat: AWS를 활용한 배포 (AWS RDS, ECR, ECS) 환경 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,62 +1,126 @@
-CREATE TABLE IF NOT EXISTS binary_contents (
-    id UUID PRIMARY KEY,
-    created_at timestamp with time zone NOT NULL,
-    file_name VARCHAR(255) NOT NULL,
-    size BIGINT NOT NULL,
-    bytes BYTEA
-);
-
-CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY,
+-- 테이블
+-- User
+CREATE TABLE users
+(
+    id         uuid PRIMARY KEY,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    profile_id UUID REFERENCES binary_contents(id) ON DELETE SET NULL
+    username   varchar(50) UNIQUE       NOT NULL,
+    email      varchar(100) UNIQUE      NOT NULL,
+    password   varchar(60)              NOT NULL,
+    profile_id uuid
 );
 
-
-CREATE TABLE IF NOT EXISTS user_statuses (
-    id UUID PRIMARY KEY,
-    created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    last_active_at TIMESTAMPTZ NOT NULL
+-- BinaryContent
+CREATE TABLE binary_contents
+(
+    id           uuid PRIMARY KEY,
+    created_at   timestamp with time zone NOT NULL,
+    file_name    varchar(255)             NOT NULL,
+    size         bigint                   NOT NULL,
+    content_type varchar(100)             NOT NULL
+--     ,bytes        bytea        NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS channels (
-    id UUID PRIMARY KEY,
-    created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone,
-    name VARCHAR(100),
-    description VARCHAR(500),
-    type VARCHAR(10) NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE'))
+-- UserStatus
+CREATE TABLE user_statuses
+(
+    id             uuid PRIMARY KEY,
+    created_at     timestamp with time zone NOT NULL,
+    updated_at     timestamp with time zone,
+    user_id        uuid UNIQUE              NOT NULL,
+    last_active_at timestamp with time zone NOT NULL
 );
 
+-- Channel
+CREATE TABLE channels
+(
+    id          uuid PRIMARY KEY,
+    created_at  timestamp with time zone NOT NULL,
+    updated_at  timestamp with time zone,
+    name        varchar(100),
+    description varchar(500),
+    type        varchar(10)              NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS read_statuses (
-    id UUID PRIMARY KEY,
+-- Message
+CREATE TABLE messages
+(
+    id         uuid PRIMARY KEY,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    channel_id UUID NOT NULL REFERENCES channels(id) ON DELETE CASCADE,
-    last_read_at TIMESTAMPTZ NOT NULL,
+    content    text,
+    channel_id uuid                     NOT NULL,
+    author_id  uuid
+);
+
+-- Message.attachments
+CREATE TABLE message_attachments
+(
+    message_id    uuid,
+    attachment_id uuid,
+    PRIMARY KEY (message_id, attachment_id)
+);
+
+-- ReadStatus
+CREATE TABLE read_statuses
+(
+    id           uuid PRIMARY KEY,
+    created_at   timestamp with time zone NOT NULL,
+    updated_at   timestamp with time zone,
+    user_id      uuid                     NOT NULL,
+    channel_id   uuid                     NOT NULL,
+    last_read_at timestamp with time zone NOT NULL,
     UNIQUE (user_id, channel_id)
 );
 
-CREATE TABLE IF NOT EXISTS messages (
-    id UUID PRIMARY KEY,
-    created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone,
-    content TEXT NOT NULL,
-    channel_id UUID NOT NULL REFERENCES channels(id) ON DELETE CASCADE,
-    author_id UUID REFERENCES users(id) ON DELETE SET NULL
-);
 
-CREATE TABLE IF NOT EXISTS message_attachments (
-    id UUID PRIMARY KEY,
-    message_id UUID NOT NULL REFERENCES messages(id) ON DELETE CASCADE,
-    attachment_id UUID NOT NULL REFERENCES binary_contents(id) ON DELETE CASCADE
-);
+-- 제약 조건
+-- User (1) -> BinaryContent (1)
+ALTER TABLE users
+    ADD CONSTRAINT fk_user_binary_content
+        FOREIGN KEY (profile_id)
+            REFERENCES binary_contents (id)
+            ON DELETE SET NULL;
+
+-- UserStatus (1) -> User (1)
+ALTER TABLE user_statuses
+    ADD CONSTRAINT fk_user_status_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE;
+
+-- Message (N) -> Channel (1)
+ALTER TABLE messages
+    ADD CONSTRAINT fk_message_channel
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE;
+
+-- Message (N) -> Author (1)
+ALTER TABLE messages
+    ADD CONSTRAINT fk_message_user
+        FOREIGN KEY (author_id)
+            REFERENCES users (id)
+            ON DELETE SET NULL;
+
+-- MessageAttachment (1) -> BinaryContent (1)
+ALTER TABLE message_attachments
+    ADD CONSTRAINT fk_message_attachment_binary_content
+        FOREIGN KEY (attachment_id)
+            REFERENCES binary_contents (id)
+            ON DELETE CASCADE;
+
+-- ReadStatus (N) -> User (1)
+ALTER TABLE read_statuses
+    ADD CONSTRAINT fk_read_status_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE;
+
+-- ReadStatus (N) -> User (1)
+ALTER TABLE read_statuses
+    ADD CONSTRAINT fk_read_status_channel
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE;
