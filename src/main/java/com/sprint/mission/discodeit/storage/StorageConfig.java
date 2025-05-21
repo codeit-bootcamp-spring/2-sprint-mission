5f4be22f641ab8fb8e5d9b5e72d2162b94f2803f
Jun Hyeok Lee <dlwns4194@gmail.com>
2025-05-21 16:34:50 +0900

    BinaryContentStorage 고도화

--- src/main/java/com/sprint/mission/discodeit/storage/StorageConfig.java
+++ src/main/java/com/sprint/mission/discodeit/storage/StorageConfig.java
@@ -6,13 +6,15 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class StorageConfig {
 
   @Bean
   @ConditionalOnProperty(name = "discodeit.storage.type", havingValue = "s3")
-  public BinaryContentStorage s3Storage(S3Client s3Client, S3Properties properties) {
-    return new S3BinaryContentStorage(s3Client, properties);
+  public BinaryContentStorage s3Storage(S3Client s3Client, S3Properties properties,
+      S3Presigner s3Presigner) {
+    return new S3BinaryContentStorage(s3Client, properties, s3Presigner);
   }
 }
