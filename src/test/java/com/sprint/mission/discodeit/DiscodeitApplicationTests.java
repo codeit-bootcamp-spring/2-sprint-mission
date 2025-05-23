d3c47696a06a943b337df8785f68e9aadd0440a4
Leekb0804 <lkb8713@gmail.com>
2025-05-23 14:35:57 +0900

    feat: @ActiveProfiles("test") 추가

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -2,7 +2,9 @@ package com.sprint.mission.discodeit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DiscodeitApplicationTests {
 
