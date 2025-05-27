c9bdd259226b6df5a6f183387ad6e77543483f3f
seonhyelin <seonseon933@gmail.com>
2025-05-27 18:39:04 +0900

    Refactor: test코드 정리 및 Codecov 뱃지 추가

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -1,23 +1,14 @@
 package com.sprint.mission.discodeit;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class DiscodeitApplicationTests {
 
-
-  @Autowired
-  private Environment env;
-
   @Test
   void contextLoads() {
-    System.out.println("▶ contextLoads started");
-    System.out.println("▶ accessKey = " + env.getProperty("discodeit.storage.s3.access-key"));
-    System.out.println("▶ storage type = " + env.getProperty("discodeit.storage.type"));
   }
 }
