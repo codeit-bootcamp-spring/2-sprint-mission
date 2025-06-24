5106feda71ed5ce8305fbfc2065847f792c982d1
nayu-yuna <dldldbsk@naver.com>
2025-06-24 16:24:17 +0900

    feat: Spring Security 환경 설정

---
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/", "/login", "/swagger-ui/**", "/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+}
+
