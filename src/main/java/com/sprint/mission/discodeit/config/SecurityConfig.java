0741c730a8ceaa62c7b2e1d572de82d5db56546d
Jun Hyeok Lee <dlwns4194@gmail.com>
2025-06-25 14:19:47 +0900

    기본 인증 구현

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,20 +1,57 @@
 package com.sprint.mission.discodeit.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.mapper.UserMapper;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final UserDetailsService userDetailsService;
+  private final PasswordEncoder passwordEncoder;
+  private final UserMapper userMapper;
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsService);
+    provider.setPasswordEncoder(passwordEncoder);
+
+    return new ProviderManager(provider);
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    AuthenticationManager manager = authenticationManager(http);
+    JsonUsernamePasswordAuthenticationFilter loginFilter = new JsonUsernamePasswordAuthenticationFilter(
+        manager);
+    loginFilter.setAuthenticationSuccessHandler(successHandler());
+    loginFilter.setAuthenticationFailureHandler(failureHandler());
+
     http
         .csrf(csrf -> csrf
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())  // CSR 대응
@@ -34,7 +71,10 @@ public class SecurityConfig {
 
             .anyRequest().authenticated()
         )
-
+        .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
+        .securityContext(securityContext -> securityContext
+            .securityContextRepository(new HttpSessionSecurityContextRepository())
+        )
         .httpBasic(Customizer.withDefaults());
 
     SecurityFilterChain chain = http.build();
@@ -44,4 +84,23 @@ public class SecurityConfig {
 
     return chain;
   }
+
+  private AuthenticationSuccessHandler successHandler() {
+    return (request, response, authentication) -> {
+      UserDto userDto = userMapper.toDto((User) authentication.getPrincipal());
+
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      new ObjectMapper().writeValue(response.getWriter(), userDto);
+    };
+  }
+
+  private AuthenticationFailureHandler failureHandler() {
+    return (request, response, exception) -> {
+      response.setStatus(HttpStatus.UNAUTHORIZED.value());
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      new ObjectMapper().writeValue(response.getWriter(),
+          Map.of("message", "인증 실패", "error", exception.getMessage()));
+    };
+  }
+
 }
