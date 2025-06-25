ebaef531d29586df1b2ae4f4a1cb78861e11e9b6
Jaeseok Heo <jasonheo1998@gmail.com>
2025-06-25 11:34:37 +0900

    feat: 코드 전체 베이스코드로 리팩토링, Spring Security 의존성 추가

--- src/main/java/com/sprint/mission/discodeit/exception/WrongPasswordException.java
+++
@@ -1,8 +0,0 @@
-package com.sprint.mission.discodeit.exception;
-
-public class WrongPasswordException extends RuntimeException {
-
-  public WrongPasswordException() {
-    super("Wrong password");
-  }
-}
