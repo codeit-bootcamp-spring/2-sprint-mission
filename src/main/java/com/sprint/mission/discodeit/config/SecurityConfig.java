bc529dbf576f6d12def61fe40ebd63e91d360283
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-06-26 17:46:15 +0900

    Feat: CSRF 토큰 발급 기능 구현

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -15,9 +16,12 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/auth/csrf-token").permitAll()
             .requestMatchers("/api/**").authenticated()
             .anyRequest().permitAll()
         )
+        .csrf(csrf -> csrf
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
         .logout(logout -> logout.disable());
     return http.build();
   }
--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -12,7 +12,10 @@ import com.sprint.mission.discodeit.util.MaskingUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +30,6 @@ public class AuthController implements AuthApi {
   private final AuthService authService;
   private final AuthMapper authMapper;
 
-
   @Override
   @LogMasking
   @PostMapping("/login")
@@ -39,4 +41,11 @@ public class AuthController implements AuthApi {
 
     return ResponseEntity.ok(user);
   }
+
+  @GetMapping("/csrf-token")
+  public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
+    return ResponseEntity.status(HttpStatus.OK).body(csrfToken);
+  }
+
+
 }
