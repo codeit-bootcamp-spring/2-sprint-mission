4b60b141e46d9ca005d1f091b53ecada437580dc
JongwonLee <gudtksfl@gmail.com>
2025-06-27 18:09:29 +0900

    refactor: User UseCase 삭제 및 logback 일부분 수정

--- src/main/java/com/sprint/mission/discodeit/core/user/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/core/user/service/UserService.java
@@ -1,12 +1,26 @@
 package com.sprint.mission.discodeit.core.user.service;
 
-import com.sprint.mission.discodeit.core.user.service.usecase.CreateUserUseCase;
-import com.sprint.mission.discodeit.core.user.service.usecase.DeleteUserUseCase;
-import com.sprint.mission.discodeit.core.user.service.usecase.FindUserUseCase;
-import com.sprint.mission.discodeit.core.user.service.usecase.OnlineUserUseCase;
-import com.sprint.mission.discodeit.core.user.service.usecase.UpdateUserUseCase;
+import com.sprint.mission.discodeit.core.storage.dto.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.core.user.dto.UserDto;
+import com.sprint.mission.discodeit.core.user.dto.UserStatusDto;
+import com.sprint.mission.discodeit.core.user.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.core.user.dto.request.UserStatusRequest;
+import com.sprint.mission.discodeit.core.user.dto.request.UserUpdateRequest;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface UserService extends CreateUserUseCase, FindUserUseCase, UpdateUserUseCase,
-    DeleteUserUseCase, OnlineUserUseCase {
+public interface UserService {
 
+  UserDto create(UserCreateRequest request,
+      Optional<BinaryContentCreateRequest> binaryContentCreateRequest);
+
+  List<UserDto> findAll();
+
+  UserStatusDto online(UUID userId, UserStatusRequest request);
+
+  UserDto update(UUID id, UserUpdateRequest request,
+      Optional<BinaryContentCreateRequest> binaryContentDTO);
+
+  void delete(UUID id);
 }
--- src/main/java/com/sprint/mission/discodeit/core/user/service/usecase/DeleteUserUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.core.user.service.usecase;
-
-import java.util.UUID;
-
-public interface DeleteUserUseCase {
-
-  void delete(UUID id);
-
-}
