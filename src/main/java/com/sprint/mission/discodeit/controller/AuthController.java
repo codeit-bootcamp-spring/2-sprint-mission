0ad1a0b4dfceef09ecf261c2ad4c31989cf48df1
hyorim <tg305@naver.com>
2025-06-28 14:21:11 +0900

    feat: 세션을 활용한 로그인 사용자 정보 조회 API 추가 - 세션ID 기반으로 인증된 사용자 정보를 조회하도록 /api/auth/me 엔드포인트 정의

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,13 +1,16 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
-import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.dto.user.UserDto;
+import com.sprint.mission.discodeit.security.CustomUserDetails;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController implements AuthApi {
 
-    private final AuthService authService;
-
     @GetMapping("/csrf-token")
     public ResponseEntity<Map<String, String>> getToken(CsrfToken token) {
         return ResponseEntity.status(HttpStatus.OK).body(Map.of("token", token.getToken()));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(
+        @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.status(HttpStatus.OK).body(customUserDetails.getUserDto());
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 
