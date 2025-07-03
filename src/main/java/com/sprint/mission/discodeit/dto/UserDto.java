9859ffc17ffb3c7a7b90c6884af548808bf1279e
hanna kong <khn1346@gmail.com>
2025-07-03 14:45:48 +0900

    feat: 권한 추가

--- src/main/java/com/sprint/mission/discodeit/dto/UserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/UserDto.java
@@ -1,13 +1,17 @@
 package com.sprint.mission.discodeit.dto;
 
+import com.sprint.mission.discodeit.domain.Role;
 import java.util.UUID;
+import lombok.Builder;
 
+@Builder(toBuilder = true)
 public record UserDto(
     UUID id,
     String username,
     String email,
     BinaryContentDto profile,
-    boolean online
+    boolean online,
+    Role role
 ) {
 
 }
