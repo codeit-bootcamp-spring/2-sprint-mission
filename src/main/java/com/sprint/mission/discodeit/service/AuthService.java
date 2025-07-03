af2ae1707ffc2c65aebf84e828868c08e2776b3e
hanna kong <khn1346@gmail.com>
2025-07-03 15:47:25 +0900

    feat: 권한 수정 api, admin 생성 기능 구현

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/AuthService.java
@@ -1,10 +1,11 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
 import com.sprint.mission.discodeit.dto.UserDto;
+import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 
 public interface AuthService {
 
-  UserDto login(LoginRequest request);
+  UserDto updateUserRole(RoleUpdateRequest request);
 
+  UserDto initAdmin();
 }
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,38 +1,65 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.domain.Role;
 import com.sprint.mission.discodeit.domain.User;
 import com.sprint.mission.discodeit.dto.UserDto;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
-import com.sprint.mission.discodeit.exception.user.InvalidCredentialsException;
+import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.repository.UserStatusRepository;
+import com.sprint.mission.discodeit.security.SessionManager;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BasicAuthService implements AuthService {
 
   private final UserRepository userRepository;
-  private final UserStatusRepository userStatusRepository;
   private final UserMapper userMapper;
+  private final PasswordEncoder passwordEncoder;
+  private final SessionManager sessionManager;
+
+  @Value("${account.admin.username}")
+  private String adminUsername;
+  @Value("${account.admin.password}")
+  private String adminPassword;
+  @Value("${account.admin.email}")
+  private String adminEmail;
 
   @Transactional
+  @PreAuthorize("hasRole('ADMIN')")
   @Override
-  public UserDto login(LoginRequest request) {
-    User user = userRepository.findByUsername(request.username())
-        .orElseThrow(
-            () -> UserNotFoundException.byUsername(request.username()));
-
-    if (!request.password().equals(user.getPassword())) {
-      throw InvalidCredentialsException.wrongPassword();
-    }
-
+  public UserDto updateUserRole(RoleUpdateRequest request) {
+    User user = userRepository.findById(request.userId())
+        .orElseThrow(() -> UserNotFoundException.byId(request.userId()));
+    user.updateRole(request.newRole());
+    sessionManager.expireUserSessions(user.getId());
     return userMapper.toDto(user);
   }
 
+  @Transactional
+  @Override
+  public UserDto initAdmin() {
+    if (userRepository.existsByEmail(adminEmail) || userRepository.existsByUsername(
+        adminUsername)) {
+      log.warn("이미 존재하는 Admin 계정입니다.");
+      return null;
+    }
+
+    User admin = User.create(adminUsername, adminEmail, passwordEncoder.encode(adminPassword),
+        null);
+    admin.updateRole(Role.ADMIN);
+    userRepository.save(admin);
+
+    log.info("Admin 계정 초기화 완료: username={}", adminUsername);
+    return userMapper.toDto(admin);
+  }
 }
