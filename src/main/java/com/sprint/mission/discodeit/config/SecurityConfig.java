2ef4cda2d90060d887a8ca88ba42a62da5aba428
Leekb0804 <lkb8713@gmail.com>
2025-07-04 17:58:15 +0900

    feat: 로그아웃 구현

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.sprint.mission.discodeit.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.entity.Role;
 import com.sprint.mission.discodeit.security.SecurityMatchers;
+import com.sprint.mission.discodeit.security.SessionRegistryLogoutHandler;
 import com.sprint.mission.discodeit.security.filter.JsonUsernamePasswordAuthenticationFilter;
 import com.sprint.mission.discodeit.security.filter.LogoutFilter;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +12,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,12 +25,13 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(
       HttpSecurity http,
-      ObjectMapper objectMapper
+      ObjectMapper objectMapper,
+      SessionRegistry sessionRegistry
   ) throws Exception {
 
     return http
         .csrf(csrf -> csrf
-            .ignoringRequestMatchers(SecurityMatchers.LOGIN)
+            .ignoringRequestMatchers(SecurityMatchers.LOGOUT, SecurityMatchers.LOGIN)
         )
         .authorizeHttpRequests(authorizeRequests -> authorizeRequests
             .requestMatchers(
@@ -33,10 +39,21 @@ public class SecurityConfig {
                 SecurityMatchers.GET_CSRF_TOKEN,
                 SecurityMatchers.SIGN_UP
             ).permitAll()
+            .anyRequest().hasRole(Role.USER.name())
         )
         .formLogin(AbstractHttpConfigurer::disable)
+        .logout(logout -> logout
+            .logoutRequestMatcher(SecurityMatchers.LOGOUT)
+            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
+            .addLogoutHandler(new SessionRegistryLogoutHandler(sessionRegistry))
+        )
         .with(new JsonUsernamePasswordAuthenticationFilter.Configurer(objectMapper),
             Customizer.withDefaults())
         .build();
+  }
+
+  @Bean
+  public SessionRegistry sessionRegistry() {
+    return new SessionRegistryImpl();
   }
 }
