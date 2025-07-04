b55a2621f2d81d17497c845058802fc847b1985d
Leekb0804 <lkb8713@gmail.com>
2025-07-04 17:53:41 +0900

    feat: url 을 SecurityMatchers로 통합 후 인증 필터 적용 및 csrf 매핑

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,32 +1,42 @@
 package com.sprint.mission.discodeit.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.security.SecurityMatchers;
+import com.sprint.mission.discodeit.security.filter.JsonUsernamePasswordAuthenticationFilter;
+import com.sprint.mission.discodeit.security.filter.LogoutFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+  private final LogoutFilter logoutFilter;
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(
+      HttpSecurity http,
+      ObjectMapper objectMapper
+  ) throws Exception {
+
     return http
         .csrf(csrf -> csrf
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .requestMatchers("/", "/assets/**", "/index.html", "/favicon.ico",
-            "/swagger", "/v3/api-docs/**", "/swagger-resources/**",
-            "/actuator/**",
-            "/login").permitAll()
-            .requestMatchers(HttpMethod.GET, "/api/auth/csrf-token").permitAll()
-            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
-            .anyRequest().authenticated()
+            .ignoringRequestMatchers(SecurityMatchers.LOGIN)
         )
-        .formLogin(withDefaults())
-        .logout(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+            .requestMatchers(
+                SecurityMatchers.NON_API,
+                SecurityMatchers.GET_CSRF_TOKEN,
+                SecurityMatchers.SIGN_UP
+            ).permitAll()
+        )
+        .formLogin(AbstractHttpConfigurer::disable)
+        .with(new JsonUsernamePasswordAuthenticationFilter.Configurer(objectMapper),
+            Customizer.withDefaults())
         .build();
   }
 }
