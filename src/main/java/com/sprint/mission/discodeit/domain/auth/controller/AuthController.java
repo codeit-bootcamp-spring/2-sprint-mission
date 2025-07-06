c9238fd33cb6b9960cf25203c4f446ffe7c312dc
황지환 <hth99131@gmail.com>
2025-07-06 13:35:03 +0900

    feat: 세션을 활용한 현재 사용자 정보 조회

--- src/main/java/com/sprint/mission/discodeit/domain/auth/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/domain/auth/controller/AuthController.java
@@ -1,7 +1,12 @@
 package com.sprint.mission.discodeit.domain.auth.controller;
 
+import com.sprint.mission.discodeit.domain.user.dto.UserResult;
+import com.sprint.mission.discodeit.security.service.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +18,17 @@ public class AuthController {
   @GetMapping("/csrf-token")
   public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
     return ResponseEntity.ok(csrfToken);
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserResult> getUser(
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ) {
+    if (userDetails == null) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    return ResponseEntity.ok(userDetails.getUserResult());
   }
 
 }
