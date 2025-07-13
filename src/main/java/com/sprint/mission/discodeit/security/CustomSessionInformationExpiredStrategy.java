7211048ac067f61f885d6d7cbfd39f3a25107080
hanna kong <khn1346@gmail.com>
2025-07-13 22:30:56 +0900

    feat: SessionAuthenticationException 적용

--- src/main/java/com/sprint/mission/discodeit/security/CustomSessionInformationExpiredStrategy.java
+++ src/main/java/com/sprint/mission/discodeit/security/CustomSessionInformationExpiredStrategy.java
@@ -5,10 +5,9 @@ import com.sprint.mission.discodeit.exception.ErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.Instant;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.security.web.session.SessionInformationExpiredEvent;
 import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 
@@ -25,11 +24,7 @@ public class CustomSessionInformationExpiredStrategy implements SessionInformati
     response.setStatus(status);
     response.setContentType(MediaType.APPLICATION_JSON_VALUE);
     ErrorResponse errorResponse = new ErrorResponse(
-        Instant.now(),
-        "SESSION_EXPIRED",
-        "다른 장치에서 로그인하여 세션이 만료되었습니다.",
-        Map.of("reason", "Concurrent login detected"),
-        "SessionInformationExpiredException",
+        new SessionAuthenticationException("Session is expired."),
         status
     );
     response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
