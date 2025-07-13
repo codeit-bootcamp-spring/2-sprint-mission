846f2ae8373d4384d0090d52c93405d14d545f3a
20184415@s.scnu.ac.kr <@Kuija44861>
2025-07-13 21:33:59 +0900

    레포지토리:메세지 읽음 메서드 추가,기존 csrf 불필요 삭제

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -3,12 +3,15 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.ReadStatus;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
+
+    Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId);
 
     List<ReadStatus> findAllByUserId(UUID userId);
 
