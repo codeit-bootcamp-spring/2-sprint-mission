09f07fd95878c57f6d245444c089f32c6a93941f
양찬혁 <20184415@s.scnu.ac.kr>
2025-07-20 22:50:11 +0900

    리팩토링: 레포로 방향 변경

--- src/main/java/com/sprint/mission/discodeit/mapper/ChannelMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/ChannelMapper.java
@@ -3,24 +3,25 @@ package com.sprint.mission.discodeit.mapper;
 import com.sprint.mission.discodeit.dto.data.ChannelDto;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
-import com.sprint.mission.discodeit.service.MessageService;
-import lombok.RequiredArgsConstructor;
 import org.mapstruct.*;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring",
-    uses = {UserMapper.class}
+        uses = {UserMapper.class}
 )
 public abstract class ChannelMapper {
 
     @Autowired
-    protected MessageService messageService;
+    protected MessageRepository messageRepository;
     @Autowired
     protected UserMapper userMapper;
 
@@ -36,12 +37,14 @@ public abstract class ChannelMapper {
     public abstract List<ChannelDto> toDto(List<Channel> channels);
 
     public Instant getLastMessageTime(Channel channel) {
-        return messageService.lastMessageTime(channel.getId());
+        return messageRepository.findTopByChannelIdOrderByCreatedAtDesc(channel.getId())
+                .map(Message::getCreatedAt)
+                .orElse(null);
     }
 
     protected Set<UserDto> mapParticipants(Channel channel) {
         return readStatusRepository.findAllByChannelId(channel.getId()).stream()
-            .map(readStatus -> userMapper.toDto(readStatus.getUser()))
-            .collect(Collectors.toSet());
+                .map(readStatus -> userMapper.toDto(readStatus.getUser()))
+                .collect(Collectors.toSet());
     }
 }
