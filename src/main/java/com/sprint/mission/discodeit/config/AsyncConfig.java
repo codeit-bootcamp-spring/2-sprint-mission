6278a199f5d76dd132f7576a3905a68cb1998eef
양찬혁 <20184415@s.scnu.ac.kr>
2025-07-20 22:35:01 +0900

    config: 비동기, mdc 설정 추가

---
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -0,0 +1,38 @@
+package com.sprint.mission.discodeit.config;
+
+import com.sprint.mission.discodeit.exception.AsyncExceptionHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+@RequiredArgsConstructor
+public class AsyncConfig implements AsyncConfigurer {
+
+    private final AsyncExceptionHandler asyncExceptionHandler;
+    private final MdcTaskDecorator mdcTaskDecorator;
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(30);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Discodeit-Async-");
+        executor.setTaskDecorator(mdcTaskDecorator);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return asyncExceptionHandler;
+    }
+} 
