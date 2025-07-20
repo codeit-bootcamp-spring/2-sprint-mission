5f76441ca7af629f5797d8778e229b9493214a32
JJHyunDev <bellhyunforgithub@gmail.com>
2025-07-20 22:05:51 +0900

    feat: Notification entity, dto 선언

---
+++ src/main/java/com/sprint/mission/discodeit/entity/Notification.java
@@ -0,0 +1,34 @@
+package com.sprint.mission.discodeit.entity;
+
+import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseUpdatableEntity {
+
+  private UUID receiverId;
+  private String title;
+  private String content;
+  @Enumerated(EnumType.STRING)
+  private NotificationType type;
+  private UUID targetId; // Optional
+
+  public Notification(UUID receiverId, String title, String content, NotificationType type,
+      UUID targetId) {
+    this.receiverId = receiverId;
+    this.title = title;
+    this.content = content;
+    this.type = type;
+    this.targetId = targetId;
+  }
+}
