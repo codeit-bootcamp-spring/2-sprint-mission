94e5dc45d48e0a4a81bad36a9b5eeca324cb6905
seonhyelin <seonseon933@gmail.com>
2025-07-23 18:39:00 +0900

    Feat: 웹소켓 적용

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -15,12 +15,15 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -32,7 +35,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -41,15 +43,25 @@ import lombok.extern.slf4j.Slf4j;
 public class MessageController implements MessageApi {
 
   private final MessageService messageService;
+  private final SimpMessagingTemplate simpMessagingTemplate;
+
+  @MessageMapping("/messages")
+  public void send(MessageCreateRequest request) {
+    log.debug("웹소켓 메시지 수신 : {}", request);
+    MessageDto createdMessage = messageService.create(request, new ArrayList<>());
+    String destination = String.format("/sub/channels/%s/messages", createdMessage.channelId());
+    simpMessagingTemplate.convertAndSend(destination, createdMessage);
+    log.debug("웹소켓 메시지 발신[{}] :  {}", destination, createdMessage);
+  }
 
   @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<MessageDto> create(
       @RequestPart("messageCreateRequest") @Valid MessageCreateRequest messageCreateRequest,
       @RequestPart(value = "attachments", required = false) List<MultipartFile> attachments
   ) {
-    log.info("메시지 생성 요청: request={}, attachmentCount={}", 
+    log.info("메시지 생성 요청: request={}, attachmentCount={}",
         messageCreateRequest, attachments != null ? attachments.size() : 0);
-    
+
     List<BinaryContentCreateRequest> attachmentRequests = Optional.ofNullable(attachments)
         .map(files -> files.stream()
             .map(file -> {
@@ -104,7 +116,7 @@ public class MessageController implements MessageApi {
           sort = "createdAt",
           direction = Direction.DESC
       ) Pageable pageable) {
-    log.info("채널별 메시지 목록 조회 요청: channelId={}, cursor={}, pageable={}", 
+    log.info("채널별 메시지 목록 조회 요청: channelId={}, cursor={}, pageable={}",
         channelId, cursor, pageable);
     PageResponse<MessageDto> messages = messageService.findAllByChannelId(channelId, cursor,
         pageable);
