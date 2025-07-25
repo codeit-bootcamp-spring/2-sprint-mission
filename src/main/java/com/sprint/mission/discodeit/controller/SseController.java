72a62cc811a285ebb3e8a8806edddb836c9e44c3
김민식 <u1r2r3@naver.com>
2025-07-25 16:31:51 +0900

    fix: SSE 연결 실패 원인 해결 및 정상 연결 구현
    - Spirng Security로 인증된 사용자 정보 추출

--- src/main/java/com/sprint/mission/discodeit/controller/SseController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/SseController.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.security.CustomUserDetails;
 import com.sprint.mission.discodeit.service.SseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -14,9 +17,9 @@ public class SseController {
 
     @GetMapping("/api/sse")
     public SseEmitter connect(
-            @RequestHeader("Request-User-ID") String userId,
+            @AuthenticationPrincipal CustomUserDetails user,
             @RequestHeader(value = "Last-Event-ID", required = false) String lastEventId
     ) {
-        return sseService.connect(userId, lastEventId);
+        return sseService.connect(user.getUserId().toString(), lastEventId);
     }
 }
