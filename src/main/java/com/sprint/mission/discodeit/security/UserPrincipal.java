5064ecf6fee96448f0dc03e4479031d0b4446c3a
양찬혁 <20184415@s.scnu.ac.kr>
2025-08-03 23:15:45 +0900

    feat: Userprincipal 추가

---
+++ src/main/java/com/sprint/mission/discodeit/security/UserPrincipal.java
@@ -0,0 +1,52 @@
+package com.sprint.mission.discodeit.security;
+
+import com.sprint.mission.discodeit.entity.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+
+@Getter
+public class UserPrincipal implements UserDetails {
+
+    private final UUID id;
+    private final String username;
+    private final String email;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(UUID id, String username, String email, Role role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+} 
