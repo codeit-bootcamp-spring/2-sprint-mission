78f20c017a15127195da4cea3e21d6d43a644214
양찬혁 <20184415@s.scnu.ac.kr>
2025-08-03 23:22:22 +0900

    fix : 바이너리 엔티티 수정

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -2,22 +2,18 @@ package com.sprint.mission.discodeit.entity;
 
 import com.sprint.mission.discodeit.entity.base.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "binary_contents")
 public class BinaryContent extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
 
     @Column(name = "file_name", nullable = false)
     private String fileName;
@@ -32,21 +28,6 @@ public class BinaryContent extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private BinaryContentUploadStatus uploadStatus;
 
-    @Builder
-    private BinaryContent(String fileName, Long size, String contentType) {
-        this.fileName = fileName;
-        this.size = size;
-        this.contentType = contentType;
-        this.uploadStatus = BinaryContentUploadStatus.WAITING;
-    }
-
-    public static BinaryContent of(String fileName, Long size, String contentType) {
-        return BinaryContent.builder()
-                .fileName(fileName)
-                .size(size)
-                .contentType(contentType)
-                .build();
-    }
 
     public void completeUpload() {
         this.uploadStatus = BinaryContentUploadStatus.SUCCESS;
